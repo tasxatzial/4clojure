28b62da38243d85722864084d52880f421433b9f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-16 13:18:29 +0200

    problem 20

--- core.clj
+++ core.clj
@@ -7,3 +7,11 @@
         nil
         (nth % (- (count %) 1)))
      [[1 2 3 4 5] '(5 4 3) ["b" "c" "d"]])
+
+
+;p20: Penultimate element
+;Write a function which returns the second to last element from a sequence
+(map #(if (>= (count %) 2)
+        (nth % (- (count %) 2))
+        nil)
+     [(list 1 2 3 4 5) ["a" "b" "c"] [[1 2] [3 4]]])
