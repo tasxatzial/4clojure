a112e952d65bd0bef874e2e5cc1c8817872b0b37
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-16 20:13:57 +0200

    problem 39

--- core.clj
+++ core.clj
@@ -263,3 +263,15 @@
                          max_))
                      (first col) (rest col))))
 (map #(apply p38_2 %) [[1 8 3 4] [30 20] [45 67 11]])
+
+
+;p39: Interleave Two Seqs
+;Write a function which takes two sequences and returns the first item from each, then the second item from each, then the third, etc
+;restrictions: interleave
+(def p39 (fn [col1 col2]
+           (reduce concat (map (fn [x1 x2]
+                                 [x1 x2]) col1 col2))))
+(p39 [1 2 3] [:a :b :c])
+(p39 [1 2] [3 4 5 6])
+(p39 [1 2 3 4] [5])
+(p39 [30 20] [25 15])
