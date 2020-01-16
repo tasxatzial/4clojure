5378711c148099289d68ddc8a00f38cc56e5780c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-16 13:16:18 +0200

    problem 19

--- core.clj
+++ core.clj
@@ -1,2 +1,9 @@
 (ns four-clojure.core)
 
+;p19: last element
+;Write a function which returns the last element in a sequence
+;restrictions: last
+(map #(if (empty? %)
+        nil
+        (nth % (- (count %) 1)))
+     [[1 2 3 4 5] '(5 4 3) ["b" "c" "d"]])
