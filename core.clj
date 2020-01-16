0bd341cf038d40c9c0795cb6d8b65f558efb5d76
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-16 20:33:18 +0200

    problem 40

--- core.clj
+++ core.clj
@@ -275,3 +275,15 @@
 (p39 [1 2] [3 4 5 6])
 (p39 [1 2 3 4] [5])
 (p39 [30 20] [25 15])
+
+
+;p40: Interpose a Seq
+;Write a function which separates the items of a sequence by an arbitrary value
+;restrictions: interpose
+(def p40 (fn [N col]
+           (butlast (reduce (fn [result x]
+                              (concat result (concat (list x) (list N))))
+                            '() col))))
+(p40 0 [1 2 3])
+(apply str (p40 ", " ["one" "two" "three"]))
+(p40 :z [:a :b :c :d])
