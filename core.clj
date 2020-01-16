274f463f022ddf2179b172a287417c7e83d57d51
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-16 18:06:50 +0200

    problem 38

--- core.clj
+++ core.clj
@@ -239,3 +239,19 @@
               result
               (cons N1 (my-range (+ N1 1) N2))))))
 (map p34 [1 -2 5] [4 2 8])
+
+
+;p38: Maximum value
+;Write a function which takes a variable number of parameters and returns the maximum value
+;restrictions: max, max-key
+(def p38 (fn [& col]
+           (if (empty? col)
+             nil
+             ((fn my-max [x col]
+                (if (empty? col)
+                  x
+                  (if (> x (first col))
+                    (recur x (rest col))
+                    (recur (first col) (rest col)))))
+              (first col) (rest col)))))
+(map #(apply p38 %) [[1 8 3 4] [30 20] [45 67 11]])
