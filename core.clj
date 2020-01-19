ab226e17fb89251260e06a9449ecede0d4658481
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-19 20:10:17 +0200

    problem 41

--- core.clj
+++ core.clj
@@ -293,3 +293,18 @@
 (p40 0 [1 2 3])
 (apply str (p40 ", " ["one" "two" "three"]))
 (p40 :z [:a :b :c :d])
+
+
+;p41: Drop Every Nth Item
+;Write a function which drops every Nth item from a sequence
+(def p41 (fn [col N]
+           ((fn my-dropNth [result idx]
+              (if (= idx (count col))
+                result
+                (if (= 0 (mod (+ idx 1) N))
+                  (recur result (+ idx 1))
+                  (recur (concat result (list (nth col idx))) (+ idx 1)))))
+            '() 0)))
+(p41 [1 2 3 4 5 6 7 8] 3)
+(p41 [:a :b :c :d :e :f] 2)
+(p41 [1 2 3 4 5 6] 4)
