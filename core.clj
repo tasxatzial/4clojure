1129f8ef6f0afbd5571eaa8f0ca711c72711ab57
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-20 18:27:26 +0200

    problem 44

--- core.clj
+++ core.clj
@@ -363,3 +363,15 @@
 (p43_4 [1 2 3 4 5 6] 2)
 (p43_4 (range 9) 3)
 (p43_4 (range 10) 5)
+
+
+;p44: Rotate Sequence
+;Write a function which can rotate a sequence in either direction
+(def p44 (fn [N col]
+           (let [N (mod N (count col))]
+             (concat (drop N col) (take N col)))))
+(p44 6 [1 2 3 4 5])
+(p44 -2 [1 2 3 4 5])
+(p44 6 [1 2 3 4 5])
+(p44 1 '(:a :b :c))
+(p44 -4 '(:a :b :c))
