e9afbeb365005d85455f960473e767dd8040eb73
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-21 14:01:29 +0200

    problem 50, solution 2

--- core.clj
+++ core.clj
@@ -422,6 +422,7 @@
 ;Write a function which takes a sequence consisting of items with different types and splits them up into a set of
 ;homogeneous sub-sequences. The internal order of each sub-sequence should be maintained, but the sub-sequences
 ;themselves can be returned in any order (this is why 'set' is used in the test cases)
+;solution 1: filter
 (def p50 (fn [col]
            (map #(apply vector %)
                 (filter #(not-empty %)
@@ -429,3 +430,12 @@
 (set (p50 [1 2 3 :a]))
 (set (p50 [:a "foo"  "bar" :b]))
 (set (p50 [[1 2] :a [3 4] 5 6 :b]))
+
+;solution 2: group-by
+(def p50_2 (fn [col]
+             (filter #(not (nil? %))
+                     (map #(% true)
+                          (map #(group-by % col) [keyword? number? string? vector?])))))
+(set (p50_2 [1 2 3 :a]))
+(set (p50_2 [:a "foo"  "bar" :b]))
+(set (p50_2 [[1 2] :a [3 4] 5 6 :b]))
