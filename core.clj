2a2c3278de933abbba63b1ec5e35b26ee9d0fc22
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-21 14:21:16 +0200

    problem 52

--- core.clj
+++ core.clj
@@ -444,3 +444,8 @@
 ;p51: Advanced Destructuring
 ;Here is an example of some more sophisticated destructuring
 (= [1 2 [3 4 5] [1 2 3 4 5]] (let [[a b & c :as d] [1 2 3 4 5]] [a b c d])) ;[1 2 3 4 5]
+
+
+;p52: Intro to Destructuring
+;Let bindings and function parameter lists support destructuring
+(= [2 4] (let [[a b c d e] [0 1 2 3 4]] [c e]))             ;[c e]
