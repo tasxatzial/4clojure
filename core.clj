1dc888ce4b9bbd85673f738d19c6063bf0dd0ee0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-21 10:45:29 +0200

    problem 46

--- core.clj
+++ core.clj
@@ -382,3 +382,12 @@
 (take 5 (iterate #(+ 3 %) 1))                               ;(1 4 7 10 13)
 
 
+;p46: Flipping out
+;Write a higher-order function which flips the order of the arguments of an input function
+(def p46 (fn [func]
+           (fn [arg1 arg2]
+             (func arg2 arg1))))
+((p46 nth) 2 [1 2 3 4 5])
+((p46 >) 7 8)
+((p46 quot) 2 8)
+((p46 take) [1 2 3 4 5] 3)
