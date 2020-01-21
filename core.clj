47ad0878da45bed2765d06beb29828df81ae2252
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-21 13:59:49 +0200

    problem 50

--- core.clj
+++ core.clj
@@ -416,3 +416,16 @@
 (p49 3 [1 2 3 4 5 6])
 (p49 1 [:a :b :c :d])
 (p49 2 [[1 2] [3 4] [5 6]])
+
+
+;p50: Split by Type
+;Write a function which takes a sequence consisting of items with different types and splits them up into a set of
+;homogeneous sub-sequences. The internal order of each sub-sequence should be maintained, but the sub-sequences
+;themselves can be returned in any order (this is why 'set' is used in the test cases)
+(def p50 (fn [col]
+           (map #(apply vector %)
+                (filter #(not-empty %)
+                        (map #(filter % col) [keyword? number? string? vector?])))))
+(set (p50 [1 2 3 :a]))
+(set (p50 [:a "foo"  "bar" :b]))
+(set (p50 [[1 2] :a [3 4] 5 6 :b]))
