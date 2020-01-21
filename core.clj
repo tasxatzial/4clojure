d7a87e597fa304fad0755b6e37376478ebe6473f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-21 10:03:07 +0200

    problem 45

--- core.clj
+++ core.clj
@@ -375,3 +375,10 @@
 (p44 6 [1 2 3 4 5])
 (p44 1 '(:a :b :c))
 (p44 -4 '(:a :b :c))
+
+
+;p45: Intro to Iterate
+;The iterate function can be used to produce an infinite lazy sequence
+(take 5 (iterate #(+ 3 %) 1))                               ;(1 4 7 10 13)
+
+
