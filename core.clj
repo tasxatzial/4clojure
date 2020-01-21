b414cb1f4cdaf28697fdfb423457fe988526379d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-21 10:50:06 +0200

    problem 47

--- core.clj
+++ core.clj
@@ -391,3 +391,12 @@
 ((p46 >) 7 8)
 ((p46 quot) 2 8)
 ((p46 take) [1 2 3 4 5] 3)
+
+
+;p47: Contain Yourself
+;The contains? function checks if a KEY is present in a given collection.
+;This often leads beginner clojurians to use it incorrectly with numerically indexed collections like vectors and lists
+(contains? #{4 5 6} 4)                                      ;true; 4
+(contains? [1 1 1 1 1] 4)                                   ;true, 4th index
+(contains? {4 :a 2 :b} 4)                                   ;true, 4
+(not (contains? [1 2 4] 4))                                 ;true, 4th index
