393dc6b43fe1635a83baa039656e44bb0ab7dc7a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-22 02:11:15 +0200

    problem 53

--- core.clj
+++ core.clj
@@ -449,3 +449,26 @@
 ;p52: Intro to Destructuring
 ;Let bindings and function parameter lists support destructuring
 (= [2 4] (let [[a b c d e] [0 1 2 3 4]] [c e]))             ;[c e]
+
+
+;p53: Longest Increasing Sub-Seq
+;Given a vector of integers, find the longest consecutive sub-sequence of increasing numbers.
+;If two sub-sequences have the same length, use the one that occurs first.
+;An increasing sub-sequence must have a length of 2 or greater to qualify
+(def p53 (fn [col]
+           (reduce (fn [result x]
+                     (if (> (count x) (count result))
+                       x
+                       result))
+                   [] (filter #(> (count %) 1)
+                              (reduce (fn [result x]
+                                        (if (empty? result)
+                                          (conj result [x])
+                                          (if (> x (last (last result)))
+                                            (conj (apply vector (butlast result)) (conj (apply vector (last result)) x))
+                                            (conj result [x]))))
+                                      [] col)))))
+(p53 [1 0 1 2 3 0 4 5])
+(p53 [5 6 1 3 2 7])
+(p53 [2 3 3 4 5])
+(p53 [7 6 5 4])
