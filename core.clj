7f449e32ac45a8647a77889f5e104bcc3adada70
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-22 16:07:27 +0200

    problem 34: simplify

--- core.clj
+++ core.clj
@@ -234,12 +234,10 @@
 ;p34: Implement range
 ;Write a function which creates a list of all integers in a given range
 ;restrictions: range
-(def p34 (fn my-range
-           ([N1 N2] (my-range N1 N2 '()))
-           ([N1 N2 result]
-            (if (= N1 N2)
-              result
-              (cons N1 (my-range (+ N1 1) N2))))))
+;works due to the fact that (cons X nil) -> (X)
+(def p34 (fn my-range [N1 N2]
+           (when (< N1 N2)
+             (cons N1 (my-range (+ N1 1) N2)))))
 (p34 1 4)
 (p34 -2 2)
 (p34 5 8)
