d4db06e9ce003035fa59c53f8ec8bef528f80b18
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-22 12:09:10 +0200

    problem 55

--- core.clj
+++ core.clj
@@ -493,3 +493,16 @@
 (p54_2 3 (range 9))
 (p54_2 2 (range 8))
 (p54_2 3 (range 8))
+
+
+;p55: Count Occurrences
+;Write a function which returns a map containing the number of occurrences of each distinct item in a sequence
+(def p55 (fn [col]
+            (reduce (fn [result x]
+                      (if (result x)
+                        (conj result [x (+ 1 (result x))])
+                        (conj result [x 1])))
+                    {} col)))
+(p55 [1 1 2 3 2 1 1])
+(p55 [:b :a :b :a :b])
+(p55 '([1 2] [1 3] [1 3]))
