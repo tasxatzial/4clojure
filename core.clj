efb4b8b6118c1ab403a2d4023eac353f16e76609
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-24 20:41:16 +0200

    problem 67

--- core.clj
+++ core.clj
@@ -633,3 +633,18 @@
 (p66 10 5)
 (p66 5 7)
 (p66 1023 858)
+
+
+;p67: Prime Numbers
+;Write a function which returns the first x number of prime numbers
+(def p67 (fn my-primes
+           ([x] (my-primes x [] 2))
+           ([x result N]
+            (if (= x 0)
+              result
+              (if (contains? (set (map #(mod N %) result)) 0)
+                (my-primes x result (+ N 1))
+                (my-primes (- x 1) (conj result N) (+ N 1)))))))
+(p67 2)
+(p67 5)
+(last (p67 100))
