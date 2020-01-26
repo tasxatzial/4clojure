a64cbe0985f2ee2fcd0c8f322268e6b44962b787
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-26 22:11:56 +0200

    problem 73

--- core.clj
+++ core.clj
@@ -709,3 +709,96 @@
 (= (apply + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))      ;apply +
    (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (apply +))
    11)
+
+
+;Analyze a Tic-Tac-Toe Board
+;A tic-tac-toe board is represented by a two dimensional vector. X is represented by :x, O is represented by :o,
+;and empty is represented by :e. A player wins by placing three Xs or three Os in a horizontal, vertical, or diagonal
+;row. Write a function which analyzes a tic-tac-toe board and returns :x if X has won, :o if O has won, and nil
+;if neither player has won
+(def p73_1 (fn [row]                                        ;analyze a row or column
+             (let [sr (set row)]
+               (when (= 1 (count sr))
+                 (if (contains? sr :x)
+                   :x
+                   (when (contains? sr :o)
+                     :o))))))
+
+(def tic_cols ((fn [tic]                                    ;all columns
+                 (apply map vector tic))
+               tic))
+
+(def d1 ((fn [tic]                                          ;diagonal 1
+           [(get (get tic 0) 0) (get (get tic 1) 1) (get (get tic 2) 2)])
+         tic))
+
+(def d2 ((fn [tic]                                          ;diagonal 2
+           [(get (get tic 0) 0) (get (get tic 1) 1) (get (get tic 2) 2)])
+         tic))
+
+(def p73_2 (fn [tic]                                        ;rows + columns + diagonal 1 + diagonal 2
+           (concat tic tic_cols [d1] [d2])))
+
+(def p73_3 (fn [col]                                        ;analyze (rows + columns + diagonal 1 + diagonal 2)
+            (set (reduce (fn [result x]                     ;should contain 8 values
+                           (concat result [(p73_1 x)]))
+                         [] (p73_2 col)))))
+
+(def p73_S (fn [col]                                        ;check for :x :o
+             (let [res ((memoize p73_3) col)]
+               (if (contains? res :x)
+                 :x
+                 (if (contains? res :o)
+                   :o
+                   nil)))))
+
+;submit to 4clojure
+(def p73 (fn [tic]
+             (let [res ((memoize (fn [col]
+                                   (set (reduce (fn [result x]
+                                                  (concat result [((fn [row]
+                                                                     (let [sr (set row)]
+                                                                       (when (= 1 (count sr))
+                                                                         (if (contains? sr :x)
+                                                                           :x
+                                                                           (when (contains? sr :o)
+                                                                             :o))))) x)]))
+                                                [] ((fn [tic]
+                                                      (concat tic
+                                                              ((fn [tic]
+                                                                 (apply map vector tic))
+                                                               tic)
+                                                              [((fn [tic]
+                                                                  [(get (get tic 0) 0) (get (get tic 1) 1) (get (get tic 2) 2)])
+                                                                tic)]
+                                                              [((fn [tic]
+                                                                  [(get (get tic 0) 2) (get (get tic 1) 1) (get (get tic 2) 0)])
+                                                                tic)]))
+                                                    col))))) tic)]
+               (if (contains? res :x)
+                 :x
+                 (if (contains? res :o)
+                   :o
+                   nil)))))
+
+(= nil (p73 [[:e :e :e]
+            [:e :e :e]
+            [:e :e :e]]))
+(= :x (p73 [[:x :e :o]
+           [:x :e :e]
+           [:x :e :o]]))
+(= :o (p73 [[:e :x :e]
+           [:o :o :o]
+           [:x :e :x]]))
+(= nil (p73 [[:x :e :o]
+            [:x :x :e]
+            [:o :x :o]]))
+(= :x (p73 [[:x :e :e]
+           [:o :x :e]
+           [:o :e :x]]))
+(= :o (p73 [[:x :e :o]
+           [:x :o :e]
+           [:o :e :x]]))
+(= nil (p73 [[:x :o :x]
+            [:x :o :x]
+            [:o :x :o]]))
