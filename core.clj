2cd54c9a7a5118c555e28f87d8dcf5ba983d3fb3
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-27 00:52:37 +0200

    problem 75

--- core.clj
+++ core.clj
@@ -813,3 +813,21 @@
                                                  (clojure.string/split string #","))))))
 (p74 "4,5,6,7,8,9")
 (p74 "15,16,25,36,37")
+
+
+;p75: Euler's Totient Function
+;Two numbers are coprime if their greatest common divisor equals 1. Euler's totient function f(x) is defined as the
+;number of positive integers less than x which are coprime to x. The special case f(1) equals 1. Write a function
+;which calculates Euler's totient function
+(def p75 (fn [N]
+           (if (= 1 N )
+             1
+             (count (filter #(= 1 %)
+                            (map (fn my-gcd [N1 N2]
+                                   (if (= 0 N2)
+                                     N1
+                                     (my-gcd N2 (mod N1 N2)))) (range 1 N) (repeat N)))))))
+(p75 1)
+(p75 10)
+(p75 40)
+(p75 99)
