e34715972fe2a5abc8b8a2d6e2002935a9aea0f6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-27 00:34:49 +0200

    problem 74

--- core.clj
+++ core.clj
@@ -802,3 +802,14 @@
 (= nil (p73 [[:x :o :x]
             [:x :o :x]
             [:o :x :o]]))
+
+
+;p74: Filter Perfect Squares
+;Given a string of comma separated integers, write a function which returns a new comma separated string that only
+;contains the numbers which are perfect squares
+(def p74 (fn [string]
+           (clojure.string/join "," (filter #(< (- (Math/sqrt %) (Math/floor (Math/sqrt %))) 0.001)
+                                            (map #(read-string %)
+                                                 (clojure.string/split string #","))))))
+(p74 "4,5,6,7,8,9")
+(p74 "15,16,25,36,37")
