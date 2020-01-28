97dc1ea74d4cbdfd17149742e64d193fa5e23ec3
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-28 19:34:38 +0200

    problem 78

--- core.clj
+++ core.clj
@@ -894,3 +894,26 @@
 
 (p77_2 ["meat" "mat" "team" "mate" "eat"])
 (p77_2 ["veer" "lake" "item" "kale" "mite" "ever"])
+
+
+;p78: Reimplement Trampoline
+;Reimplement the function described in "Intro to Trampoline" (problem 76)
+(def p78 (fn my-trampoline
+           ([fn1 & args]
+            (if (empty? args)
+              (my-trampoline fn1)
+              (my-trampoline (apply fn1 args))))
+           ([fn1]
+            (if (fn? fn1)
+              (my-trampoline (fn1))
+              fn1))))
+
+(= (letfn [(triple [x] #(sub-two (* 3 x)))
+           (sub-two [x] #(stop?(- x 2)))
+           (stop? [x] (if (> x 50) x #(triple x)))]
+     (p78 triple 2))
+   82)
+(= (letfn [(my-even? [x] (if (zero? x) true #(my-odd? (dec x))))
+           (my-odd? [x] (if (zero? x) false #(my-even? (dec x))))]
+     (map (partial p78 my-even?) (range 6)))
+   [true false true false true false])
