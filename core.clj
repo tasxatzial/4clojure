c004172914bbbdbe2f656cf73c450fe4b2284856
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-29 07:21:32 +0200

    problem 81, solution 2

--- core.clj
+++ core.clj
@@ -964,6 +964,7 @@
 ;p81: Set Intersection
 ;Write a function which returns the intersection of two sets.
 ;The intersection is the sub-set of items that each set has in common
+;solution 1: union
 (def p81 (fn [col1 col2]
            (reduce (fn [result x]
                      (if (and (contains? col1 x) (contains? col2 x))
@@ -973,3 +974,14 @@
 (p81 #{0 1 2 3} #{2 3 4 5})
 (p81 #{0 1 2} #{3 4 5})
 (p81 #{:a :b :c :d} #{:c :e :a :f :d})
+
+;solution2
+(def p81_2 (fn [col1 col2]
+             (reduce (fn [result x]
+                       (if (contains? col2 x)
+                         (conj result x)
+                         result))
+                     #{} col1)))
+(p81_2 #{0 1 2 3} #{2 3 4 5})
+(p81_2 #{0 1 2} #{3 4 5})
+(p81_2 #{:a :b :c :d} #{:c :e :a :f :d})
