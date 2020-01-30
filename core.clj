cad9072fd3fb08a0a8e91f634e00e3afc2c0d05a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-30 11:09:56 +0200

    problem 83

--- core.clj
+++ core.clj
@@ -985,3 +985,16 @@
 (p81_2 #{0 1 2 3} #{2 3 4 5})
 (p81_2 #{0 1 2} #{3 4 5})
 (p81_2 #{:a :b :c :d} #{:c :e :a :f :d})
+
+
+;p83: A Half-Truth
+;Write a function which takes a variable number of booleans. Your function should return true if some of the
+;parameters are true, but not all of the parameters are true. Otherwise your function should return false
+(def p83 (fn [& args]
+           (and (or (some true? args) false)  (or (some false? args) false))))
+(p83 false false)
+(p83 true false)
+(p83 true)
+(p83 false true false)
+(p83 true true true)
+(p83 true true true false)
