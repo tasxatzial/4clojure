4146bb7459636b768ec63f53cd68c1cd28c0fc2d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-30 18:00:05 +0200

    problem 90

--- core.clj
+++ core.clj
@@ -1065,3 +1065,20 @@
 (p88_2 #{:a :b :c} #{})
 (p88_2 #{} #{4 5 6})
 (p88_2 #{[1 2] [2 3]} #{[2 3] [3 4]})
+
+
+;p90: Cartesian Product
+;Write a function which calculates the Cartesian product of two sets
+(def p90 (fn [set1 set2]
+           (reduce (fn [result x]
+                     (into result (reduce (fn [result2 x2]
+                                            (conj result2 [x x2]))
+                                          #{} set2)))
+                   #{} set1)))
+(= (p90 #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
+   #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
+     ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
+     ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})
+(p90 #{1 2 3} #{4 5})
+(= 300 (count (p90 (into #{} (range 10))
+                  (into #{} (range 30)))))
