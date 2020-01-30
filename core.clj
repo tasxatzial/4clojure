e13410d78e125d3f892a8d8c539da18bc9c8b4a0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-30 12:23:31 +0200

    problem 85

--- core.clj
+++ core.clj
@@ -998,3 +998,20 @@
 (p83 false true false)
 (p83 true true true)
 (p83 true true true false)
+
+
+;p85: Power Set
+;Write a function which generates the power set of a given set. The power set of a set x is the set of all
+;subsets of x, including the empty set and x itself
+(def p85 (fn [col]
+           (conj (reduce (fn [result x]
+                           (into result (conj (reduce (fn [result2 x2]
+                                                        (conj result2 (conj x2 x)))
+                                                      #{} result)
+                                              #{x})))
+                         #{} col)
+                 #{})))
+(p85 #{1 :a})
+(p85 #{})
+(p85 #{1 2 3})
+(= (count (p85 (into #{} (range 10)))) 1024)
