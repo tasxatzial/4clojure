f76b7e09fe266ed443ed45e4293152fb6d036617
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-01-31 14:56:09 +0200

    problem 95

--- core.clj
+++ core.clj
@@ -1113,3 +1113,27 @@
 (p93  [["Do"] ["Nothing"]])
 (p93 [[[[:a :b]]] [[:c :d]] [:e :f]])
 (p93 '((1 2)((3 4)((((5 6)))))))
+
+
+;p95: To Tree, or not to Tree
+;Write a predicate which checks whether or not a given sequence represents a binary tree. Each node in the tree must
+;have a value, a left child, and a right child
+(def p95 (fn tree-check [node]
+                     (if (nil? node)
+                       true
+                       (if (sequential? node)
+                         (if (and (= (count node) 3)
+                                  (tree-check (second node))
+                                  (tree-check (nth node 2)))
+                           true
+                           false)
+                         (if node
+                           true
+                           false)))))
+(p95 '(:a (:b nil nil) nil))
+(p95 '(:a (:b nil nil)))
+(p95 [1 nil [2 [3 nil nil] [4 nil nil]]])
+(p95 [1 [2 nil nil] [3 nil nil] [4 nil nil]])
+(p95 [1 [2 [3 [4 nil nil] nil] nil] nil])
+(p95 [1 [2 [3 [4 false nil] nil] nil] nil])
+(p95 '(:a nil ()))
