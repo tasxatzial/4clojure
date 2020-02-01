05296a40df92d124aff5a295a6b73ed65f7a31cf
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-01 03:43:40 +0200

    problem 98

--- core.clj
+++ core.clj
@@ -1188,3 +1188,17 @@
 (p97 1)
 (map p97 (range 1 6))
 (p97 11)
+
+
+;p98: Equivalence Classes
+;A function f defined on a domain D induces an equivalence relation on D, as follows: a is equivalent to b with
+;respect to f if and only if (f a) is equal to (f b). Write a function with arguments f and D that computes the
+;equivalence classes of D with respect to f
+(def p98 (fn [func col]
+           (reduce (fn [result x]
+                     (conj result (set (x 1))))
+                   #{} (group-by func col))))
+(p98 #(* % %) #{-2 -1 0 1 2})
+(p98 #(rem % 3) #{0 1 2 3 4 5})
+(p98 identity #{0 1 2 3 4})
+(p98 (constantly true) #{0 1 2 3 4})
