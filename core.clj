89b62639277aac85855ada30db6c758b80f078fc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-06 19:52:39 +0200

    problem 100

--- core.clj
+++ core.clj
@@ -1211,3 +1211,20 @@
 (p99 1 1)
 (p99 99 9)
 (p99 999 99)
+
+
+;p100: Least Common Multiple
+;Write a function which calculates the least common multiple. Your function should accept a variable number of
+;positive integers or ratios
+(def p100 (fn [& args]
+            (let [maxN (memoize (fn [args] (apply max args)))]
+              ((fn my-lcm[i]
+                 (if (some #(not= 0 (mod i %)) args)
+                   (recur (+ (maxN args) i))
+                   i))
+               (maxN args)))))
+(p100 2 3)
+(p100 3 5 7)
+(p100 1/3 2/5)
+(p100 3/4 1/6)
+(p100 7 5/7 2 3/5)
