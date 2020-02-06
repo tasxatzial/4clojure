1e88bcaee5eb24c77470fe46a470212ab2ca49ae
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-06 20:38:12 +0200

    problem 102

--- core.clj
+++ core.clj
@@ -1244,3 +1244,18 @@
 (p100_2 1/3 2/5)
 (p100_2 3/4 1/6)
 (p100_2 7 5/7 2 3/5)
+
+
+;p102: intoCamelCase
+;When working with java, you often need to create an object with fieldsLikeThis, but you'd rather work with a hashmap
+;that has :keys-like-this until it's time to convert. Write a function which takes lower-case hyphen-separated strings
+;and converts them to camel-case strings
+(def p102 (fn [s]
+            (let [res (memoize (fn [s]
+                                 (clojure.string/split s #"-")))]
+              (str (first (res s))
+                   (apply str (map #(str (clojure.string/upper-case (first %)) (apply str (rest %)))
+                                   (rest (res s))))))))
+(p102 "something")
+(p102 "multi-word-key")
+(p102 "leaveMeAlone")
