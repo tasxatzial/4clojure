19540288fb38d7ffac31f5399f9849ef69c59cdc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-07 22:12:30 +0200

    problem 105

--- core.clj
+++ core.clj
@@ -1281,3 +1281,21 @@
 (p103 3 #{0 1 2 3 4})
 (p103 4 #{[1 2 3] :a "abc" "efg"})
 (p103 2 #{[1 2 3] :a "abc" "efg"})
+
+
+;p105: Identify keys and values
+;Given an input sequence of keywords and numbers, create a map such that each key in the map is a keyword,
+;and the value is a sequence of all the numbers (if any) between it and the next keyword in the sequence
+(def p105 (fn idk
+            ([col] (idk {} col))
+            ([result col] (idk result col (first col)))
+            ([result col x]
+             (if (empty? col)
+               result
+               (if (keyword? (first col))
+                 (recur (assoc result (first col) []) (rest col) (first col))
+                 (recur (assoc result x (conj (result x) (first col))) (rest col) x))))))
+(p105 [])
+(p105 [:a 1])
+(p105 [:a 1, :b 2])
+(p105 [:a 1 2 3 :b :c 4])
