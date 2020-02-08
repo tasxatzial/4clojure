75d6a0ff05d2e5902334bfddf84f8d9fb4dcea20
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-08 07:09:09 +0200

    problem 106

--- core.clj
+++ core.clj
@@ -1299,3 +1299,22 @@
 (p105 [:a 1])
 (p105 [:a 1, :b 2])
 (p105 [:a 1 2 3 :b :c 4])
+
+
+;p106: Simple closures
+;Lexical scope and first-class functions are two of the most basic building blocks of a functional language like
+;Clojure. When you combine the two together, you get something very powerful called lexical closures. With these,
+;you can exercise a great deal of control over the lifetime of your local bindings, saving their values for use
+;later, long after the code you're running now has finished.
+;It can be hard to follow in the abstract, so let's build a simple closure. Given a positive integer n, return a
+;function (f x) which computes x^n. Observe that the effect of this is to preserve the value of n for use outside
+;the scope in which it is defined
+(def p106 (fn [n]
+            #((fn my-pow [res n]
+                (if (= 0 n)
+                  res
+                  (recur (* res %) (- n 1))))
+              1 n)))
+((p106 2) 16)
+(map (p106 3) [1 2 3 4])
+(map #((p106 %) 2) [0 1 2 3 4])
