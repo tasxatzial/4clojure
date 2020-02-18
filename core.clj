9ecef4bb09d2455c9bcf9aad6af6dfd48b1a41ec
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-18 21:43:14 +0200

    problem 112: bugfix

--- core.clj
+++ core.clj
@@ -1373,17 +1373,19 @@
 ;of the input collection and return a sequence which maintains the nested structure, and which includes all
 ;elements starting from the head whose sum is less than or equal to the input integer
 (def p112 (fn [N col]
-            ((fn my-sh [col sm fcol idx]
-               (if (nil? (nth col idx nil))
-                 fcol
-                 (if (sequential? (nth col idx))
-                   (concat fcol (list (my-sh (nth col idx) sm '() 0)))
-                   (if (< N (+ sm (nth col idx)))
-                     fcol
-                     (my-sh col (+ sm (nth col idx)) (concat fcol [(nth col idx)]) (+ idx 1))))))
-             col 0 '() 0)))
-(p112 30 [1 2 [3 [4 5] 6] 7])
-(p112 48 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11])
+             ((fn my-sh [col sm fcol]
+                (if (empty? col)
+                  fcol
+                  (if (sequential? (first col))
+                    (let [f (memoize (fn [col]
+                                       (my-sh (first col) sm '())))]
+                      (recur (next col) (+ sm (reduce + (flatten (f col)))) (concat fcol [(f col)])))
+                    (if (< N (+ sm (first col)))
+                      fcol
+                      (recur (next col) (+ sm (first col)) (concat fcol [(first col)]))))))
+              col 0 '())))
+(p112 10 [1 2 [3 [4 5] 6] 7])
+(p112 30 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11])
 (p112 9 (range))
 (p112 1 [[[[[1]]]]])
 (p112 0 [1 2 [3 [4 5] 6] 7])
