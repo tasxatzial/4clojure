2bc18ad65fc65e0c3c838000b32add83f9e22a21
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-23 13:19:03 +0200

    problem 132

--- core.clj
+++ core.clj
@@ -1655,3 +1655,28 @@
       #{1 -1 2 -2 4 -4 8 -8})
 (p131 #{-10 9 -8 7 -6 5 -4 3 -2 1}
       #{10 -9 8 -7 6 -5 4 -3 2 -1})
+
+
+;p132: Insert between two items
+;Write a function that takes a two-argument predicate, a value, and a collection; and returns a new collection
+;where the value is inserted between every two items that satisfy the predicate
+(def p132 (fn [pred val col]
+            (if (empty? col)
+              col
+              ((fn my-ins [idx]
+                 (if (nil? (nth col (+ idx 1) nil))
+                   [(nth col idx)]
+                   (if (pred (nth col idx) (nth col (+ idx 1)))
+                     (lazy-seq (cons (nth col idx) (cons val (my-ins (+ idx 1)))))
+                     (lazy-seq (cons (nth col idx) (my-ins (+ idx 1)))))))
+               0))))
+(p132 < :less [1 6 7 4 3])
+(p132 > :more [2])
+(p132 #(and (pos? %) (< % %2)) :x (range 5))
+(empty? (p132 > :more ()))
+(take 12 (->> [0 1]
+              (iterate (fn [[a b]] [b (+ a b)]))
+              (map first) ; fibonacci numbers
+              (p132 (fn [a b] ; both even or both odd
+                      (= (mod a 2) (mod b 2)))
+                    :same)))
