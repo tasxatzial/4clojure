79a0cd6b8379d38e4a2aa2704b59f4f4d96d6408
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-24 14:02:44 +0200

    problem 135

--- core.clj
+++ core.clj
@@ -1690,3 +1690,20 @@
 (p134 :a {:a nil :b 2})
 (p134 :b {:a nil :b 2})
 (p134 :c {:a nil :b 2})
+
+
+;p135: Infix Calculator
+;Your friend Joe is always whining about Lisps using the prefix notation for math. Show him how you could easily
+;write a function that does math using the infix notation. Is your favorite language that flexible, Joe? Write a
+;function that accepts a variable length mathematical expression consisting of numbers and the operations +, -, *,
+;and /. Assume a simple calculator that does not do precedence and instead just calculates left to right
+(def p135 (fn [& args]
+            ((fn [result args]
+               (if (nil? (first args))
+                 result
+                 (recur ((first args) result (second args)) (drop 2 args))))
+             (first args) (rest args))))
+(p135 2 + 5)
+(p135 38 + 48 - 2 / 2)
+(p135 10 / 2 - 1 * 2)
+(p135 20 / 2 + 2 + 4 + 8 - 6 - 10 * 9)
