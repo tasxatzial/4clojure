fb48f9e9a27d57105a3b3929a72166e8c089d1a0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-25 12:59:12 +0200

    problem 144

--- core.clj
+++ core.clj
@@ -1770,3 +1770,16 @@
 (p143 [1 1 1] [1 1 1])
 (p143 [1 2 3] [4 5 6])
 (p143 [2 5 6] [100 10 1])
+
+
+;p144: Oscilrate
+;Write an oscillating iterate: a function that takes an initial value and a variable number of functions. It should
+;return a lazy sequence of the functions applied to the value in order, restarting from the first function after
+;it hits the end
+(def p144 (fn [val & args]
+            ((fn oscil [val idx]
+               (cons val (lazy-seq (oscil ((nth args (mod idx (count args))) val) (+ idx 1)))))
+             val 0)))
+(take 5 (p144 3.14 int double))
+(take 5 (p144 3 #(- % 3) #(+ 5 %)))
+(take 12 (p144 0 inc dec inc dec inc))
