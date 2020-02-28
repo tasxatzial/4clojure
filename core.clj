334a2ed29c69797a2b39f48112312a61b77423e5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-29 01:43:21 +0200

    problem 157, solutions 1, 2, 3

--- core.clj
+++ core.clj
@@ -1923,3 +1923,33 @@
 (p156_2 0 [:a :b :c])
 (p156_2 "x" [1 2 3])
 (p156_2 [:a :b] [:foo :bar])
+
+
+;p157: Indexing Sequences
+;Transform a sequence into a sequence of pairs containing the original elements along with their index
+;solution 1
+(def p157 (fn [col]
+            (partition 2 (interleave col (range)))))
+(p157 [:a :b :c])
+(p157 [0 1 3])
+(p157 [[:foo] {:bar :baz}])
+
+;solution 2
+(def p157_2 (fn [col]
+              ((fn [result col idx]
+                 (if (empty? col)
+                   result
+                   (recur (conj result [(first col) idx]) (rest col) (+ idx 1))))
+               [] col 0)))
+(p157_2 [:a :b :c])
+(p157_2 [0 1 3])
+(p157_2 [[:foo] {:bar :baz}])
+
+;solution 3
+(def p157_3 (fn [col]
+              (rest (reduce (fn [result x]
+                              (conj result [x (inc (last (last result)))]))
+                            [[-1]] col))))
+(p157_3 [:a :b :c])
+(p157_3 [0 1 3])
+(p157_3 [[:foo] {:bar :baz}])
