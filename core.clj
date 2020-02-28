66aacec4e1f6604113d0ee4828ff77aa758ac7d6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-02-29 01:05:37 +0200

    problem 156, solutions 1, 2

--- core.clj
+++ core.clj
@@ -1901,3 +1901,25 @@
         #{'+ '* mapcat (comment mapcat)}
         #{(do) set contains? nil?}
         #{, , , #_, , empty?}})
+
+
+;p156: Map Defaults
+;When retrieving values from a map, you can specify default values in case the key is not found:
+;(= 2 (:foo {:bar 0, :baz 1} 2))
+;However, what if you want the map itself to contain the default values? Write a function which takes a default
+;value and a sequence of keys and constructs a map
+;solution 1
+(def p156 (fn [val col]
+            (reduce (fn [result x]
+                      (conj result [x val]))
+                    {} col)))
+(p156 0 [:a :b :c])
+(p156 "x" [1 2 3])
+(p156 [:a :b] [:foo :bar])
+
+;solution 2
+(def p156_2 (fn [val col]
+            (apply hash-map (interleave col (repeat val)))))
+(p156_2 0 [:a :b :c])
+(p156_2 "x" [1 2 3])
+(p156_2 [:a :b] [:foo :bar])
