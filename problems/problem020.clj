edcc43dfd1a669df1ab42d8b893ab9a3ba4cb7bc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-20 19:25:56 +0200

    problem 20: refactoring

--- problems/problem020.clj
+++ problems/problem020.clj
@@ -1,8 +1,9 @@
 ;p20: Penultimate element
 ;Write a function which returns the second to last element from a sequence
-(def p20 #(if (>= (count %) 2)
-            (nth % (- (count %) 2))
-            nil))
-
-;tests
-(map p20 [(list 1 2 3 4 5) ["a" "b" "c"] [[1 2] [3 4]]])
+;
+(defn second-to-last
+  "Returns the second to last element in col."
+  [col]
+  (if (>= (count col) 2)
+    (nth col (- (count col) 2))
+    nil))
