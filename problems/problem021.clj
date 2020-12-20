0807000a1a420e0b1368c8aa9ef9dd930335ccee
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-20 19:27:36 +0200

    problem 21: refactoring

--- problems/problem021.clj
+++ problems/problem021.clj
@@ -3,10 +3,9 @@
 ;
 ;restrictions: nth
 ;
-(def p21 (fn my-nth [col n]
-           (if (= n 0)
-             (first col)
-             (recur (rest col) (- n 1)))))
-
-;tests
-(map p21 ['(4 5 6 7) [:a :b :c] [1 2 3 4] '([1 2] [3 4] [5 6])] [2 0 1 2])
+(defn my-nth
+  "Returns the nth element in col."
+  [col n]
+  (if (= n 0)
+    (first col)
+    (recur (rest col) (- n 1))))
