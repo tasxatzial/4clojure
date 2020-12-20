48bfa8ddac2343ebaa1564fbe376da59390655c7
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-20 19:34:34 +0200

    problem 25: refactoring

--- problems/problem025.clj
+++ problems/problem025.clj
@@ -1,7 +1,7 @@
 ;p25: Find the odd numbers
 ;Write a function which returns only the odd numbers from a sequence
-(def p25 (fn [col]
-           (filter #(= 1 (mod % 2)) col)))
-
-;tests
-(map p25 [#{1 2 3 4 5} [4 2 1 6] [2 2 4 6] [1 1 1 3]])
+;
+(defn find-odd
+  "Returns a col that consists of the odd numbers only."
+  [col]
+  (filter #(= 1 (mod % 2)) col))
