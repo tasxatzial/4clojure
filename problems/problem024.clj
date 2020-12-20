b57111530d721ea5872d0b6865c1cc7c2d2609f0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-20 19:33:05 +0200

    problem 24: refactoring

--- problems/problem024.clj
+++ problems/problem024.clj
@@ -1,7 +1,7 @@
 ;p24: Sum It All Up
 ;Write a function which returns the sum of a sequence of numbers
-(def p24 (fn [x]
-           (apply + x)))
-
-;tests
-(map p24 [[1 2 3] (list 0 -2 5 5) #{4 2 1} '(0 0 -1) '(1 10 3)])
+;
+(defn my-sum
+  "Returns the sum of a sequence of numbers."
+  [col]
+  (apply + col))
