4dae6435387c6b3ea9195964a5289876a050da35
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-20 19:42:37 +0200

    problem 26: refactoring

--- problems/problem026.clj
+++ problems/problem026.clj
@@ -1,11 +1,12 @@
 ;p26: Fibonacci Sequence
 ;Write a function which returns the first X fibonacci numbers
-(def p26 (fn [n]
-           (take n ((fn fib
-                      ([] (fib 1 1))
-                      ([x1 x2]
-                       (let [sum (+ x1 x2)]
-                         (cons x1 (lazy-seq (fib x2 sum))))))))))
-
-;tests
-(map p26 [3 6 8])
+;
+(defn first-N-fib
+  "Returns the first N fibonacci numbers."
+  [N]
+  (letfn [(lazy-fib
+            ([] (lazy-fib 0 1))
+            ([prev next]
+             (let [sum (+ prev next)]
+               (cons prev (lazy-seq (lazy-fib next sum))))))]
+    (take N (lazy-fib))))
