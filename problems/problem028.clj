1713a2388c711701944a4ab36aab4dc5aad31d28
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-20 20:08:36 +0200

    problem 28: refactoring

--- problems/problem028.clj
+++ problems/problem028.clj
@@ -3,13 +3,13 @@
 ;
 ;restrictions: flatten
 ;
-(def p28 (fn my-flatten [col]
-           (reduce (fn [result x]
-                     (concat result
-                             (if (sequential? x)
-                               (my-flatten x)
-                               (list x))))
-                   '() col)))
-
-;tests
-(map p28 ['((1 2) 3 [4 [5 6]]) ["a" ["b"] "c"] '((((:a))))])
+(defn my-flatten
+  "Flattens col."
+  [col]
+  (reduce (fn [result x]
+            (concat result
+                    (if (sequential? x)
+                      (my-flatten x)
+                      (list x))))
+          '()
+          col))
