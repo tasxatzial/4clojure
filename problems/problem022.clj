675e791109626409f37c4e26faded375626a357b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-20 19:29:48 +0200

    problem 22: refactoring

--- problems/problem022.clj
+++ problems/problem022.clj
@@ -3,10 +3,10 @@
 ;
 ;restrictions: count
 ;
-(def p22 (fn [col]
-           (reduce (fn [new-val x]
-                     (inc new-val))
-                   0 col)))
-
-;tests
-(map p22 ['(1 2 3 3 1) "Hello World" [[1 2] [3 4] [5 6]] '(13) '(:a :b :c)])
+(defn my-count
+  "Returns the total number of elements in col."
+  [col]
+  (reduce (fn [total el]
+            (inc total))
+          0
+          col))
