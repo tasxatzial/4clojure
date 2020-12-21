130aa59dff9168a46173ffabddbec7f8aac90c80
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-21 19:59:29 +0200

    problem 29: refactoring/simplified

--- problems/problem029.clj
+++ problems/problem029.clj
@@ -1,33 +1,14 @@
 ;p29: Get the Caps
 ;Write a function which takes a string and returns a new string containing only the capital letters
+;
+;solution 1
+(defn get-caps1
+  "Returns a new string containing only the capital letters."
+  [s]
+  (apply str (filter #(and (>= (int %) (int \A)) (<= (int %) (int \Z))) s)))
 
-;solution 1 ----------------------------------------
-(def p29 (fn [col]
-           (apply str
-                  (filter #(and (re-find #"[A-Z]" %) (= (clojure.string/upper-case %) %))
-                          (clojure.string/split col, #"")))))
-
-;solution 1 tests
-(map p29 ["HeLlO, WoRlD!" "nothing" "$#A(*&987Zf"])
-
-;solution 2 ----------------------------------------
-(def p29_2 (fn [col]
-             (apply str
-                    (let [matcher (re-matcher #"[A-Z]" col)]
-                      ((fn find-all [has-next result]
-                         (if has-next
-                           (let [match (re-find matcher)]
-                             (recur match (concat result [has-next])))
-                           result))
-                       (re-find matcher) '())))))
-
-;solution 2 tests
-(map p29_2 ["HeLlO, WoRlD!" "nothing" "$#A(*&987Zf"])
-
-;solution 3 ----------------------------------------
-(def p29_3 (fn [col]
-             (apply str
-                    (re-seq #"[A-Z]" col))))
-
-;solution 3 tests
-(map p29_3 ["HeLlO, WoRlD!" "nothing" "$#A(*&987Zf"])
+;solution 2
+(defn get-caps2
+  "Returns a new string containing only the capital letters."
+  [s]
+  (apply str (re-seq #"[A-Z]" s)))
