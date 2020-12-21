4570bfc0eda7964154c5c141153db6411d16613e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-21 20:30:37 +0200

    problem 33: refactoring/new solution

--- problems/problem033.clj
+++ problems/problem033.clj
@@ -1,13 +1,17 @@
 ;p33: Replicate a Sequence
 ;Write a function which replicates each element of a sequence a variable number of times
-(def p33 (fn [col N]
-           (reduce (fn [result x]
-                     (concat result (take N (repeat x))))
-                   '() col)))
 
-;tests
-(p33 [1 2 3] 2)
-(p33 [:a :b] 4)
-(p33 [4 5 6] 1)
-(p33 [[1 2] [3 4]] 2)
-(p33 [44 33] 2)
+;solution 1
+(defn my-replicate1
+  "Replicates each element of a sequence a variable number of times."
+  [col N]
+  (reduce (fn [result x]
+            (concat result (take N (repeat x))))
+          '()
+          col))
+
+;solution 2
+(defn my-replicate2
+  "Replicates each element of a sequence a variable number of times."
+  [col N]
+  (apply concat (map #(take N (repeat %)) col)))
