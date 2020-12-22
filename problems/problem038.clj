4ee5b750d79799881e04ab70c38a537e99efc6bd
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-22 06:36:40 +0200

    problem 34: refactoring/keep simplest solution

--- problems/problem038.clj
+++ problems/problem038.clj
@@ -3,28 +3,12 @@
 ;
 ;restrictions: max, max-key
 
-;solution 1 ----------------------------------------
-(def p38 (fn [& col]
-           (if (empty? col)
-             nil
-             ((fn my-max [x col]
-                (if (empty? col)
-                  x
-                  (if (> x (first col))
-                    (recur x (rest col))
-                    (recur (first col) (rest col)))))
-              (first col) (rest col)))))
-
-;solution 1 tests
-(map #(apply p38 %) [[1 8 3 4] [30 20] [45 67 11]])
-
-;solution2 ----------------------------------------
-(def p38_2 (fn [& col]
-             (reduce (fn [max_ x]
-                       (if (> x max_)
-                         x
-                         max_))
-                     (first col) (rest col))))
-
-;solution 2 tests
-(map #(apply p38_2 %) [[1 8 3 4] [30 20] [45 67 11]])
+(defn my-max
+  "Returns the maximum value."
+  [& col]
+  (reduce (fn [max x]
+            (if (> x max)
+              x
+              max))
+          (first col)
+          (rest col)))
