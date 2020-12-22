debf8ac3dd71d3fcc569da077bbfaf08f6a9ccd0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-22 06:39:00 +0200

    problem 39: refactoring

--- problems/problem039.clj
+++ problems/problem039.clj
@@ -4,12 +4,7 @@
 ;
 ;restrictions: interleave
 ;
-(def p39 (fn [col1 col2]
-           (reduce concat (map (fn [x1 x2]
-                                 [x1 x2]) col1 col2))))
-
-;tests
-(p39 [1 2 3] [:a :b :c])
-(p39 [1 2] [3 4 5 6])
-(p39 [1 2 3 4] [5])
-(p39 [30 20] [25 15])
+(defn my-interleave
+  "Interleaves two sequences."
+  [col1 col2]
+  (reduce concat (map #(vector %1 %2) col1 col2)))
