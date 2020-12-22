69e99d7347f57116e6133848af132ce15b890110
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-22 07:33:17 +0200

    problem 44: refactoring

--- problems/problem044.clj
+++ problems/problem044.clj
@@ -1,12 +1,12 @@
 ;p44: Rotate Sequence
 ;Write a function which can rotate a sequence in either direction
-(def p44 (fn [N col]
-           (let [N (mod N (count col))]
-             (concat (drop N col) (take N col)))))
-
-;tests
-(p44 6 [1 2 3 4 5])
-(p44 -2 [1 2 3 4 5])
-(p44 6 [1 2 3 4 5])
-(p44 1 '(:a :b :c))
-(p44 -4 '(:a :b :c))
+;
+;example: (f 2 [1 2 3 4 5]) -> '(3 4 5 1 2)
+;example: (f -2 [1 2 3 4 5]) -> '(4 5 1 2 3)
+;example: (f 6 [1 2 3 4 5]) -> '(2 3 4 5 1)
+;
+(defn rotate
+  "Rotates a sequence in either direction."
+  [N col]
+  (let [N (mod N (count col))]
+    (concat (drop N col) (take N col))))
