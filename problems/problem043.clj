9517e775262fca990294b093cb3199b907faa8e0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-22 07:25:48 +0200

    problem 43: refactoring/keep simplest solutions

--- problems/problem043.clj
+++ problems/problem043.clj
@@ -1,50 +1,19 @@
 ;p43: Reverse Interleave
 ;Write a function which reverses the interleave process into x number of subsequences
+;
+;Assume that the total number of elements in the sequence is a multiple of x
+;
+;example: (f [1 2 3 4 5 6] 2) -> '((1 3 5) (2 4 6))
+;example: (f (range 9) 3) -> '((0 3 6) (1 4 7) (2 5 8))
 
-;solution 1 ----------------------------------------
-(def p43 (fn [col N]
-           (let [T (/ (count col) N)]
-             ((fn my-rinterleave [result col]
-                (if (empty? col)
-                  result
-                  (recur (concat result (list (take T col))) (nthrest col T))))
-              '() (apply interleave (partition N col))))
-           ))
+;solution 1
+(defn rev-interleave1
+  [col N]
+  (let [partitioned (partition N col)
+        interleaved (apply interleave partitioned)]
+    (partition (count partitioned) interleaved)))
 
-;solution 1 tests
-(p43 [1 2 3 4 5 6] 2)
-(p43 (range 9) 3)
-(p43 (range 10) 5)
-
-;solution 2 ----------------------------------------
-(def p43_2 (fn [col N]
-             (let [col_new (partition N col) count_col (count (first col_new))]
-               ((fn my-rinterleave [result C]
-                  (if (= count_col C)
-                    result
-                    (recur (concat result (list (map #(nth % C) col_new)) ) (+ C 1))))
-                '() 0))))
-
-;solution 2 tests
-(p43_2 [1 2 3 4 5 6] 2)
-(p43_2 (range 9) 3)
-(p43_2 (range 10) 5)
-
-;solution 3 ----------------------------------------
-(def p43_3 (fn [col N]
-             (let [partitioned (partition N col) interleaved (apply interleave partitioned)]
-               (partition (count partitioned) interleaved))))
-
-;solution 3 tests
-(p43_3 [1 2 3 4 5 6] 2)
-(p43_3 (range 9) 3)
-(p43_3 (range 10) 5)
-
-;solution 4 ----------------------------------------
-(def p43_4 (fn [col N]
-             (apply map list (partition N col))))
-
-;solution 4 tests
-(p43_4 [1 2 3 4 5 6] 2)
-(p43_4 (range 9) 3)
-(p43_4 (range 10) 5)
+;solution 2
+(defn rev-interleave2
+  [col N]
+  (apply map list (partition N col)))
