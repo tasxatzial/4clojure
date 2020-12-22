787f7753c7511599e05be8097291b3cbe98617a7
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-22 07:15:17 +0200

    problem 42: refactoring/simplified

--- problems/problem042.clj
+++ problems/problem042.clj
@@ -1,36 +1,17 @@
 ;p42: Factorial Fun
 ;Write a function which calculates factorials
 
-;solution 1 ----------------------------------------
-(def p42 (fn [N]
-           ((fn my-factorial [N result]
-              (if (= N 0)
-                result
-                (recur (- N 1) (* result N))))
-            N 1)))
+;solution 1
+(defn factorial1
+  "Returns the factorial of N."
+  [N]
+  (reduce * (range 1 (inc N))))
 
-;solution 1 tests
-(map p42 [1 3 5 8])
-
-;solution 2 ----------------------------------------
-(def p42_2 (fn my-recursion [N]
-             (if (= N 0)
-               1
-               (* N (my-recursion (- N 1))))))
-
-;solution 2 tests
-(map p42_2 [1 3 5 8])
-
-;solution 3 ----------------------------------------
-(def p42_3 #(last (last (take (inc %) (iterate (fn [[x y]]
-                                                 [(inc x) (* x y)]) [1 1])))))
-
-;solution 3 tests
-(map p42_3 [1 3 5 8])
-
-;solution 4 ----------------------------------------
-(def p42_4 (fn [N]
-             (reduce * (range 1 (inc N)))))
-
-;solution 4 tests
-(map p42_4 [1 3 5 8])
+;solution 2
+(defn factorial2
+  "Returns the factorial of N."
+  ([N] (factorial2 N 1))
+  ([N result]
+   (if (= N 1)
+     result
+     (recur (dec N) (* result N)))))
