56fd33032d01b60701c65105fef8a47d157fe65b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-22 07:09:32 +0200

    problem 41: refactoring

--- problems/problem041.clj
+++ problems/problem041.clj
@@ -1,15 +1,13 @@
 ;p41: Drop Every Nth Item
 ;Write a function which drops every Nth item from a sequence
-(def p41 (fn [col N]
-           ((fn my-dropNth [result idx]
-              (if (= idx (count col))
-                result
-                (if (= 0 (mod (+ idx 1) N))
-                  (recur result (+ idx 1))
-                  (recur (concat result (list (nth col idx))) (+ idx 1)))))
-            '() 0)))
-
-;tests
-(p41 [1 2 3 4 5 6 7 8] 3)
-(p41 [:a :b :c :d :e :f] 2)
-(p41 [1 2 3 4 5 6] 4)
+;
+(defn drop-nth
+  "Drops every Nth item from a sequence."
+  ([col N] (drop-nth col N 0 []))
+  ([col N count result]
+   (if (empty? col)
+     result
+     (let [new-result (if (= 0 (mod (inc count) N))
+                        result
+                        (conj result (first col)))]
+       (recur (rest col) N (inc count) new-result)))))
