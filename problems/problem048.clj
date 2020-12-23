3c46f097e9d8fa1ab898b97bb5e60f8ff0ada3d4
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-23 23:09:14 +0200

    problem 48: delete (too simple)

--- problems/problem048.clj
+++
@@ -1,8 +0,0 @@
-;p48: Intro to some
-;The some function takes a predicate function and a collection.
-;It returns the first logical true value of (predicate x) where x is an item in the collection
-;
-;restrictions: split-at
-;
-(= 6 (some #{2 7 6} [5 6 7 8]))                            ;6
-(= 6 (some #(when (even? %) %) [5 6 7 8]))                 ;6
