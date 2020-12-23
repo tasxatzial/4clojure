82e843f1fc9e324d2dc78c0e906e24b9cf4f5f9e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-23 23:13:28 +0200

    problem 49: refactoring

--- problems/problem049.clj
+++ problems/problem049.clj
@@ -1,9 +1,8 @@
 ;p49: Split a sequence
-;Write a function which will split a sequence into two parts
-(def p49 (fn [N col]
-           [(apply vector (take N col)) (apply vector (drop N col))]))
-
-;tests
-(p49 3 [1 2 3 4 5 6])
-(p49 1 [:a :b :c :d])
-(p49 2 [[1 2] [3 4] [5 6]])
+;Write a function which will split a sequence into two parts.
+;
+;restrictions: split-at
+(defn my-split-at
+  "Splits col into two parts, first part has exactly N elements."
+  [N col]
+  (vector (take N col) (drop N col)))
