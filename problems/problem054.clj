b826b1a166bac9f3fa377a12257d0e0a4d9a8734
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-24 01:22:35 +0200

    problem 54: refactoring, reduce #solutions

--- problems/problem054.clj
+++ problems/problem054.clj
@@ -1,38 +1,25 @@
 ;p54: Partition a Sequence
-;Write a function which returns a sequence of lists of x items each. Lists of less than x items should not be returned
+;Write a function which returns a sequence of lists of x items each.
+;Lists of less than x items should not be returned
 ;
 ;restrictions: partition, partition-all
 
-;solution 1 ----------------------------------------
-(def p54 (fn my-partition [N col]
-           (take (Math/floor (/ (count col) N))  (concat (list (take N col)) (lazy-seq (my-partition N (drop N col)))))))
+;solution 1: lazy
+(defn my-partition1
+  "Returns a sequence of lists of N items each."
+  ([N col] (my-partition1 N col (Math/floor (/ (count col) N))))
+  ([N col subseqs-count]
+   (let [rest-subseqs (lazy-seq (my-partition1 N (drop N col) subseqs-count))]
+     (take subseqs-count (concat (list (take N col)) rest-subseqs)))))
 
-;solution 1 tests
-(p54 3 (range 9))
-(p54 2 (range 8))
-(p54 3 (range 8))
-
-;solution 2 ----------------------------------------
-(def p54_2 (fn [N col]
-             ((fn my-partition [result col]
-                (if (< (count col) N)
-                  result
-                  (recur (concat result (list (take N col))) (drop N col))))
-              '() col)))
-
-;solution 2 tests
-(p54_2 3 (range 9))
-(p54_2 2 (range 8))
-(p54_2 3 (range 8))
-
-;solution 3 ----------------------------------------
-(def p54_3 (fn [N col]
-             (concat [] ((fn my-partition [col]             ;concat necessary if we pass empty seq
-                           (when (>= (count col) N)
-                             (cons (take N col) (my-partition (drop N col)))))
-                         col))))
-
-;solution 3 tests
-(p54_3 3 (range 9))
-(p54_3 2 (range 8))
-(p54_3 3 (range 8))
+;solution 2: recursion
+(defn my-partition2
+  "Returns a sequence of lists of N items each."
+  [N col]
+  ((fn my-partition
+     [result col]
+     (if (< (count col) N)
+       result
+       (recur (concat result (list (take N col))) (drop N col))))
+   '()
+   col))
