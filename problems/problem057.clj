e099d8dfc0a742595097c5c47f082eac076a2038
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-24 01:48:21 +0200

    problem 57: delete (too simple)

--- problems/problem057.clj
+++
@@ -1,4 +0,0 @@
-;p57: Simple Recursion
-;A recursive function is a function which calls itself.
-;This is one of the fundamental techniques used in functional programming
-(= [5 4 3 2 1] ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5)) ;[5 4 3 2 1]
