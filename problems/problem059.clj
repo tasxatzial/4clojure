5a4705eb753d7cc256b66faa8e36ca067d2d4357
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-24 01:55:54 +0200

    problem 59: refactoring

--- problems/problem059.clj
+++ problems/problem059.clj
@@ -1,14 +1,16 @@
 ;p59: Juxtaposition
-;Take a set of functions and return a new function that takes a variable number of arguments and returns a sequence
-;containing the result of applying each function left-to-right to the argument list
+;Take a set of functions and return a new function that takes a variable number of
+;arguments and returns a sequence containing the result of applying each function
+;left-to-right to the argument list
 ;
 ;restrictions: juxt
-;
-(def p59 (fn [& f-args]
-           (fn [& args]
-             (map #(apply % args) f-args))))
 
-;tests
-((p59 + max min) 2 3 5 1 6 4)
-((p59 #(.toUpperCase %) count) "hello")
-((p59 :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10})
+(defn my-juxt
+  "Takes a set of functions and returns a fn that is the juxtaposition of those fns."
+  [& f-args]
+  (fn [& args]
+    (map #(apply % args) f-args)))
+
+(= [21 6 1] ((my-juxt + max min) 2 3 5 1 6 4))
+(= ["HELLO" 5] ((my-juxt #(.toUpperCase %) count) "hello"))
+(= [2 6 4] ((my-juxt :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))
