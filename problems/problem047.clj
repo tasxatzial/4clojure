baf81a756bf437cef1798071de7e0d48319b58cc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-23 23:08:15 +0200

    problem 47: delete (too simple)

--- problems/problem047.clj
+++
@@ -1,7 +0,0 @@
-;p47: Contain Yourself
-;The contains? function checks if a KEY is present in a given collection.
-;This often leads beginner clojurians to use it incorrectly with numerically indexed collections like vectors and lists
-(contains? #{4 5 6} 4)                                      ;true; 4
-(contains? [1 1 1 1 1] 4)                                   ;true, 4th index
-(contains? {4 :a 2 :b} 4)                                   ;true, 4
-(not (contains? [1 2 4] 4))                                 ;true, 4th index
