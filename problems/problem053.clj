f9f4ba1d598aaaabb6cb0c876822834bfd10ac61
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-24 01:02:17 +0200

    problem 53: refactoring

--- problems/problem053.clj
+++ problems/problem053.clj
@@ -2,22 +2,28 @@
 ;Given a vector of integers, find the longest consecutive sub-sequence of increasing numbers.
 ;If two sub-sequences have the same length, use the one that occurs first.
 ;An increasing sub-sequence must have a length of 2 or greater to qualify
-(def p53 (fn [col]
-           (reduce (fn [result x]
-                     (if (> (count x) (count result))
-                       x
-                       result))
-                   [] (filter #(> (count %) 1)
-                              (reduce (fn [result x]
-                                        (if (empty? result)
-                                          (conj result [x])
-                                          (if (> x (last (last result)))
-                                            (conj (apply vector (butlast result)) (conj (apply vector (last result)) x))
-                                            (conj result [x]))))
-                                      [] col)))))
+;
+(defn split-increasing
+   "Splits col into increasing sub sequences."
+   [col]
+   (reduce (fn [result x]
+             (if (empty? result)
+               (conj result [x])
+               (if (> x (last (last result)))
+                 (let [butlast-result (apply vector (butlast result))
+                       last-result (conj (apply vector (last result)) x)]
+                   (conj butlast-result last-result))
+                 (conj result [x]))))
+           []
+           col))
 
-;tests
-(p53 [1 0 1 2 3 0 4 5])
-(p53 [5 6 1 3 2 7])
-(p53 [2 3 3 4 5])
-(p53 [7 6 5 4])
+(defn longest-increasing
+  "Returns the longest increasing sub sequence."
+  [col]
+  (let [split-col (filter #(> (count %) 1) (split-increasing col))]
+    (reduce (fn [result x]
+              (if (> (count x) (count result))
+                x
+                result))
+            []
+            split-col)))
