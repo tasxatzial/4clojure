37b4fe30cb2248bb17da476f4b1d4e1db6f41e31
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-23 23:31:44 +0200

    problem 50: refactoring, keep one solution

--- problems/problem050.clj
+++ problems/problem050.clj
@@ -3,24 +3,15 @@
 ;homogeneous sub-sequences. The internal order of each sub-sequence should be maintained, but the sub-sequences
 ;themselves can be returned in any order (this is why 'set' is used in the test cases)
 
-;solution 1 ----------------------------------------
-(def p50 (fn [col]
-           (map #(apply vector %)
-                (filter #(not-empty %)
-                        (map #(filter % col) [keyword? number? string? vector?])))))
+(defn type-split
+  "Splits col by type."
+   [col]
+  (->>
+   [keyword? number? string? vector?]
+   (map #(filter % col))
+   (filter not-empty)
+   (map #(apply vector %))))
 
-;solution 1 tests
-(set (p50 [1 2 3 :a]))
-(set (p50 [:a "foo"  "bar" :b]))
-(set (p50 [[1 2] :a [3 4] 5 6 :b]))
-
-;solution 2 ----------------------------------------
-(def p50_2 (fn [col]
-             (filter #(not (nil? %))
-                     (map #(% true)
-                          (map #(group-by % col) [keyword? number? string? vector?])))))
-
-;solution 2 tests
-(set (p50_2 [1 2 3 :a]))
-(set (p50_2 [:a "foo"  "bar" :b]))
-(set (p50_2 [[1 2] :a [3 4] 5 6 :b]))
+(= (set (type-split [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
+(= (set (type-split [:a "foo"  "bar" :b])) #{[:a :b] ["foo" "bar"]})
+(= (set (type-split [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]})
