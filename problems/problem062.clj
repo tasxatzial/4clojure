95994ab28aff2d51a10783dd6d62996c57f11b36
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-24 02:21:58 +0200

    problem 62: refactoring

--- problems/problem062.clj
+++ problems/problem062.clj
@@ -1,13 +1,14 @@
 ;p62: Re-implement Iterate
-;Given a side-effect free function f and an initial value x write a function which returns an infinite
-;lazy sequence of x, (f x), (f (f x)), (f (f (f x))), etc
+;Given a side-effect free function f and an initial value x write a function which
+;returns an infinite lazy sequence of x, (f x), (f (f x)), (f (f (f x))), etc
 ;
 ;restrictions: iterate
 ;
-(def p62 (fn my-iterate [func i]
-           (cons i (lazy-seq (my-iterate func (func i))))))
+(defn my-iterate
+  "Returns a lazy sequence of x, (f x), (f (f x)) etc. f must be free of side-effects."
+  [func i]
+  (cons i (lazy-seq (my-iterate func (func i)))))
 
-;tests
-(take 5 (p62 #(* 2 %) 1))
-(take 100 (p62 inc 0))
-(take 9 (p62 #(inc (mod % 3)) 1))
+(= (take 5 (my-iterate #(* 2 %) 1)) [1 2 4 8 16])
+(= (take 100 (my-iterate inc 0)) (take 100 (range)))
+(= (take 9 (my-iterate #(inc (mod % 3)) 1)) (take 9 (cycle [1 2 3])))
