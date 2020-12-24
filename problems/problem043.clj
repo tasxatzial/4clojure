9e5d2a82a7b55a35ccddc011dc26e756ff677466
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-24 02:06:28 +0200

    problem 43: add test cases

--- problems/problem043.clj
+++ problems/problem043.clj
@@ -3,11 +3,10 @@
 ;
 ;Assume that the total number of elements in the sequence is a multiple of x
 ;
-;example: (f [1 2 3 4 5 6] 2) -> '((1 3 5) (2 4 6))
-;example: (f (range 9) 3) -> '((0 3 6) (1 4 7) (2 5 8))
 
 ;solution 1
 (defn rev-interleave1
+  "Reverses the interleave process into x number of subsequences."
   [col N]
   (let [partitioned (partition N col)
         interleaved (apply interleave partitioned)]
@@ -15,5 +14,10 @@
 
 ;solution 2
 (defn rev-interleave2
+  "Reverses the interleave process into x number of subsequences."
   [col N]
   (apply map list (partition N col)))
+
+(= (rev-interleave1 [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
+(= (rev-interleave1 (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
+(= (rev-interleave1 (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
