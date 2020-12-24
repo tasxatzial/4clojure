84e8cff2ed2fb9cd458d90a74b1f03c2ddc789f3
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-24 02:04:09 +0200

    problem 39: add test cases

--- problems/problem039.clj
+++ problems/problem039.clj
@@ -8,3 +8,6 @@
   "Interleaves two sequences."
   [col1 col2]
   (reduce concat (map #(vector %1 %2) col1 col2)))
+
+(= (my-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+(= (my-interleave [1 2] [3 4 5 6]) '(1 3 2 4))
