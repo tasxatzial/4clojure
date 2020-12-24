edcb185dea0d3c8f54ccbfb174f760442ed29e2b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-24 02:07:42 +0200

    problem 44: add test cases

--- problems/problem044.clj
+++ problems/problem044.clj
@@ -1,12 +1,12 @@
 ;p44: Rotate Sequence
 ;Write a function which can rotate a sequence in either direction
 ;
-;example: (f 2 [1 2 3 4 5]) -> '(3 4 5 1 2)
-;example: (f -2 [1 2 3 4 5]) -> '(4 5 1 2 3)
-;example: (f 6 [1 2 3 4 5]) -> '(2 3 4 5 1)
-;
 (defn rotate
   "Rotates a sequence in either direction."
   [N col]
   (let [N (mod N (count col))]
     (concat (drop N col) (take N col))))
+
+(= (rotate 2 [1 2 3 4 5]) '(3 4 5 1 2))
+(= (rotate -2 [1 2 3 4 5]) '(4 5 1 2 3))
+(= (rotate 6 [1 2 3 4 5]) '(2 3 4 5 1))
