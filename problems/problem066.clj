1179bdf8b9aa7c0a6f9249f036ab8abc6ee2e168
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 01:46:39 +0200

    problem 66: refactoring

--- problems/problem066.clj
+++ problems/problem066.clj
@@ -1,12 +1,9 @@
 ;p66: Greatest Common Divisor
 ;Given two integers, write a function which returns the greatest common divisor
-(def p66 (fn my-gcd [N1 N2]
-           (if (= 0 N2)
-             N1
-             (my-gcd N2 (mod N1 N2)))))
-
-;tests
-(p66 2 4)
-(p66 10 5)
-(p66 5 7)
-(p66 1023 858)
+;
+(defn my-gcd
+  "Returns the GCD of N1, N2."
+  [N1 N2]
+  (if (= 0 N2)
+    N1
+    (my-gcd N2 (mod N1 N2))))
