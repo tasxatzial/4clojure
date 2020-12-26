baefdeababe471df44e14e9b9aa7b16947f41177
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 01:56:45 +0200

    problem 67: refactoring

--- problems/problem067.clj
+++ problems/problem067.clj
@@ -1,15 +1,12 @@
 ;p67: Prime Numbers
 ;Write a function which returns the first x number of prime numbers
-(def p67 (fn my-primes
-           ([x] (my-primes x [] 2))
-           ([x result N]
-            (if (= x 0)
-              result
-              (if (contains? (set (map #(mod N %) result)) 0)
-                (my-primes x result (+ N 1))
-                (my-primes (- x 1) (conj result N) (+ N 1)))))))
-
-;tests
-(p67 2)
-(p67 5)
-(last (p67 100))
+;
+(defn my-primes
+  "Returns the first x prime numbers."
+  ([x] (my-primes x [] 2))
+  ([x result N]
+   (if (= x 0)
+     result
+     (if (contains? (set (map #(mod N %) result)) 0)
+       (recur x result (inc N)) ; N is composite
+       (recur (dec x) (conj result N) (inc N)))))) ; N is prime
