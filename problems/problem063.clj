dbc78277908ef19a2facc4a5f8d4c595272af710
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-26 23:11:51 +0200

    problem 63: refactoring

--- problems/problem063.clj
+++ problems/problem063.clj
@@ -5,16 +5,27 @@
 ;
 ;restrictions: group-by
 ;
-(def p63 (fn [func col]
-            (reduce (fn [result x]
-                      (if (contains? result (x 1))
-                        (into result {(x 1) (conj (result (x 1)) (x 0))} )
-                        (into result {(x 1) [(x 0)]})))
-                    {} (into [] (map (fn [x]
-                                       [x, (func x)])
-                                     col)))))
+(defn map-f
+  "Maps func to each item in col and returns [x, func(x)] for each item in col."
+  [func col]
+  (into [] (map #(vector % (func %)) col)))
 
-;tests
-(p63 #(> % 5) [1 3 6 8])
-(p63 #(apply / %) [[1 2] [2 4] [4 6] [3 6]])
-(p63 count [[1] [1 2] [3] [1 2 3] [2 3]])
+(defn my-group-by
+  "Returns a map of the elements of col keyed by the result of
+  func on each element. The value at each key will be a vector of the
+  corresponding elements, in the order they appeared in col."
+  [func col]
+  (reduce (fn [result x]
+            (let [func-el (get x 1)
+                  el (get x 0)]
+              (if (contains? result func-el)
+                (assoc result func-el (conj (result func-el) el))
+                (assoc result func-el [el]))))
+          {}
+          (map-f func col)))
+
+(= (my-group-by #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})
+(= (my-group-by #(apply / %) [[1 2] [2 4] [4 6] [3 6]])
+   {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})
+(= (my-group-by count [[1] [1 2] [3] [1 2 3] [2 3]])
+   {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]})
