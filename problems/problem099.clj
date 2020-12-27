6ae699e8996a0fe35c39f40d834bbf6eddd0e7b0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 13:30:54 +0200

    problem 99: refactoring

--- problems/problem099.clj
+++ problems/problem099.clj
@@ -1,9 +1,11 @@
 ;p99: Product Digits
 ;Write a function which multiplies two numbers and returns the result as a sequence of its digits
-(def p99 (fn [x y]
-           (map (comp read-string str) (str (* x y)))))
+;
+(defn p99
+  "Multiplies two numbers and returns the result as a sequence of its digits."
+  [x y]
+  (map (comp read-string str) (str (* x y))))
 
-;tests
-(p99 1 1)
-(p99 99 9)
-(p99 999 99)
+(= (p99 1 1) [1])
+(= (p99 99 9) [8 9 1])
+(= (p99 999 99) [9 8 9 0 1])
