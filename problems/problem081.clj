81146137ffacd06ee7dc42d47af02a9ba6c7d305
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 06:57:46 +0200

    problem 81: refactoring

--- problems/problem081.clj
+++ problems/problem081.clj
@@ -4,28 +4,12 @@
 ;
 ;restrictions: intersection
 
-;solution 1 ----------------------------------------
-(def p81 (fn [col1 col2]
-           (reduce (fn [result x]
-                     (if (and (contains? col1 x) (contains? col2 x))
-                       (conj result x)
-                       result))
-                   #{} (into col1 col2))))
-
-;solution 1 tests
-(p81 #{0 1 2 3} #{2 3 4 5})
-(p81 #{0 1 2} #{3 4 5})
-(p81 #{:a :b :c :d} #{:c :e :a :f :d})
-
-;solution2 ----------------------------------------
-(def p81_2 (fn [col1 col2]
-             (reduce (fn [result x]
-                       (if (contains? col2 x)
-                         (conj result x)
-                         result))
-                     #{} col1)))
-
-;solution 2 tests
-(p81_2 #{0 1 2 3} #{2 3 4 5})
-(p81_2 #{0 1 2} #{3 4 5})
-(p81_2 #{:a :b :c :d} #{:c :e :a :f :d})
+(defn my-intersection
+  "Returns the intersection of two sets."
+  [col1 col2]
+  (reduce (fn [result x]
+            (if (contains? col2 x)
+              (conj result x)
+              result))
+          #{}
+          col1))
