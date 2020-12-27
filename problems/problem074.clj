8dc98d385d1774674a797d674861844d35133be7
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 03:43:44 +0200

    problem 74: refactoring

--- problems/problem074.clj
+++ problems/problem074.clj
@@ -1,11 +1,22 @@
 ;p74: Filter Perfect Squares
-;Given a string of comma separated integers, write a function which returns a new comma separated string that only
-;contains the numbers which are perfect squares
-(def p74 (fn [string]
-           (clojure.string/join "," (filter #(< (- (Math/sqrt %) (Math/floor (Math/sqrt %))) 0.001)
-                                            (map #(read-string %)
-                                                 (clojure.string/split string #","))))))
+;Given a string of comma separated integers, write a function which returns a new comma
+;separated string that only contains the numbers which are perfect squares
+;
+(defn square?
+  "Returns true if x is square, false otherwise."
+  [x]
+  (let [sqrt (Math/sqrt x)
+        rounded-sqrt (Math/round sqrt)
+        diff (Math/abs (- rounded-sqrt sqrt))]
+    (< diff (Math/ulp diff))))
 
-;tests
-(p74 "4,5,6,7,8,9")
-(p74 "15,16,25,36,37")
+(defn filter-squares
+  "Given a string s of comma separated integers, it returns a new comma separated string
+  that only contains the numbers which are perfect squares."
+  [s]
+  (let [split-s (clojure.string/split s #",")
+        ints (map #(Integer. ^String %) split-s)]
+    (clojure.string/join "," (filter square? ints))))
+
+(= (filter-squares "4,5,6,7,8,9") "4,9")
+(= (filter-squares "15,16,25,36,37") "16,25,36")
