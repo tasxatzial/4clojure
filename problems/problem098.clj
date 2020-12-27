2af7903fc1c3b9cd21a90849244ef1addb5edc1c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 13:27:57 +0200

    problem 98: refactoring

--- problems/problem098.clj
+++ problems/problem098.clj
@@ -1,14 +1,21 @@
 ;p98: Equivalence Classes
-;A function f defined on a domain D induces an equivalence relation on D, as follows: a is equivalent to b with
-;respect to f if and only if (f a) is equal to (f b). Write a function with arguments f and D that computes the
-;equivalence classes of D with respect to f
-(def p98 (fn [func col]
-           (reduce (fn [result x]
-                     (conj result (set (x 1))))
-                   #{} (group-by func col))))
+;A function f defined on a domain D induces an equivalence relation on D, as follows:
+;a is equivalent to b with respect to f if and only if (f a) is equal to (f b). Write a
+;function with arguments f and D that computes the equivalence classes of D with respect to f
+;
+(defn group-by-equivalence
+  "Computes the equivalence classes of col wtih respect to func."
+  [func col]
+  (reduce (fn [result x]
+            (conj result (set (get x 1))))
+          #{}
+          (group-by func col)))
 
-;tests
-(p98 #(* % %) #{-2 -1 0 1 2})
-(p98 #(rem % 3) #{0 1 2 3 4 5})
-(p98 identity #{0 1 2 3 4})
-(p98 (constantly true) #{0 1 2 3 4})
+(= (group-by-equivalence #(* % %) #{-2 -1 0 1 2})
+   #{#{0} #{1 -1} #{2 -2}})
+(= (group-by-equivalence #(rem % 3) #{0 1 2 3 4 5})
+   #{#{0 3} #{1 4} #{2 5}})
+(= (group-by-equivalence identity #{0 1 2 3 4})
+   #{#{0} #{1} #{2} #{3} #{4}})
+(= (group-by-equivalence (constantly true) #{0 1 2 3 4})
+   #{#{0 1 2 3 4}})
