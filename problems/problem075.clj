235b21bd9be4f0bac31c1ae5bf9c46f01aade861
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 03:50:12 +0200

    problem 75: refactoring

--- problems/problem075.clj
+++ problems/problem075.clj
@@ -1,18 +1,20 @@
 ;p75: Euler's Totient Function
-;Two numbers are coprime if their greatest common divisor equals 1. Euler's totient function f(x) is defined as the
-;number of positive integers less than x which are coprime to x. The special case f(1) equals 1. Write a function
-;which calculates Euler's totient function
-(def p75 (fn [N]
-           (if (= 1 N )
-             1
-             (count (filter #(= 1 %)
-                            (map (fn my-gcd [N1 N2]
-                                   (if (= 0 N2)
-                                     N1
-                                     (my-gcd N2 (mod N1 N2)))) (range 1 N) (repeat N)))))))
+;Two numbers are coprime if their greatest common divisor equals 1. Euler's totient function f(x)
+;is defined as the number of positive integers less than x which are coprime to x. The special
+;case f(1) equals 1. Write a function which calculates Euler's totient function
+;
+(defn my-gcd
+  "Returns the GCD of N1, N2."
+  [N1 N2]
+  (if (= 0 N2)
+    N1
+    (my-gcd N2 (mod N1 N2))))
 
-;tests
-(p75 1)
-(p75 10)
-(p75 40)
-(p75 99)
+(defn totient
+  "Calculates the number of positive integers less than N which are coprime to N.
+   The special case totient(1) equals 1."
+  [N]
+  (if (= 1 N)
+    1
+    (let [gcds (map my-gcd (range 1 N) (repeat N))]
+      (count (filter #(= 1 %) gcds)))))
