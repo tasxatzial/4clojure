d69c88e0334d935fb0d50c46eea224f0b1709104
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 08:00:47 +0200

    problem 85: refactoring

--- problems/problem085.clj
+++ problems/problem085.clj
@@ -1,17 +1,26 @@
 ;p85: Power Set
-;Write a function which generates the power set of a given set. The power set of a set x is the set of all
-;subsets of x, including the empty set and x itself
-(def p85 (fn [col]
-           (conj (reduce (fn [result x]
-                           (into result (conj (reduce (fn [result2 x2]
-                                                        (conj result2 (conj x2 x)))
-                                                      #{} result)
-                                              #{x})))
-                         #{} col)
-                 #{})))
+;Write a function which generates the power set of a given set. The power set of a
+;set x is the set of all subsets of x, including the empty set and x itself
+;
+(defn set-add
+  "Adds el to every set in col."
+  [col el]
+  (reduce (fn [result x]
+            (conj result (conj x el)))
+          #{}
+          col))
 
-;tests
-(p85 #{1 :a})
-(p85 #{})
-(p85 #{1 2 3})
-(= (count (p85 (into #{} (range 10)))) 1024)
+(defn power-set
+  "Returns the power set of set."
+  [set]
+  (let [power-set0 (reduce (fn [result x]
+                             (into result (conj (set-add result x) #{x})))
+                           #{}
+                           set)]
+    (conj power-set0 #{})))
+
+(= (power-set #{1 :a}) #{#{1 :a} #{:a} #{} #{1}})
+(= (power-set #{}) #{#{}})
+(= (power-set #{1 2 3})
+   #{#{} #{1} #{2} #{3} #{1 2} #{1 3} #{2 3} #{1 2 3}})
+(= (count (power-set (into #{} (range 10)))) 1024)
