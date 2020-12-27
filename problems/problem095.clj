1a68df50854ff6e61f42a3b924d88add403c37a7
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 10:28:01 +0200

    problem 95: refactoring

--- problems/problem095.clj
+++ problems/problem095.clj
@@ -1,24 +1,31 @@
 ;p95: To Tree, or not to Tree
-;Write a predicate which checks whether or not a given sequence represents a binary tree. Each node in the tree must
-;have a value, a left child, and a right child
-(def p95 (fn tree-check [node]
-                     (if (nil? node)
-                       true
-                       (if (sequential? node)
-                         (if (and (= (count node) 3)
-                                  (tree-check (second node))
-                                  (tree-check (nth node 2)))
-                           true
-                           false)
-                         (if node
-                           true
-                           false)))))
+;Write a predicate which checks whether or not a given sequence represents a binary tree.
+;Each node in the tree must have a value, a left child, and a right child
+;
+(defn binary-tree?
+  "Returns true if node is the root node of a binary tree."
+  [node]
+  (if (nil? node)
+    true
+    (if (sequential? node)
+      (if (and (= (count node) 3)
+               (binary-tree? (second node))
+               (binary-tree? (last node)))
+        true
+        false)
+      false)))
 
-;tests
-(p95 '(:a (:b nil nil) nil))
-(p95 '(:a (:b nil nil)))
-(p95 [1 nil [2 [3 nil nil] [4 nil nil]]])
-(p95 [1 [2 nil nil] [3 nil nil] [4 nil nil]])
-(p95 [1 [2 [3 [4 nil nil] nil] nil] nil])
-(p95 [1 [2 [3 [4 false nil] nil] nil] nil])
-(p95 '(:a nil ()))
+(= (binary-tree? '(:a (:b nil nil) nil))
+   true)
+(= (binary-tree? '(:a (:b nil nil)))
+   false)
+(= (binary-tree? [1 nil [2 [3 nil nil] [4 nil nil]]])
+   true)
+(= (binary-tree? [1 [2 nil nil] [3 nil nil] [4 nil nil]])
+   false)
+(= (binary-tree? [1 [2 [3 [4 nil nil] nil] nil] nil])
+   true)
+(= (binary-tree? [1 [2 [3 [4 false nil] nil] nil] nil])
+   false)
+(= (binary-tree? '(:a nil ()))
+   false)
