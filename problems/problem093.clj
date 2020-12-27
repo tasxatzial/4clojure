25311c2e0eb7af4b242d9ebefea221333421434c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-27 09:55:16 +0200

    problem 93: refactoring

--- problems/problem093.clj
+++ problems/problem093.clj
@@ -1,17 +1,20 @@
 ;p93: Partially Flatten a Sequence
 ;Write a function which flattens any nested combination of sequential things (lists, vectors, etc.), but
 ;maintains the lowest level sequential items. The result should be a sequence of sequences with only one level of nesting
-(def p93 (fn my-flatten [col]
-           (reduce (fn [result x]
-                     (into result
-                           (if (sequential? x)
-                             (if (sequential? (first x))
-                               (my-flatten x)
-                               [x])
-                             [x])))
-                   [] col)))
+;
+(defn my-partial-flatten
+   "Partially flattens a sequence. Maintains the lowest level sequential items."
+   [col]
+   (reduce (fn [result x]
+             (into result
+                   (if (and (sequential? x) (sequential? (first x)))
+                     (my-partial-flatten x)
+                     [x])))
+           [] col))
 
-;tests
-(p93  [["Do"] ["Nothing"]])
-(p93 [[[[:a :b]]] [[:c :d]] [:e :f]])
-(p93 '((1 2)((3 4)((((5 6)))))))
+(= (my-partial-flatten [["Do"] ["Nothing"]])
+   [["Do"] ["Nothing"]])
+(= (my-partial-flatten [[[[:a :b]]] [[:c :d]] [:e :f]])
+   [[:a :b] [:c :d] [:e :f]])
+(= (my-partial-flatten '((1 2) ((3 4) ((((5 6)))))))
+   '((1 2) (3 4) (5 6)))
