8227b59de238125e313563517576c53a6e30c03c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-28 03:28:36 +0200

    problem 102: refactoring

--- problems/102.clj
+++ problems/102.clj
@@ -2,14 +2,19 @@
 ;When working with java, you often need to create an object with fieldsLikeThis, but you'd rather work with a hashmap
 ;that has :keys-like-this until it's time to convert. Write a function which takes lower-case hyphen-separated strings
 ;and converts them to camel-case strings
-(def p102 (fn [s]
-            (let [res (memoize (fn [s]
-                                 (clojure.string/split s #"-")))]
-              (str (first (res s))
-                   (apply str (map #(str (clojure.string/upper-case (first %)) (apply str (rest %)))
-                                   (rest (res s))))))))
+;
+(defn capitalize-first
+   "Capitalizes the first letter of s."
+   [s]
+   (str (clojure.string/upper-case (first s)) (apply str (rest s))))
 
-;tests
-(p102 "something")
-(p102 "multi-word-key")
-(p102 "leaveMeAlone")
+(defn to-camel-case
+  "Converts s to camel-case."
+  [s]
+  (let [split-s (clojure.string/split s #"-")
+        capitalized-first (map capitalize-first (rest split-s))]
+    (str (first split-s) (apply str capitalized-first))))
+
+(= (to-camel-case "something") "something")
+(= (to-camel-case "multi-word-key") "multiWordKey")
+(= (to-camel-case "leaveMeAlone") "leaveMeAlone")
