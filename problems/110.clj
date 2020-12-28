ad554eeafdc7419170933233012b16c537c17465
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-28 07:40:15 +0200

    problem 110: refactoring

--- problems/110.clj
+++ problems/110.clj
@@ -1,20 +1,25 @@
 ;p110: Sequence of pronunciations
-;Write a function that returns a lazy sequence of "pronunciations" of a sequence of numbers. A pronunciation of each
-;element in the sequence consists of the number of repeating identical numbers and the number itself.
-;For example, [1 1] is pronounced as [2 1] ("two ones"), which in turn is pronounced as [1 2 1 1] ("one two, one one").
-;Your function should accept an initial sequence of numbers, and return an infinite lazy sequence of pronunciations,
-;each element being a pronunciation of the previous element
-(def p110 (fn [col]
-            (letfn [(p110_S [col]
-                      (reduce (fn [result x]
-                                (conj result (count x) (first x)))
-                              [] (partition-by identity col)))]
-              ((fn my-sp [col]
-                 (lazy-seq (cons (p110_S col) (my-sp (p110_S col)))))
-               col))))
+;Write a function that returns a lazy sequence of "pronunciations" of a sequence
+;of numbers. A pronunciation of each element in the sequence consists of the number
+;of repeating identical numbers and the number itself. For example, [1 1] is pronounced
+;as [2 1] ("two ones"), which in turn is pronounced as [1 2 1 1] ("one two, one one").
+;Your function should accept an initial sequence of numbers, and return an infinite lazy
+;sequence of pronunciations, each element being a pronunciation of the previous element
+;
+(defn pronounce
+  "Returns the pronunciation of a sequence of numbers."
+  [col]
+  (reduce (fn [result x]
+            (conj result (count x) (first x)))
+          []
+          (partition-by identity col)))
 
-;tests
-(take 3 (p110 [1]))
-(first (p110 [1 1 1 4 4]))
-(nth (p110 [1]) 6)
-(count (nth (p110 [3 2]) 15))
+(defn gen-pronounce-seq
+  "Returns a lazy sequence of \"pronunciations\" of a sequence of numbers."
+  [col]
+  (lazy-seq (cons (pronounce col) (gen-pronounce-seq (pronounce col)))))
+
+(= [[1 1] [2 1] [1 2 1 1]] (take 3 (gen-pronounce-seq [1])))
+(= [3 1 2 4] (first (gen-pronounce-seq [1 1 1 4 4])))
+(= [1 1 1 3 2 1 3 2 1 1] (nth (gen-pronounce-seq [1]) 6))
+(= 338 (count (nth (gen-pronounce-seq [3 2]) 15)))
