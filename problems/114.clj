77c5b697de04f4cde0bb3996eb9910f65270c0f8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-28 09:39:39 +0200

    problem 114: refactoring

--- problems/114.clj
+++ problems/114.clj
@@ -1,18 +1,32 @@
 ;p114: Global take-while
-;take-while is great for filtering sequences, but it limited: you can only examine a single item of the sequence
-;at a time. What if you need to keep track of some state as you go over the sequence?
-;Write a function which accepts an integer n, a predicate p, and a sequence. It should return a lazy sequence
-;of items in the list up to, but not including, the nth item that satisfies the predicate
-(def p114 (fn [N pred col]
-            ((fn gltake [result N i]
-               (if (pred (nth col i))
-                 (if (= N 1)
-                   result
-                   (lazy-seq (cons (nth col i) (gltake result (- N 1) (+ i 1)))))
-                 (lazy-seq (cons (nth col i) (gltake result N (+ i 1))))))
-             [] N 0)))
+;take-while is great for filtering sequences, but it is limited: you can only examine
+;a single item of the sequence at a time. What if you need to keep track of some
+;state as you go over the sequence? Write a function which accepts an integer n,
+;a predicate p, and a sequence. It should return a lazy sequence of items in the list
+;up to, but not including, the nth item that satisfies the predicate
+;
+(defn global-take-while
+  "Returns a lazy sequence of items in col up to, but not including,
+  the Nth item that satisfies the pred."
+  [N pred col]
+  ((fn _global-take-while
+     [col result collected]
+     (if (empty? col)
+       result
+       (let [current-item (first col)]
+         (if (pred current-item)
+           (if (= (dec N) collected)
+             result
+             (lazy-seq (cons current-item (_global-take-while (rest col) result (inc collected)))))
+           (lazy-seq (cons current-item (_global-take-while (rest col) result collected)))))))
+   col [] 0))
 
-;tests
-(p114 4 #(= 2 (mod % 3)) [2 3 5 7 11 13 17 19 23])
-(p114 3 #(some #{\i} %) ["this" "is" "a" "sentence" "i" "wrote"])
-(p114 1 #{"a"} ["this" "is" "a" "sentence" "i" "wrote"])
+(= [2 3 5 7 11 13]
+   (global-take-while 4 #(= 2 (mod % 3))
+                      [2 3 5 7 11 13 17 19 23]))
+(= ["this" "is" "a" "sentence"]
+   (global-take-while 3 #(some #{\i} %)
+                      ["this" "is" "a" "sentence" "i" "wrote"]))
+(= ["this" "is"]
+   (global-take-while 1 #{"a"}
+                      ["this" "is" "a" "sentence" "i" "wrote"]))
