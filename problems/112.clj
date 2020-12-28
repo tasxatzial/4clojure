3662d349662fa83d915bfd7cf5a7c87f8b77a0db
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-28 08:57:36 +0200

    problem 112: refactoring

--- problems/112.clj
+++ problems/112.clj
@@ -1,25 +1,39 @@
 ;p112: Sequs Horribilis
-;Create a function which takes an integer and a nested collection of integers as arguments. Analyze the elements
-;of the input collection and return a sequence which maintains the nested structure, and which includes all
-;elements starting from the head whose sum is less than or equal to the input integer
-(def p112 (fn [N col]
-             ((fn my-sh [col sm fcol]
-                (if (empty? col)
-                  fcol
-                  (if (sequential? (first col))
-                    (let [f (memoize (fn [col]
-                                       (my-sh (first col) sm '())))]
-                      (recur (next col) (+ sm (reduce + (flatten (f col)))) (concat fcol [(f col)])))
-                    (if (< N (+ sm (first col)))
-                      fcol
-                      (recur (next col) (+ sm (first col)) (concat fcol [(first col)]))))))
-              col 0 '())))
+;Create a function which takes an integer and a nested collection of integers as
+;arguments. Analyze the elements of the input collection and return a sequence which
+;maintains the nested structure, and which includes all elements starting from the
+;head whose sum is less than or equal to the input integer
+;
+(defn sequs-horribilis
+  [N col]
+  ((fn _sequs-horribilis
+     [col sum result]
+     (if (empty? col)
+       result
+       (let [current-element (first col)]
+         (if (sequential? current-element)
+           (let [partial-seq (_sequs-horribilis current-element sum '())
+                 new-sum (+ sum (reduce + (flatten partial-seq)))
+                 new-result (concat result [partial-seq])]
+             (recur (next col) new-sum new-result))
+           (if (< N (+ sum current-element))
+             result
+             (let [new-sum (+ sum current-element)
+                   new-result (concat result [current-element])]
+               (recur (next col) new-sum new-result)))))))
+   col 0 '()))
 
-;tests
-(p112 10 [1 2 [3 [4 5] 6] 7])
-(p112 30 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11])
-(p112 9 (range))
-(p112 1 [[[[[1]]]]])
-(p112 0 [1 2 [3 [4 5] 6] 7])
-(p112 0 [0 0 [0 [0]]])
-(p112 1 [-10 [1 [2 3 [4 5 [6 7 [8]]]]]])
+(=  (sequs-horribilis 10 [1 2 [3 [4 5] 6] 7])
+    '(1 2 (3 (4))))
+(=  (sequs-horribilis 30 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11])
+    '(1 2 (3 (4 (5 (6 (7)))))))
+(=  (sequs-horribilis 9 (range))
+    '(0 1 2 3))
+(=  (sequs-horribilis 1 [[[[[1]]]]])
+    '(((((1))))))
+(=  (sequs-horribilis 0 [1 2 [3 [4 5] 6] 7])
+    '())
+(=  (sequs-horribilis 0 [0 0 [0 [0]]])
+    '(0 0 (0 (0))))
+(=  (sequs-horribilis 1 [-10 [1 [2 3 [4 5 [6 7 [8]]]]]])
+    '(-10 (1 (2 3 (4)))))
