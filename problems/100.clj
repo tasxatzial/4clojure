4da663a60b4b0cf2bf397fe1651e45134b2931b5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-28 03:19:59 +0200

    problem 100: refactoring

--- problems/100.clj
+++ problems/100.clj
@@ -2,35 +2,35 @@
 ;Write a function which calculates the least common multiple. Your function should accept a variable number of
 ;positive integers or ratios
 
-;solution 1 ----------------------------------------
-(def p100 (fn [& args]
-            (let [maxN (memoize (fn [args] (apply max args)))]
-              ((fn my-lcm[i]
-                 (if (some #(not= 0 (mod i %)) args)
-                   (recur (+ (maxN args) i))
-                   i))
-               (maxN args)))))
+(defn my-gcd
+  "Returns the GCD of x,y."
+  [x y]
+  (if (= 0 y)
+    x
+    (my-gcd y (mod x y))))
 
-;solution 1 tests
-(p100 2 3)
-(p100 3 5 7)
-(p100 1/3 2/5)
-(p100 3/4 1/6)
-(p100 7 5/7 2 3/5)
+;solution 1
+(defn my-lcm1
+  "Returns the LCM of args."
+  [& args]
+  (let [max (apply max args)]
+    ((fn lcm
+       [i]
+       (if (some #(not= 0 (mod i %)) args)
+         (recur (+ max i))
+         i))
+     max)))
 
-;solution 2 ----------------------------------------
-(def p100_2 (fn [& args]
-              (letfn [(my-gcd [x y]
-                        (if (= 0 y)
-                          x
-                          (my-gcd y (mod x y))))]
-                (reduce (fn [result x]
-                          (/ (* result x) (my-gcd result x)))
-                        (first args) args))))
+;solution 2: using GCD
+(defn my-lcm2
+  "Returns the LCM of args."
+  [& args]
+  (reduce (fn [result x]
+            (/ (* result x) (my-gcd result x)))
+          (first args) args))
 
-;solution 2 tests
-(p100_2 2 3)
-(p100_2 3 5 7)
-(p100_2 1/3 2/5)
-(p100_2 3/4 1/6)
-(p100_2 7 5/7 2 3/5)
+(== (my-lcm1 2 3) 6)
+(== (my-lcm1 5 3 7) 105)
+(== (my-lcm1 1/3 2/5) 2)
+(== (my-lcm1 3/4 1/6) 3/2)
+(== (my-lcm1 7 5/7 2 3/5) 210)
