d46c90f0e72b7262c1d365cee883e97f883c2c09
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-28 11:28:08 +0200

    problem 120: refactoring

--- problems/120.clj
+++ problems/120.clj
@@ -1,18 +1,26 @@
 ;p120: Sum of square of digits
-;Write a function which takes a collection of integers as an argument. Return the count of how many elements are
-;smaller than the sum of their squared component digits. For example: 10 is larger than 1 squared plus 0 squared;
+;Write a function which takes a collection of integers as an argument.
+;Return the count of how many elements are smaller than the sum of their squared
+;component digits. For example: 10 is larger than 1 squared plus 0 squared;
 ;whereas 15 is smaller than 1 squared plus 5 squared
-(def p120 (fn [col]
-            (letfn [(toDigits [n]
-                      (map (comp read-string str) (str n)))
-                    (sumSqrd [num]
-                      (reduce (fn [result x]
-                                (+ result (* x x)))
-                              0 (toDigits num)))]
-              (count (filter #(< % (sumSqrd %)) col)))))
+;
+(defn to-digits
+  "Returns a list of the digits of a number."
+  [N]
+  (map (comp read-string str) (str N)))
 
-;tests
-(p120 (range 10))
-(p120 (range 30))
-(p120 (range 100))
-(p120 (range 1000))
+(defn squared-digit-sum
+  "Returns the sum of the squares of the digits of num."
+  [num]
+  (reduce (fn [result x]
+            (+ result (* x x)))
+          0 (to-digits num)))
+
+(defn p120
+  [col]
+  (count (filter #(< % (squared-digit-sum %)) col)))
+
+(= 8 (p120 (range 10)))
+(= 19 (p120 (range 30)))
+(= 50 (p120 (range 100)))
+(= 50 (p120 (range 1000)))
