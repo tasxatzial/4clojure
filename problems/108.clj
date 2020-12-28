035a3f2a8a48eebace5bdf54b7e837eccd5b4d7a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-28 07:01:18 +0200

    problem 108: refactoring

--- problems/108.clj
+++ problems/108.clj
@@ -1,20 +1,35 @@
 ;p108: Lazy Searching
-;Given any number of sequences, each sorted from smallest to largest, find the smallest single number which appears
-;in all of the sequences. The sequences may be infinite, so be careful to search lazily
-(def p108 (fn [& args]
-            (letfn [(p108_S [col X]
-                      (= X (last (take-while #(<= % X) col))))]
-              ((fn my-lz [N]
-                 (if (nil? (nth (first args) N nil))
-                   nil
-                   (if (some #(false? %) (map p108_S (rest args) (repeat (nth (first args) N))))
-                     (my-lz (+ N 1))
-                     (nth (first args) N))))
-               0))))
+;Given any number of sequences, each sorted from smallest to largest, find the
+;smallest single number which appears in all of the sequences. The sequences may
+;be infinite, so be careful to search lazily
+;
+(defn my-contains?
+  "Returns true if col contains X, false otherwise. col must be sorted in
+  increasing order and may be infinite."
+  [col X]
+  (= X (last (take-while #(<= % X) col))))
 
-;tests
-(p108 [1 2 3 4 5 6 7] [0.5 3/2 4 19])
-(p108 (range) (range 0 100 7/6) [2 3 5 7 11 13])
-(p108 (map #(* % % %) (range)) ;; perfect cubes
-    (filter #(zero? (bit-and % (dec %))) (range)) ;; powers of 2
-    (iterate inc 20))    ;64
+(defn _smallest-common
+  "Returns the smallest number which appears in all args. Every collection
+   in args must be sorted in increasing order."
+  ([args] (_smallest-common (first args) (rest args)))
+  ([first-arg rest-args]
+   (if (empty? first-arg)
+     nil
+     (let [num (first first-arg)]
+       (if (some false? (map #(my-contains? % num) rest-args))
+         (_smallest-common (rest first-arg) rest-args)
+         num)))))
+
+(defn smallest-common
+  "Returns the smallest number which appears in all args. Every collection
+   in args must be sorted in increasing order."
+  [& args]
+  (_smallest-common args))
+
+(= 3 (smallest-common [3 4 5]))
+(= 4 (smallest-common [1 2 3 4 5 6 7] [0.5 3/2 4 19]))
+(= 7 (smallest-common (range) (range 0 100 7/6) [2 3 5 7 11 13]))
+(= 64 (smallest-common (map #(* % % %) (range)) ;; perfect cubes
+                       (filter #(zero? (bit-and % (dec %))) (range)) ;; powers of 2
+                       (iterate inc 20))) ;; at least as large as 20
