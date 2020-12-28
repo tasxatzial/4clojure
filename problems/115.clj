2c3d47d60bbd27458485db783d3b02dee4835be0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-28 10:08:56 +0200

    problem 115: refactoring

--- problems/115.clj
+++ problems/115.clj
@@ -1,20 +1,37 @@
 ;p115: The Balance of N
-;A balanced number is one whose component digits have the same sum on the left and right halves of the number.
-;Write a function which accepts an integer n, and returns true iff n is balanced
-(def p115 (fn [N]
-            (let [toseq (memoize (fn [N] (map (comp read-string str) (str N))))
-                  half (memoize (fn [N] (Math/round (Math/floor (/ (count (toseq N)) 2)))))]
-              (= (apply + (take (half N) (toseq N))) (apply + (drop (if (odd? (count (toseq N)))
-                                                                        (inc (half N))
-                                                                        (half N))
-                                                                      (toseq N)))))))
+;A balanced number is one whose component digits have the same sum on the
+;left and right halves of the number. Write a function which accepts an
+;integer n, and returns true iff n is balanced
+;
+(defn to-digits
+  "Returns a list of the digits of a number."
+  [N]
+  (map (comp read-string str) (str N)))
 
-;tests
-(p115 11)
-(p115 121)
-(p115 123)
-(p115 0)
-(p115 88099)
-(p115 89098)
-(p115 89089)
-(take 20 (filter p115 (range)))
+(defn count-half-digits
+  "Returns how many items from digits are in each of
+  the left/right half of digits."
+  [digits]
+  (let [digit-count (count digits)]
+    (if (= 0 (mod digit-count 2))
+      (Math/round ^double (* digit-count 0.5))
+      (Math/round ^double (* (dec digit-count) 0.5)))))
+
+(defn balanced?
+  "Returns true if N is balanced, false otherwise."
+  [N]
+  (let [digits (to-digits N)
+        half-digit-count (count-half-digits digits)
+        sum-left-half (apply + (take half-digit-count digits))
+        sum-right-half (apply + (take-last half-digit-count digits))]
+    (= sum-left-half sum-right-half)))
+
+(= true (balanced? 11))
+(= true (balanced? 121))
+(= false (balanced? 123))
+(= true (balanced? 0))
+(= false (balanced? 88099))
+(= true (balanced? 89098))
+(= true (balanced? 89089))
+(= (take 20 (filter balanced? (range)))
+   [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101])
