73312113347c5e14230e852aa06c8ba89bc55702
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-29 07:30:08 +0200

    problem 137: refactoring

--- problems/137.clj
+++ problems/137.clj
@@ -1,19 +1,26 @@
 ;p137: Digits and bases
-;Write a function which returns a sequence of digits of a non-negative number (first argument) in numerical system
-;with an arbitrary base (second argument). Digits should be represented with their integer values,
-;e.g. 15 would be [1 5] in base 10, [1 1 1 1] in base 2 and [15] in base 16
-(def p137 (fn [number base]
-            (if (= 0 number)
-              '(0)
-              ((fn [result num]
-                 (if (= 0 num)
-                   result
-                   (recur (conj result (mod num base)) (int (/ num base)))))
-               '() number))))
+;Write a function which returns a sequence of digits of a non-negative number (first argument)
+;in numerical system with an arbitrary base (second argument). Digits should be represented
+;with their integer values, e.g. 15 would be [1 5] in base 10, [1 1 1 1] in base 2 and [15]
+;in base 16
+;
+(defn to-digits
+  "Returns a sequence of digits of a non-negative number in numerical
+   system with an arbitrary base."
+  [number base]
+  (if (= 0 number)
+    '(0)
+    ((fn [result num]
+       (if (= 0 num)
+         result
+         (let [new-result (conj result (mod num base))
+               new-num (int (/ num base))]
+           (recur new-result new-num))))
+     '()
+     number)))
 
-;tests
-(p137 1234501 10)
-(p137 0 11)
-(p137 9 2)
-(let [n (rand-int 100000)](p137 n n))
-(p137 Integer/MAX_VALUE 42)
+(= [1 2 3 4 5 0 1] (to-digits 1234501 10))
+(= [0] (to-digits 0 11))
+(= [1 0 0 1] (to-digits 9 2))
+(= [1 0] (let [n (rand-int 100000)] (to-digits n n)))
+(= [16 18 5 24 15 1] (to-digits Integer/MAX_VALUE 42))
