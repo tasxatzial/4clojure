e74a3ae657395ad835faff0a451f33c0b127ec81
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-29 07:04:34 +0200

    problem 134: refactoring

--- problems/134.clj
+++ problems/134.clj
@@ -1,10 +1,11 @@
 ;p134: A nil key
-;Write a function which, given a key and map, returns true iff the map contains an entry with that key and its
-;value is nil
-(def p134 (fn [key col]
-            (and (contains? col key) (nil? (col key)))))
+;Write a function which, given a key and map, returns true iff the map contains an entry with
+;that key and its value is nil
+;
+(defn my-nil?
+  [key col]
+  (and (contains? col key) (nil? (col key))))
 
-;tests
-(p134 :a {:a nil :b 2})
-(p134 :b {:a nil :b 2})
-(p134 :c {:a nil :b 2})
+(true?  (my-nil? :a {:a nil :b 2}))
+(false? (my-nil? :b {:a nil :b 2}))
+(false? (my-nil? :c {:a nil :b 2}))
