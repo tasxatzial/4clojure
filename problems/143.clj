7284664bdf734b27dd9cbb9533c8ca1e4c33c01a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-29 07:39:16 +0200

    problem 143: refactoring

--- problems/143.clj
+++ problems/143.clj
@@ -1,11 +1,12 @@
 ;p143: dot product
-;Create a function that computes the dot product of two sequences. You may assume that the vectors will have the
-;same length
-(def p143 (fn [col1 col2]
-            (reduce + (map * col1 col2))))
+;Create a function that computes the dot product of two sequences. You may assume that the
+;vectors will have the same length
+;
+ (defn compute-dot-product
+   [col1 col2]
+   (reduce + (map * col1 col2)))
 
-;tests
-(p143 [0 1 0] [1 0 0])
-(p143 [1 1 1] [1 1 1])
-(p143 [1 2 3] [4 5 6])
-(p143 [2 5 6] [100 10 1])
+(= 0 (compute-dot-product [0 1 0] [1 0 0]))
+(= 3 (compute-dot-product [1 1 1] [1 1 1]))
+(= 32 (compute-dot-product [1 2 3] [4 5 6]))
+(= 256 (compute-dot-product [2 5 6] [100 10 1]))
