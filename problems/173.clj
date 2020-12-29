e989a6ba28b0ece706b35a7fd9658278d7e9e147
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-29 09:21:13 +0200

    problem 161,162,173: delete (too simple)

--- problems/173.clj
+++
@@ -1,7 +0,0 @@
-;p173: Intro to Destructuring 2
-;Sequential destructuring allows you to bind symbols to parts of sequential things (vectors, lists, seqs, etc.):
-;(let [bindings* ] exprs*) Complete the bindings so all let-parts evaluate to 3
-(= 3
-   (let [[x y] [+ (range 3)]] (apply x y))                  ;x y
-   (let [[[x y] b] [[+ 1] 2]] (x y b))
-   (let [[x y] [inc 2]] (x y)))
