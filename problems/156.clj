804b600a9962e1c66396f9ed006889791d587540
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-29 09:15:21 +0200

    problem 156: refactoring

--- problems/156.clj
+++ problems/156.clj
@@ -1,25 +1,23 @@
 ;p156: Map Defaults
 ;When retrieving values from a map, you can specify default values in case the key is not found:
 ;(= 2 (:foo {:bar 0, :baz 1} 2))
-;However, what if you want the map itself to contain the default values? Write a function which takes a default
-;value and a sequence of keys and constructs a map
+;However, what if you want the map itself to contain the default values? Write a function which takes
+;a default value and a sequence of keys and constructs a map
 
-;solution 1 ----------------------------------------
-(def p156 (fn [val col]
-            (reduce (fn [result x]
-                      (conj result [x val]))
-                    {} col)))
+;solution 1
+(defn create-map1
+  "Constructs a map from val and a collection col of keys."
+  [val col]
+  (reduce (fn [result x]
+            (conj result [x val]))
+          {} col))
 
-;solution 1 tests
-(p156 0 [:a :b :c])
-(p156 "x" [1 2 3])
-(p156 [:a :b] [:foo :bar])
+;solution 2
+(defn create-map2
+  "Constructs a map from val and a collection col of keys."
+  [val col]
+  (apply hash-map (interleave col (repeat val))))
 
-;solution 2 ----------------------------------------
-(def p156_2 (fn [val col]
-            (apply hash-map (interleave col (repeat val)))))
-
-;solution 2 tests
-(p156_2 0 [:a :b :c])
-(p156_2 "x" [1 2 3])
-(p156_2 [:a :b] [:foo :bar])
+(= (create-map1 0 [:a :b :c]) {:a 0 :b 0 :c 0})
+(= (create-map1 "x" [1 2 3]) {1 "x" 2 "x" 3 "x"})
+(= (create-map1 [:a :b] [:foo :bar]) {:foo [:a :b] :bar [:a :b]})
