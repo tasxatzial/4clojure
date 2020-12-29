8fd48772e8ffa9c9ec97809727b7d8310f85fa4b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-29 07:48:33 +0200

    problem 144: refactoring

--- problems/144.clj
+++ problems/144.clj
@@ -1,13 +1,20 @@
-;p144: Oscilrate
-;Write an oscillating iterate: a function that takes an initial value and a variable number of functions. It should
-;return a lazy sequence of the functions applied to the value in order, restarting from the first function after
-;it hits the end
-(def p144 (fn [val & args]
-            ((fn oscil [val idx]
-               (cons val (lazy-seq (oscil ((nth args (mod idx (count args))) val) (+ idx 1)))))
-             val 0)))
+;p144: Oscillate
+;Write an oscillating iterate: a function that takes an initial value and a variable number of
+;functions. It should return a lazy sequence of the functions applied to the value in order,
+;restarting from the first function after it hits the end
+;
+(defn oscillate
+  "Returns a lazy sequence of the functions args applied to val in order,
+   restarting from the first function after it hits the end."
+  [val & args]
+  (let [func-count (count args)]
+    ((fn _oscillate
+       [val i]
+       (let [index (mod i func-count)]
+         (cons val (lazy-seq (_oscillate ((nth args index) val) (inc index))))))
+     val
+     0)))
 
-;tests
-(take 5 (p144 3.14 int double))
-(take 5 (p144 3 #(- % 3) #(+ 5 %)))
-(take 12 (p144 0 inc dec inc dec inc))
+(= (take 3 (oscillate 3.14 int double)) [3.14 3 3.0])
+(= (take 5 (oscillate 3 #(- % 3) #(+ 5 %))) [3 0 5 2 7])
+(= (take 12 (oscillate 0 inc dec inc dec inc)) [0 1 0 1 0 1 2 1 2 1 2 3])
