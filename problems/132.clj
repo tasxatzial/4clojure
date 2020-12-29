77409ed168c25db7298fad585aff2081ac9141e5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-29 07:02:26 +0200

    problem 132: refactoring

--- problems/132.clj
+++ problems/132.clj
@@ -1,25 +1,32 @@
 ;p132: Insert between two items
-;Write a function that takes a two-argument predicate, a value, and a collection; and returns a new collection
-;where the value is inserted between every two items that satisfy the predicate
-(def p132 (fn [pred val col]
-            (if (empty? col)
-              col
-              ((fn my-ins [idx]
-                 (if (nil? (nth col (+ idx 1) nil))
-                   [(nth col idx)]
-                   (if (pred (nth col idx) (nth col (+ idx 1)))
-                     (lazy-seq (cons (nth col idx) (cons val (my-ins (+ idx 1)))))
-                     (lazy-seq (cons (nth col idx) (my-ins (+ idx 1)))))))
-               0))))
+;Write a function that takes a two-argument predicate, a value, and a collection; and returns
+;a new collection where the value is inserted between every two items that satisfy the predicate
+;
+(defn insert-between
+  "Returns a new collection where the value is inserted between every two
+   items that satisfy the predicate."
+  [pred val col]
+  (if (empty? col)
+    col
+    ((fn _insert-between [idx]
+       (let [next-item (nth col (inc idx) nil)
+             cur-item (nth col idx)]
+         (if (nil? next-item)
+           [cur-item]
+           (if (pred cur-item next-item)
+             (lazy-seq (cons cur-item (cons val (_insert-between (inc idx)))))
+             (lazy-seq (cons cur-item (_insert-between (inc idx))))))))
+     0)))
 
-;tests
-(p132 < :less [1 6 7 4 3])
-(p132 > :more [2])
-(p132 #(and (pos? %) (< % %2)) :x (range 5))
-(empty? (p132 > :more ()))
-(take 12 (->> [0 1]
-              (iterate (fn [[a b]] [b (+ a b)]))
-              (map first) ; fibonacci numbers
-              (p132 (fn [a b] ; both even or both odd
-                      (= (mod a 2) (mod b 2)))
-                    :same)))
+(= '(1 :less 6 :less 7 4 3) (insert-between < :less [1 6 7 4 3]))
+(= '(2) (insert-between > :more [2]))
+(= [0 1 :x 2 :x 3 :x 4]  (insert-between #(and (pos? %) (< % %2)) :x (range 5)))
+(empty? (insert-between > :more ()))
+
+(= [0 1 :same 1 2 3 :same 5 8 13 :same 21]
+   (take 12 (->> [0 1]
+                 (iterate (fn [[a b]] [b (+ a b)]))
+                 (map first) ; fibonacci numbers
+                 (insert-between (fn [a b] ; both even or both odd
+                       (= (mod a 2) (mod b 2)))
+                     :same))))
