e3e4d09cf4701f7c3a1f6d20d47500876fa5bfec
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-12-29 09:18:36 +0200

    problem 157: refactoring/keep simplest somution

--- problems/157.clj
+++ problems/157.clj
@@ -1,35 +1,10 @@
 ;p157: Indexing Sequences
 ;Transform a sequence into a sequence of pairs containing the original elements along with their index
+;
+(defn index-sequence
+  [col]
+  (partition 2 (interleave col (range))))
 
-;solution 1 ----------------------------------------
-(def p157 (fn [col]
-            (partition 2 (interleave col (range)))))
-
-;solution 1 tests
-(p157 [:a :b :c])
-(p157 [0 1 3])
-(p157 [[:foo] {:bar :baz}])
-
-;solution 2 ----------------------------------------
-(def p157_2 (fn [col]
-              ((fn [result col idx]
-                 (if (empty? col)
-                   result
-                   (recur (conj result [(first col) idx]) (rest col) (+ idx 1))))
-               [] col 0)))
-
-;solution 2 tests
-(p157_2 [:a :b :c])
-(p157_2 [0 1 3])
-(p157_2 [[:foo] {:bar :baz}])
-
-;solution 3 ----------------------------------------
-(def p157_3 (fn [col]
-              (rest (reduce (fn [result x]
-                              (conj result [x (inc (last (last result)))]))
-                            [[-1]] col))))
-
-;solution 3 tests
-(p157_3 [:a :b :c])
-(p157_3 [0 1 3])
-(p157_3 [[:foo] {:bar :baz}])
+(= (index-sequence [:a :b :c]) [[:a 0] [:b 1] [:c 2]])
+(= (index-sequence [0 1 3]) '((0 0) (1 1) (3 2)))
+(= (index-sequence [[:foo] {:bar :baz}]) [[[:foo] 0] [{:bar :baz} 1]])
