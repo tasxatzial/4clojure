2a07962425210fa8d25bef1a9d000c38c59fbf25
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2021-01-04 12:46:56 +0200

    fix readme title

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Description
+# 4clojure solutions
 
 My solutions to the problems on [4clojure.com](http://www.4clojure.com/)
---
+++ problems/166.clj
@@ -0,0 +1,11 @@
+;p60: Sequence Reductions
+;Write a function which behaves like reduce, but returns each intermediate value
+;of the reduction. Your function must accept either two or three arguments, and
+;the return sequence must be lazy.
+;
+;restrictions: reductions
+;
+(defn reduce-sequence
+  [op x ]
+  (if op))
+
