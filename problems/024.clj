18a847d1812bf4c12594115728bede364d3d9702
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-26 02:44:26 +0200

    problem 24: refactoring

--- problems/024.clj
+++ problems/024.clj
@@ -1,14 +1,27 @@
-;p24: Sum It All Up
-;Write a function which returns the sum of a sequence of numbers
-;
-(defn my-sum
-  "Returns the sum of a sequence of numbers."
-  [col]
-  (apply + col))
+;; p24: Sum It All Up
 
-;tests
-(= (my-sum [1 2 3]) 6)
-(= (my-sum (list 0 -2 5 5)) 8)
-(= (my-sum #{4 2 1}) 7)
-(= (my-sum '(0 0 -1)) -1)
-(= (my-sum '(1 10 3)) 14)
+;; Write a function which returns the sum of a sequence of numbers
+
+(defn my-sum
+  [xs]
+  (apply + xs))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (my-sum [1 2 3]) 6))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (my-sum (list 0 -2 5 5)) 8))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (my-sum #{4 2 1}) 7))))
+
+(clojure.test/deftest test4
+  (clojure.test/testing
+    (clojure.test/is (= (my-sum '(0 0 -1)) -1))))
+
+(clojure.test/deftest test5
+  (clojure.test/testing
+    (clojure.test/is (= (my-sum '(1 10 3)) 14))))
