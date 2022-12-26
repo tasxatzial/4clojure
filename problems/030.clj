eefef52b624a80a76c8891fc178eb1caf84d7c34
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-26 17:48:58 +0200

    problem 30: refactoring, add new solution & remove old ones

--- problems/030.clj
+++ problems/030.clj
@@ -1,63 +1,45 @@
-;p30: Compress a Sequence
-;Write a function which removes consecutive duplicates from a sequence
+;; p30: Compress a Sequence
 
-;solution 1: using recursion/nth and checking if nth element is the same as the nth-1 one
-(defn my-compress1
-  "Removes consecutive duplicates from a sequence."
-  ([col] (my-compress1 col 0 [] (count col)))
-  ([col N result col_count]
-   (if (= 0 N)
-     (recur col (+ N 1) (conj result (first col)) col_count)
-     (if (= col_count N)
-       result
-       (let [nth_col (nth col N)]
-         (if (= nth_col (nth col (- N 1)))
-           (recur col (+ N 1) result col_count)
-           (recur col (+ N 1) (conj result nth_col) col_count)))))))
+;; Write a function which removes consecutive duplicates from a sequence
 
-;solution 2: using recursion/rest and checking if the second element is the same as the first one
-(defn my-compress2
-  "Removes consecutive duplicates from a sequence."
-  ([col] (my-compress2 (map identity col) []))
-  ([col result]
-   (if (= 0 (count col))
-     result
-     (if (= 1 (count col))
-       (conj result (first col))
-       (if (= (first col) (second col))
-         (recur (rest col) result)
-         (recur (rest col) (conj result (first col))))))))
+(defn compress-seq
+  ([xs]
+   (if (empty? xs)
+     []
+     (compress-seq (map identity xs) [])))
+  ([[x & rest-xs] result]
+   (if (seq rest-xs)
+     (if (= x (first rest-xs))
+       (recur rest-xs result)
+       (recur rest-xs (conj result x)))
+     (conj result x))))
 
-;solution 3: using reduce
-(defn my-compress3
-  "Removes consecutive duplicates from a sequence."
-  [col]
-  (reduce (fn [result x]
-            (if (or (empty? result) (not= (last result) x))
-              (concat result [x])
-              result))
-          '()
-          col))
+(defn compress-seq2
+  [xs]
+  (map first (partition-by identity xs)))
 
-;solution 4: partition-by
-(defn my-compress4
-  "Removes consecutive duplicates from a sequence."
-  [col]
-  (map first (partition-by identity col)))
+;; testing compress-seq -----------------------------------
+(clojure.test/deftest test1-compress-seq
+  (clojure.test/testing
+    (clojure.test/is (= (apply str (compress-seq "Leeeeeerrroyyy")) "Leroy"))))
 
-;tests
-(= (apply str (my-compress1 "Leeeeeerrroyyy")) "Leroy")
-(= (my-compress1 [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
-(= (my-compress1 [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
+(clojure.test/deftest test2-compress-seq
+  (clojure.test/testing
+    (clojure.test/is (= (compress-seq [1 1 2 3 3 2 2 3]) '(1 2 3 2 3)))))
 
-(= (apply str (my-compress2 "Leeeeeerrroyyy")) "Leroy")
-(= (my-compress2 [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
-(= (my-compress2 [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
+(clojure.test/deftest test3-compress-seq
+  (clojure.test/testing
+    (clojure.test/is (= (compress-seq [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2])))))
 
-(= (apply str (my-compress3 "Leeeeeerrroyyy")) "Leroy")
-(= (my-compress3 [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
-(= (my-compress3 [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
+;; testing compress-seq2 -----------------------------------
+(clojure.test/deftest test1-compress-seq2
+  (clojure.test/testing
+    (clojure.test/is (= (apply str (compress-seq2 "Leeeeeerrroyyy")) "Leroy"))))
 
-(= (apply str (my-compress4 "Leeeeeerrroyyy")) "Leroy")
-(= (my-compress4 [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
-(= (my-compress4 [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
+(clojure.test/deftest test2-compress-seq2
+  (clojure.test/testing
+    (clojure.test/is (= (compress-seq2 [1 1 2 3 3 2 2 3]) '(1 2 3 2 3)))))
+
+(clojure.test/deftest test3-compress-seq2
+  (clojure.test/testing
+    (clojure.test/is (= (compress-seq2 [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2])))))
