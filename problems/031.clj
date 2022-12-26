7bc243e89e03507916984466f66d5003a6a1c21a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-27 00:17:22 +0200

    problem 31: refactoring, add new solution & remove old ones

--- problems/031.clj
+++ problems/031.clj
@@ -1,44 +1,45 @@
-;p31: Pack a Sequence
-;Write a function which packs consecutive duplicates into sub-lists
+;; p31: Pack a Sequence
 
-;solution 1: using recursion/nth and checking if the nth element is the same as the nth-1 one
-(defn my-pack1
-  "Packs consecutive duplicates into sub-lists."
-  ([col] (my-pack1 col 0 [] '()))
-  ([col N result result_tmp]
-   (if (= N 0)
-     (recur col (+ N 1) result (concat result_tmp (list (first col))))
-     (if (= (count col) N)
-       (conj result result_tmp)
-       (let [nth_col (nth col N)]
-         (if (= nth_col (nth col (- N 1)))
-           (recur col (+ N 1) result (concat result_tmp (list nth_col)))
-           (recur col (+ N 1) (conj result result_tmp) (list nth_col))))))))
+;; Write a function which packs consecutive duplicates into sub-lists
 
-;solution 2: using recursion/rest and checking if the second element is the same as the first one
-(defn my-pack2
-  "Packs consecutive duplicates into sub-lists."
-  ([col] (my-pack2 col [] '()))
-  ([col result result_tmp]
-   (if (= 0 (count col))
-     result
-     (if (= 1 (count col))
-       (conj result (concat result_tmp (list (first col))))
-       (if (= (first col) (second col))
-         (recur (rest col) result (concat result_tmp (list (first col))))
-         (recur (rest col) (conj result (concat result_tmp (list (first col)))) '()))))))
+(defn partition-by-identity
+  ([xs]
+   (if (empty? xs)
+     []
+     (partition-by-identity xs 1 [])))
+  ([[x & rest-xs] equal-count result]
+   (if (seq rest-xs)
+     (if (= x (first rest-xs))
+       (recur rest-xs (inc equal-count) result)
+       (recur rest-xs 1 (conj result (take equal-count (repeat x)))))
+     (conj result (take equal-count (repeat x))))))
 
-;solution 3: partition-by
-(defn my-pack3
-  "Packs consecutive duplicates into sub-lists."
-  [col]
-  (partition-by identity col))
+(defn partition-by-identity2
+  [xs]
+  (partition-by identity xs))
 
-;tests
-(= (my-pack1 [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3)))
-(= (my-pack1 [:a :a :b :b :c]) '((:a :a) (:b :b) (:c)))
-(= (my-pack1 [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4])))
+;; testing partition-by-identity -----------------------------------
+(clojure.test/deftest test1-partition-by-identity
+  (clojure.test/testing
+    (clojure.test/is (= (partition-by-identity [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3))))))
 
-(= (my-pack2 [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3)))
-(= (my-pack2 [:a :a :b :b :c]) '((:a :a) (:b :b) (:c)))
-(= (my-pack2 [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4])))
+(clojure.test/deftest test2-partition-by-identity
+  (clojure.test/testing
+    (clojure.test/is (= (partition-by-identity [:a :a :b :b :c]) '((:a :a) (:b :b) (:c))))))
+
+(clojure.test/deftest test3-partition-by-identity
+  (clojure.test/testing
+    (clojure.test/is (= (partition-by-identity [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4]))))))
+
+;; testing partition-by-identity2 -----------------------------------
+(clojure.test/deftest test1-partition-by-identity2
+  (clojure.test/testing
+    (clojure.test/is (= (partition-by-identity2 [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3))))))
+
+(clojure.test/deftest test2-partition-by-identity2
+  (clojure.test/testing
+    (clojure.test/is (= (partition-by-identity2 [:a :a :b :b :c]) '((:a :a) (:b :b) (:c))))))
+
+(clojure.test/deftest test3-partition-by-identity2
+  (clojure.test/testing
+    (clojure.test/is (= (partition-by-identity2 [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4]))))))
