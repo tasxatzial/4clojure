d961ed8336873f4b9df15cd318dd38c0adb38011
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-26 02:24:38 +0200

    problems 19,20: refactoring

--- problems/019.clj
+++ problems/019.clj
@@ -1,16 +1,21 @@
-;p19: last element
-;Write a function which returns the last element in a sequence
-;
-;restrictions: last
-;
-(defn my-last
-  "Returns the last element in col."
-  [col]
-  (if (empty? col)
-    nil
-    (nth col (- (count col) 1))))
+;; p19: Last element
 
-;tests
-(= (my-last [1 2 3 4 5]) 5)
-(= (my-last '(5 4 3)) 3)
-(= (my-last ["b" "c" "d"]) "d")
+;; Write a function which returns the last element in a sequence.
+;; restrictions: last
+
+(defn get-last
+  [xs]
+  (when (seq xs)
+    (nth xs (dec (count xs)))))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (get-last [1 2 3 4 5]) 5))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (get-last '(5 4 3)) 3))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (get-last ["b" "c" "d"]) "d"))))
--- problems/020.clj
+++ problems/020.clj
@@ -1,14 +1,21 @@
-;p20: Penultimate element
-;Write a function which returns the second to last element from a sequence
-;
-(defn second-to-last
-  "Returns the second to last element in col."
-  [col]
-  (if (>= (count col) 2)
-    (nth col (- (count col) 2))
-    nil))
+;; p20: Penultimate element
 
-;tests
-(= (second-to-last (list 1 2 3 4 5)) 4)
-(= (second-to-last ["a" "b" "c"]) "b")
-(= (second-to-last [[1 2] [3 4]]) [1 2])
+;; Write a function which returns the second to last element from a sequence.
+
+(defn get-second-to-last
+  [xs]
+  (let [c (count xs)]
+    (when (> c 1)
+      (nth xs (- c 2)))))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (get-second-to-last (list 1 2 3 4 5)) 4))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (get-second-to-last ["a" "b" "c"]) "b"))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (get-second-to-last [[1 2] [3 4]]) [1 2]))))
