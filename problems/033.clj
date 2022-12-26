0eb547cb9f5d3187ae76e5f35bacbbf63b8172ac
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-27 00:50:37 +0200

    problem 33: refactoring, remove slower solution

--- problems/033.clj
+++ problems/033.clj
@@ -1,30 +1,27 @@
-;p33: Replicate a Sequence
-;Write a function which replicates each element of a sequence a variable number of times
+;; p33: Replicate a Sequence
 
-;solution 1
-(defn my-replicate1
-  "Replicates each element of a sequence a variable number of times."
-  [col N]
-  (reduce (fn [result x]
-            (concat result (take N (repeat x))))
-          '()
-          col))
+;; Write a function which replicates each element of a sequence a variable number of times
 
-;solution 2
-(defn my-replicate2
-  "Replicates each element of a sequence a variable number of times."
-  [col N]
-  (apply concat (map #(take N (repeat %)) col)))
+(defn replicate-elements
+  [xs N]
+  (reduce into [] (map #(take N (repeat %)) xs)))
 
-;tests
-(= (my-replicate1 [1 2 3] 2) '(1 1 2 2 3 3))
-(= (my-replicate1 [:a :b] 4) '(:a :a :a :a :b :b :b :b))
-(= (my-replicate1 [4 5 6] 1) '(4 5 6))
-(= (my-replicate1 [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
-(= (my-replicate1 [44 33] 2) [44 44 33 33])
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (replicate-elements [1 2 3] 2) '(1 1 2 2 3 3)))))
 
-(= (my-replicate2 [1 2 3] 2) '(1 1 2 2 3 3))
-(= (my-replicate2 [:a :b] 4) '(:a :a :a :a :b :b :b :b))
-(= (my-replicate2 [4 5 6] 1) '(4 5 6))
-(= (my-replicate2 [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4]))
-(= (my-replicate2 [44 33] 2) [44 44 33 33])
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (replicate-elements [:a :b] 4) '(:a :a :a :a :b :b :b :b)))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (replicate-elements [4 5 6] 1) '(4 5 6)))))
+
+(clojure.test/deftest test4
+  (clojure.test/testing
+    (clojure.test/is (= (replicate-elements [[1 2] [3 4]] 2) '([1 2] [1 2] [3 4] [3 4])))))
+
+(clojure.test/deftest test5
+  (clojure.test/testing
+    (clojure.test/is (= (replicate-elements [44 33] 2) [44 44 33 33]))))
