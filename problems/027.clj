11c80b2130492cb219fd53c0449644550819716c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-26 12:11:22 +0200

    problem 27: refactoring

--- problems/027.clj
+++ problems/027.clj
@@ -1,14 +1,27 @@
-;p27: Palindrome Detector
-;Write a function which returns true if the given sequence is a palindrome
-;
-(defn palindrome?
-  "Returns true if col is palindrome, false otherwise."
-  [col]
-  (= (seq col) (reverse col)))
+;; p27: Palindrome Detector
 
-;tests
-(false? (palindrome? '(1 2 3 4 5)))
-(true? (palindrome? "racecar"))
-(true? (palindrome? [:foo :bar :foo]))
-(true? (palindrome? '(1 1 3 3 1 1)))
-(false? (palindrome? '(:a :b :c)))
+;; Write a function which returns true if the given sequence is a palindrome
+
+(defn palindrome?
+  [xs]
+  (= (seq xs) (reverse xs)))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (false? (palindrome? '(1 2 3 4 5))))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (true? (palindrome? "racecar")))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (true? (palindrome? [:foo :bar :foo])))))
+
+(clojure.test/deftest test4
+  (clojure.test/testing
+    (clojure.test/is (true? (palindrome? '(1 1 3 3 1 1))))))
+
+(clojure.test/deftest test5
+  (clojure.test/testing
+    (clojure.test/is (false? (palindrome? '(:a :b :c))))))
