e51e3f31e98b5b463a59401691d1da5b00b3b66b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-26 02:28:03 +0200

    problem 21: refactoring

--- problems/021.clj
+++ problems/021.clj
@@ -1,17 +1,26 @@
-;p21: nth element
-;Write a function which returns the Nth element from a sequence
-;
-;restrictions: nth
-;
-(defn my-nth
-  "Returns the nth element in col."
-  [col n]
-  (if (= n 0)
-    (first col)
-    (recur (rest col) (- n 1))))
+;; p21: Nth element
 
-;tests
-(= (my-nth '(4 5 6 7) 2) 6)
-(= (my-nth [:a :b :c] 0) :a)
-(= (my-nth [1 2 3 4] 1) 2)
-(= (my-nth '([1 2] [3 4] [5 6]) 2) [5 6])
+;; Write a function which returns the Nth element from a sequence
+;; restrictions: nth
+
+(defn get-nth
+  [xs n]
+  (if (zero? n)
+    (first xs)
+    (recur (rest xs) (dec n))))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (get-nth '(4 5 6 7) 2) 6))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (get-nth [:a :b :c] 0) :a))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (get-nth [1 2 3 4] 1) 2))))
+
+(clojure.test/deftest test4
+  (clojure.test/testing
+    (clojure.test/is (= (get-nth '([1 2] [3 4] [5 6]) 2) [5 6]))))
