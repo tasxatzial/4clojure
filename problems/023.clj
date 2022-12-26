3800070e195ea415648f17b36d08f61a01ba1c61
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-26 02:39:38 +0200

    problem 23: refactoring

--- problems/023.clj
+++ problems/023.clj
@@ -1,17 +1,20 @@
-;p23: Reverse a Sequence
-;Write a function which reverses a sequence
-;
-;restrictions: reverse, rseq
-;
-(defn my-reverse
-  "Reverses the order of elements in col."
-  [col]
-  (reduce (fn [result x]
-            (conj result x))
-          '()
-          col))
+;; p23: Reverse a Sequence
 
-;tests
-(= (my-reverse [1 2 3 4 5]) [5 4 3 2 1])
-(= (my-reverse (sorted-set 5 7 2 7)) '(7 5 2))
-(= (my-reverse [[1 2] [3 4] [5 6]]) [[5 6] [3 4] [1 2]])
+;; Write a function which reverses a sequence
+;; restrictions: reverse, rseq
+
+(defn reverse-seq
+  [xs]
+  (into '() xs))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-seq [1 2 3 4 5]) [5 4 3 2 1]))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-seq (sorted-set 5 7 2 7)) '(7 5 2)))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-seq [[1 2] [3 4] [5 6]]) [[5 6] [3 4] [1 2]]))))
