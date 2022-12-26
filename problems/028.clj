99e1415474d8361beabf307c96514a16d69ff8d2
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-27 01:58:12 +0200

    problem 28: remove old solution

--- problems/028.clj
+++ problems/028.clj
@@ -5,15 +5,6 @@
 
 (defn make-flat
   [xs]
-  (reduce (fn [result x]
-            (concat result (if (sequential? x)
-                             (make-flat x)
-                             (list x))))
-          '()
-          xs))
-
-(defn make-flat2
-  [xs]
   (reduce (fn flat- [result x]
             (if (sequential? x)
               (reduce flat- result x)
@@ -21,28 +12,14 @@
           []
           xs))
 
-;; testing make-flat -----------------------------------
-(clojure.test/deftest test1-make-flat
+(clojure.test/deftest test1
   (clojure.test/testing
     (clojure.test/is (= (make-flat '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))))
 
-(clojure.test/deftest test2-make-flat
+(clojure.test/deftest test2
   (clojure.test/testing
     (clojure.test/is (= (make-flat ["a" ["b"] "c"]) '("a" "b" "c")))))
 
-(clojure.test/deftest test3-make-flat
+(clojure.test/deftest test3
   (clojure.test/testing
     (clojure.test/is (= (make-flat '((((:a))))) '(:a)))))
-
-;; testing make-flat2 -----------------------------------
-(clojure.test/deftest test1-make-flat2
-  (clojure.test/testing
-    (clojure.test/is (= (make-flat2 '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))))
-
-(clojure.test/deftest test2-make-flat2
-  (clojure.test/testing
-    (clojure.test/is (= (make-flat2 ["a" ["b"] "c"]) '("a" "b" "c")))))
-
-(clojure.test/deftest test3-make-flat2
-  (clojure.test/testing
-    (clojure.test/is (= (make-flat2 '((((:a))))) '(:a)))))
