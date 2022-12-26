cb9c6c74a2c15222938870b9381749f57b5f1ebe
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-26 13:09:42 +0200

    problem 28: refactoring, add new solution

--- problems/028.clj
+++ problems/028.clj
@@ -1,20 +1,48 @@
-;p28: Flatten a Sequence
-;Write a function which flattens a sequence
-;
-;restrictions: flatten
-;
-(defn my-flatten
-  "Flattens col."
-  [col]
-  (reduce (fn [result x]
-            (concat result
-                    (if (sequential? x)
-                      (my-flatten x)
-                      (list x))))
-          '()
-          col))
+;; p28: Flatten a Sequence
 
-;tests
-(= (my-flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
-(= (my-flatten ["a" ["b"] "c"]) '("a" "b" "c"))
-(= (my-flatten '((((:a))))) '(:a))
+;; Write a function which flattens a sequence
+;; restrictions: flatten
+
+(defn make-flat
+  [xs]
+  (reduce (fn [result x]
+            (concat result (if (sequential? x)
+                             (make-flat x)
+                             (list x))))
+          '()
+          xs))
+
+(defn make-flat2
+  [xs]
+  (reduce (fn flat- [result x]
+            (if (sequential? x)
+              (reduce flat- result x)
+              (conj result x)))
+          []
+          xs))
+
+;; testing make-flat -----------------------------------
+(clojure.test/deftest test1-make-flat
+  (clojure.test/testing
+    (clojure.test/is (= (make-flat '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))))
+
+(clojure.test/deftest test2-make-flat
+  (clojure.test/testing
+    (clojure.test/is (= (make-flat ["a" ["b"] "c"]) '("a" "b" "c")))))
+
+(clojure.test/deftest test3-make-flat
+  (clojure.test/testing
+    (clojure.test/is (= (make-flat '((((:a))))) '(:a)))))
+
+;; testing make-flat2 -----------------------------------
+(clojure.test/deftest test1-make-flat2
+  (clojure.test/testing
+    (clojure.test/is (= (make-flat2 '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)))))
+
+(clojure.test/deftest test2-make-flat2
+  (clojure.test/testing
+    (clojure.test/is (= (make-flat2 ["a" ["b"] "c"]) '("a" "b" "c")))))
+
+(clojure.test/deftest test3-make-flat2
+  (clojure.test/testing
+    (clojure.test/is (= (make-flat2 '((((:a))))) '(:a)))))
