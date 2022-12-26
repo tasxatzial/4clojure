d4357cd9af26c57198f41217f1ab8e95dedc3a67
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-26 02:53:39 +0200

    problem 25: refactoring

--- problems/025.clj
+++ problems/025.clj
@@ -1,13 +1,23 @@
-;p25: Find the odd numbers
-;Write a function which returns only the odd numbers from a sequence
-;
-(defn find-odd
-  "Returns a col that consists of the odd numbers only."
-  [col]
-  (filter #(= 1 (mod % 2)) col))
+;; p25: Find the odd numbers
 
-;tests
-(= (find-odd #{1 2 3 4 5}) '(1 3 5))
-(= (find-odd [4 2 1 6]) '(1))
-(= (find-odd [2 2 4 6]) '())
-(= (find-odd [1 1 1 3]) '(1 1 1 3))
+;; Write a function which returns only the odd numbers from a sequence
+
+(defn find-odd
+  [xs]
+  (filter odd? xs))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (find-odd #{1 2 3 4 5}) '(1 3 5)))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (find-odd [4 2 1 6]) '(1)))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (find-odd [2 2 4 6]) '()))))
+
+(clojure.test/deftest test4
+  (clojure.test/testing
+    (clojure.test/is (= (find-odd [1 1 1 3]) '(1 1 1 3)))))
