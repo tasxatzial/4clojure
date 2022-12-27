a85b53a1a5605cc359e63c6d4a6c09ce33f9591f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-27 19:23:23 +0200

    problem 43: refactoring

--- problems/043.clj
+++ problems/043.clj
@@ -1,28 +1,40 @@
-;p43: Reverse Interleave
-;Write a function which reverses the interleave process into x number of subsequences
-;
-;Assume that the total number of elements in the sequence is a multiple of x
-;
+;; p43: Reverse Interleave
 
-;solution 1
-(defn rev-interleave1
-  "Reverses the interleave process into x number of subsequences."
-  [col N]
-  (let [partitioned (partition N col)
-        interleaved (apply interleave partitioned)]
-    (partition (count partitioned) interleaved)))
+;; Write a function which reverses the interleave process into N number of subsequences
 
-;solution 2
-(defn rev-interleave2
-  "Reverses the interleave process into x number of subsequences."
-  [col N]
-  (apply map list (partition N col)))
+(defn reverse-interleave
+  [xs N]
+  (let [partitioned (partition N xs)]
+    (->> partitioned
+         (apply interleave)
+         (partition (count partitioned)))))
 
-;tests
-(= (rev-interleave1 [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
-(= (rev-interleave1 (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
-(= (rev-interleave1 (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
+(defn reverse-interleave2
+  [xs N]
+  (apply map list (partition N xs)))
 
-(= (rev-interleave2 [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
-(= (rev-interleave2 (range 9) 3) '((0 3 6) (1 4 7) (2 5 8)))
-(= (rev-interleave2 (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))
+;; testing reverse-interleave -----------------------------------
+(clojure.test/deftest test1-reverse-interleave
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-interleave [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6))))))
+
+(clojure.test/deftest test2-reverse-interleave
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-interleave (range 9) 3) '((0 3 6) (1 4 7) (2 5 8))))))
+
+(clojure.test/deftest test3-reverse-interleave
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-interleave (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9))))))
+
+;; testing reverse-interleave2 -----------------------------------
+(clojure.test/deftest test1-reverse-interleave2
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-interleave2 [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6))))))
+
+(clojure.test/deftest test2-reverse-interleave2
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-interleave2 (range 9) 3) '((0 3 6) (1 4 7) (2 5 8))))))
+
+(clojure.test/deftest test3-reverse-interleave2
+  (clojure.test/testing
+    (clojure.test/is (= (reverse-interleave2 (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9))))))
