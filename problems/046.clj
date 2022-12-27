460b7a3df29c0507ca27a9e691a21ec028b4f846
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-27 19:33:10 +0200

    problem 46: refactoring

--- problems/046.clj
+++ problems/046.clj
@@ -1,14 +1,24 @@
-;p46: Flipping out
-;Write a higher-order function which flips the order of the arguments of an input function
-;
+;; p46: Flipping out
+
+;; Write a higher-order function which flips the order of the arguments of an input function
+
 (defn flip-args
-  "Flips the order of the arguments of an input function."
   [f]
   (fn [arg1 arg2]
     (f arg2 arg1)))
 
-;tests
-(= 3 ((flip-args nth) 2 [1 2 3 4 5]))
-(= true ((flip-args >) 7 8))
-(= 4 ((flip-args quot) 2 8))
-(= [1 2 3] ((flip-args take) [1 2 3 4 5] 3))
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= 3 ((flip-args nth) 2 [1 2 3 4 5])))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= true ((flip-args >) 7 8)))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= 4 ((flip-args quot) 2 8)))))
+
+(clojure.test/deftest test4
+  (clojure.test/testing
+    (clojure.test/is (= [1 2 3] ((flip-args take) [1 2 3 4 5] 3)))))
