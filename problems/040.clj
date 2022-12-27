160ddda9c19028311d6814894b8d5eb52b92b14a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-27 18:32:14 +0200

    problem 40: refactoring

--- problems/040.clj
+++ problems/040.clj
@@ -1,14 +1,20 @@
-;p40: Interpose a Seq
-;Write a function which separates the items of a sequence by an arbitrary value
-;
-;restrictions: interpose
-;
-(defn my-interpose
-  "Interposes a sequence by N."
-  [N col]
-  (butlast (apply concat (map #(vector % N) col))))
+;; p40: Interpose a Seq
 
-;tests
-(= (my-interpose 0 [1 2 3]) [1 0 2 0 3])
-(= (apply str (my-interpose ", " ["one" "two" "three"])) "one, two, three")
-(= (my-interpose :z [:a :b :c :d]) [:a :z :b :z :c :z :d])
+;; Write a function which separates the items of a sequence by an arbitrary value
+;; restrictions: interpose
+
+(defn interpose-seq
+  [N xs]
+  (butlast (reduce into [] (map #(vector % N) xs))))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (interpose-seq 0 [1 2 3]) [1 0 2 0 3]))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (apply str (interpose-seq ", " ["one" "two" "three"])) "one, two, three"))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (interpose-seq :z [:a :b :c :d]) [:a :z :b :z :c :z :d]))))
