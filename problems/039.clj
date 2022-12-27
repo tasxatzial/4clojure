fbe40cc3e4db3f3593b354ebbcd88704f84b20f3
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-27 02:21:02 +0200

    problem 39: refactoring

--- problems/039.clj
+++ problems/039.clj
@@ -1,16 +1,20 @@
-;p39: Interleave Two Seqs
-;Write a function which takes two sequences and returns the first item from each, then the second item from each,
-;then the third, etc
-;
-;restrictions: interleave
-;
-(defn my-interleave
-  "Interleaves two sequences."
-  [col1 col2]
-  (reduce concat (map #(vector %1 %2) col1 col2)))
+;; p39: Interleave Two Seqs
 
-;tests
-(= (my-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
-(= (my-interleave [1 2] [3 4 5 6]) '(1 3 2 4))
-(= (my-interleave [1 2 3 4] [5]) [1 5])
-(= (my-interleave [30 20] [25 15]) [30 25 20 15])
+;; Write a function which takes two sequences and returns the first item from each, then the second item from each, then the third, etc
+;; restrictions: interleave
+
+(defn interleave-seq
+  [xs1 xs2]
+  (reduce into [] (map #(vector %1 %2) xs1 xs2)))
+
+(clojure.test/deftest test1
+  (clojure.test/testing
+    (clojure.test/is (= (interleave-seq [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c)))))
+
+(clojure.test/deftest test2
+  (clojure.test/testing
+    (clojure.test/is (= (interleave-seq [1 2] [3 4 5 6]) '(1 3 2 4)))))
+
+(clojure.test/deftest test3
+  (clojure.test/testing
+    (clojure.test/is (= (interleave-seq [1 2 3 4] [5]) [1 5]))))
