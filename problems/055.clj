231710bdef0871b045a67874ddae36521c95f273
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-29 03:16:31 +0200

    problem 55: refactoring, fix error in old solution, add new solution

--- problems/055.clj
+++ problems/055.clj
@@ -1,19 +1,37 @@
-;p55: Count Occurrences
-;Write a function which returns a map containing the number of occurrences of each distinct item in a sequence
-;
-;restrictions: frequencies
-;
-(defn my-frequencies
-  "Returns a map containing the number of occurrences of each distinct item in a sequence."
-  [col]
-  (reduce (fn [result x]
-            (if (result x)
-              (assoc result x (inc (result x)))
-              (conj result x 1)))
-          {}
-          col))
+;; p55: Count Occurrences
 
-;tests
-(= (my-frequencies [1 1 2 3 2 1 1]) {1 4, 2 2, 3 1})
-(= (my-frequencies [:b :a :b :a :b]) {:a 2, :b 3})
-(= (my-frequencies '([1 2] [1 3] [1 3])) {[1 2] 1, [1 3] 2})
+;; Write a function which returns a map containing the number of
+;; occurrences of each distinct item in a sequence
+;; restrictions: frequencies
+
+(defn frequencies-seq
+  [xs]
+  (reduce (fn [result x]
+            (if (contains? result x)
+              (update result x inc)
+              (assoc result x 1)))
+          {}
+          xs))
+
+(defn frequencies-seq2
+  [xs]
+  (->> xs
+       (group-by identity)
+       (map #(vector (first %) (count (second %))))
+       (into {})))
+
+(deftest tests-frequencies-seq
+  (testing "test1"
+    (is (= (frequencies-seq [1 1 2 3 2 1 1]) {1 4, 2 2, 3 1})))
+  (testing "test2"
+    (is (= (frequencies-seq [:b :a :b :a :b]) {:a 2, :b 3})))
+  (testing "test3"
+    (is (= (frequencies-seq '([1 2] [1 3] [1 3])) {[1 2] 1, [1 3] 2}))))
+
+(deftest tests-frequencies-seq2
+  (testing "test1"
+    (is (= (frequencies-seq2 [1 1 2 3 2 1 1]) {1 4, 2 2, 3 1})))
+  (testing "test2"
+    (is (= (frequencies-seq2 [:b :a :b :a :b]) {:a 2, :b 3})))
+  (testing "test3"
+    (is (= (frequencies-seq2 '([1 2] [1 3] [1 3])) {[1 2] 1, [1 3] 2}))))
