53c3dd0b4b7d8a4d5b9706bc844e0fd0854c4149
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-29 03:02:01 +0200

    problem 54: refactoring, remove old solutions & add new one

--- problems/054.clj
+++ problems/054.clj
@@ -1,34 +1,22 @@
-;p54: Partition a Sequence
-;Write a function which returns a sequence of lists of x items each.
-;Lists of less than x items should not be returned
-;
-;restrictions: partition, partition-all
+;; p54: Partition a Sequence
 
-;solution 1: lazy
-(defn my-partition1
-  "Returns a sequence of lists of N items each."
-  ([N col] (my-partition1 N col (Math/floor (/ (count col) N))))
-  ([N col subseqs-count]
-   (let [rest-subseqs (lazy-seq (my-partition1 N (drop N col) subseqs-count))]
-     (take subseqs-count (concat (list (take N col)) rest-subseqs)))))
+;; Write a function which returns a sequence of lists of N items each.
+;; Lists of less than N items should not be returned.
+;; restrictions: partition, partition-all
 
-;solution 2: recursion
-(defn my-partition2
-  "Returns a sequence of lists of N items each."
-  [N col]
-  ((fn my-partition
-     [result col]
-     (if (< (count col) N)
-       result
-       (recur (concat result (list (take N col))) (drop N col))))
-   '()
-   col))
+(defn partition-seq
+  [N xs]
+  (loop [xs- xs
+         result []]
+    (let [first-N (take N xs-)]
+      (if (= N (count first-N))
+        (recur (drop N xs-) (conj result first-N))
+        result))))
 
-;tests
-(= (my-partition1 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))
-(= (my-partition1 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))
-(= (my-partition1 3 (range 8)) '((0 1 2) (3 4 5)))
-
-(= (my-partition2 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))
-(= (my-partition2 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))
-(= (my-partition2 3 (range 8)) '((0 1 2) (3 4 5)))
+(deftest tests
+  (testing "test1"
+    (is (= (partition-seq 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))))
+  (testing "test2"
+    (is (= (partition-seq 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))))
+  (testing "test3"
+    (is (= (partition-seq 3 (range 8)) '((0 1 2) (3 4 5))))))
