465e36a6e806e932fa64d4f664b0c3df730ab246
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-29 03:52:45 +0200

    problem 59: refactoring

--- problems/059.clj
+++ problems/059.clj
@@ -1,17 +1,19 @@
-;p59: Juxtaposition
-;Take a set of functions and return a new function that takes a variable number of
-;arguments and returns a sequence containing the result of applying each function
-;left-to-right to the argument list
-;
-;restrictions: juxt
+;; p59: Juxtaposition
 
-(defn my-juxt
-  "Takes a set of functions and returns a fn that is the juxtaposition of those fns."
-  [& f-args]
+;; Take a set of functions and return a new function that takes a variable number of
+;; arguments and returns a sequence containing the result of applying each function
+;; left-to-right to the argument list.
+;; restrictions: juxt
+
+(defn juxt-fns
+  [& fns]
   (fn [& args]
-    (map #(apply % args) f-args)))
+    (map #(apply % args) fns)))
 
-;tests
-(= [21 6 1] ((my-juxt + max min) 2 3 5 1 6 4))
-(= ["HELLO" 5] ((my-juxt #(.toUpperCase %) count) "hello"))
-(= [2 6 4] ((my-juxt :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))
+(deftest tests
+  (testing "test1"
+    (is (= [21 6 1] ((juxt-fns + max min) 2 3 5 1 6 4))))
+  (testing "test2"
+    (is (= ["HELLO" 5] ((juxt-fns #(.toUpperCase %) count) "hello"))))
+  (testing "test3"
+    (is (= [2 6 4] ((juxt-fns :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10})))))
