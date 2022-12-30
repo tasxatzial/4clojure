2d4c66b39204097cb9602421fb70da1a339df00d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-31 00:57:35 +0200

    problem 28: add lazy solution

--- problems/028.clj
+++ problems/028.clj
@@ -15,10 +15,28 @@
           []
           xs))
 
-(deftest tests
+;; lazy
+(defn make-flat2
+  [xs]
+  (lazy-seq
+    (when (seq xs)
+      (let [[x & rest-xs] xs]
+        (if (sequential? x)
+          (concat (make-flat2 x) (make-flat2 rest-xs))
+          (cons x (make-flat2 rest-xs)))))))
+
+(deftest tests-make-flat
   (testing "test1"
     (is (= (make-flat '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))))
   (testing "test2"
     (is (= (make-flat ["a" ["b"] "c"]) '("a" "b" "c"))))
   (testing "test3"
     (is (= (make-flat '((((:a))))) '(:a)))))
+
+(deftest tests-make-flat2
+  (testing "test1"
+    (is (= (make-flat2 '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))))
+  (testing "test2"
+    (is (= (make-flat2 ["a" ["b"] "c"]) '("a" "b" "c"))))
+  (testing "test3"
+    (is (= (make-flat2 '((((:a))))) '(:a)))))
