832e738bed957b17fc35cdeb8bad1eba1a4c1ece
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-30 23:05:38 +0200

    problem 54: add lazy solution

--- problems/054.clj
+++ problems/054.clj
@@ -13,10 +13,26 @@
         (recur (drop N xs-) (conj result first-N))
         result))))
 
-(deftest tests
+;; lazy
+(defn partition-seq2
+  [N xs]
+  (lazy-seq
+    (let [first-N (take N xs)]
+      (when (= N (count first-N))
+        (cons first-N (partition-seq2 N (drop N xs)))))))
+
+(deftest tests-partition-seq
   (testing "test1"
     (is (= (partition-seq 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))))
   (testing "test2"
     (is (= (partition-seq 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))))
   (testing "test3"
     (is (= (partition-seq 3 (range 8)) '((0 1 2) (3 4 5))))))
+
+(deftest tests-partition-seq2
+  (testing "test1"
+    (is (= (partition-seq2 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))))
+  (testing "test2"
+    (is (= (partition-seq2 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))))
+  (testing "test3"
+    (is (= (partition-seq2 3 (range 8)) '((0 1 2) (3 4 5))))))
