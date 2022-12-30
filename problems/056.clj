a711c2190a099169e40a25309821343c8ee4a18f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-31 01:10:34 +0200

    problem 56: add lazy solution

--- problems/056.clj
+++ problems/056.clj
@@ -15,7 +15,19 @@
              new-result (conj result (first xs))]
          (recur (rest xs) new-set-xs new-result))))))
 
-(deftest tests
+;; lazy
+(defn remove-duplicates2
+  ([xs]
+   (remove-duplicates2 xs #{}))
+  ([xs set-xs]
+   (lazy-seq
+     (when (seq xs)
+       (let [[x & rest-xs] xs]
+         (if (contains? set-xs x)
+           (remove-duplicates2 rest-xs set-xs)
+           (cons x (remove-duplicates2 rest-xs (conj set-xs x)))))))))
+
+(deftest tests-remove-duplicates
   (testing "test1"
     (is (= (remove-duplicates [1 2 1 3 1 2 4]) [1 2 3 4])))
   (testing "test2"
@@ -24,3 +36,13 @@
     (is (= (remove-duplicates '([2 4] [1 2] [1 3] [1 3])) '([2 4] [1 2] [1 3]))))
   (testing "test4"
     (is (= (remove-duplicates (range 50)) (range 50)))))
+
+(deftest tests-remove-duplicates2
+  (testing "test1"
+    (is (= (remove-duplicates2 [1 2 1 3 1 2 4]) [1 2 3 4])))
+  (testing "test2"
+    (is (= (remove-duplicates2 [:a :a :b :b :c :c]) [:a :b :c])))
+  (testing "test3"
+    (is (= (remove-duplicates2 '([2 4] [1 2] [1 3] [1 3])) '([2 4] [1 2] [1 3]))))
+  (testing "test4"
+    (is (= (remove-duplicates2 (range 50)) (range 50)))))
