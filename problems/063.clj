6ddb46e9c491067d52b7395fd166b39e716f357c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-31 19:00:23 +0200

    problem 63: refactoring

--- problems/063.clj
+++ problems/063.clj
@@ -1,32 +1,28 @@
-;p63: Group a Sequence
-;Given a function f and a sequence s, write a function which returns a map.
-;The keys should be the values of f applied to each item in s.
-;The value at each key should be a vector of corresponding items in the order they appear in s
-;
-;restrictions: group-by
-;
-(defn map-f
-  "Maps func to each item in col and returns [x, func(x)] for each item in col."
-  [func col]
-  (into [] (map #(vector % (func %)) col)))
+;; p63: Group a Sequence
 
-(defn my-group-by
-  "Returns a map of the elements of col keyed by the result of
-  func on each element. The value at each key will be a vector of the
-  corresponding elements, in the order they appeared in col."
-  [func col]
+;; Given a function f and a sequence s, write a function which returns a map.
+;; The keys should be the values of f applied to each item in s.
+;; The value at each key should be a vector of corresponding items in the order
+;; they appear in s.
+;; restrictions: group-by
+
+(defn group-seq
+  [f xs]
   (reduce (fn [result x]
-            (let [func-el (get x 1)
-                  el (get x 0)]
-              (if (contains? result func-el)
-                (assoc result func-el (conj (result func-el) el))
-                (assoc result func-el [el]))))
+            (let [fx (f x)]
+              (if (contains? result fx)
+                (update result fx conj x)
+                (assoc result fx [x]))))
           {}
-          (map-f func col)))
+          xs))
 
-;tests
-(= (my-group-by #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})
-(= (my-group-by #(apply / %) [[1 2] [2 4] [4 6] [3 6]])
-   {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})
-(= (my-group-by count [[1] [1 2] [3] [1 2 3] [2 3]])
-   {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]})
+(deftest tests
+  (testing "test1"
+    (is (= (group-seq #(> % 5) [1 3 6 8])
+           {false [1 3], true [6 8]})))
+  (testing "test2"
+    (is (= (group-seq #(apply / %) [[1 2] [2 4] [4 6] [3 6]])
+           {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})))
+  (testing "test3"
+    (is (= (group-seq count [[1] [1 2] [3] [1 2 3] [2 3]])
+           {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]}))))
