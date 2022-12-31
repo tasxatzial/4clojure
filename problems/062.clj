724250ac86652f5282b0b22d911e07e85f74d44a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-31 02:58:35 +0200

    problem 62: refactoring

--- problems/062.clj
+++ problems/062.clj
@@ -1,15 +1,18 @@
-;p62: Re-implement Iterate
-;Given a side-effect free function f and an initial value x write a function which
-;returns an infinite lazy sequence of x, (f x), (f (f x)), (f (f (f x))), etc
-;
-;restrictions: iterate
-;
-(defn my-iterate
-  "Returns a lazy sequence of x, (f x), (f (f x)) etc. f must be free of side-effects."
-  [func i]
-  (cons i (lazy-seq (my-iterate func (func i)))))
+;; p62: Re-implement Iterate
 
-;tests
-(= (take 5 (my-iterate #(* 2 %) 1)) [1 2 4 8 16])
-(= (take 100 (my-iterate inc 0)) (take 100 (range)))
-(= (take 9 (my-iterate #(inc (mod % 3)) 1)) (take 9 (cycle [1 2 3])))
+;; Given a side-effect free function f and an initial value x write a function which
+;; returns an infinite lazy sequence of x, (f x), (f (f x)), (f (f (f x))), etc.
+;; restrictions: iterate
+
+(defn iterate-fn
+  [f val]
+  (lazy-seq
+    (cons val (iterate-fn f (f val)))))
+
+(deftest tests
+  (testing "test1"
+    (is (= (take 5 (iterate-fn #(* 2 %) 1)) [1 2 4 8 16])))
+  (testing "test2"
+    (is (= (take 100 (iterate-fn inc 0)) (take 100 (range)))))
+  (testing "test3"
+    (is (= (take 9 (iterate-fn #(inc (mod % 3)) 1)) (take 9 (cycle [1 2 3]))))))
