6555e4fcfdd88b0b0b2a7afd865468c4de90eb45
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-31 02:52:16 +0200

    problem 61: refactoring

--- problems/061.clj
+++ problems/061.clj
@@ -1,13 +1,15 @@
-;p61: Map Construction
-;Write a function which takes a vector of keys and a vector of values and constructs a map from them
-;
-;restrictions: zipmap
-;
-(defn my-zipmap
-  "Takes a vector of keys and a vector of values and constructs a map from them."
+;; p61: Map Construction
+
+;; Write a function which takes a vector of keys and a vector of values and constructs
+;; a map from them.
+;; restrictions: zipmap
+
+(defn zip-map
   [col1 col2]
   (into {} (map #(vector %1 %2) col1 col2)))
 
-;tests
-(= (my-zipmap [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3})
-(= (my-zipmap [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"})
+(deftest tests
+  (testing "test1"
+    (is (= (zip-map [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3})))
+  (testing "test2"
+    (is (= (zip-map [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"}))))
