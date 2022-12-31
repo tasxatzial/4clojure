63d623a13adfd8603cca3824bbc922cc68ca9112
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-12-31 19:34:55 +0200

    problem 41: refactoring

--- problems/041.clj
+++ problems/041.clj
@@ -5,24 +5,30 @@
 (ns p41.core
   (:require [clojure.test :refer [deftest testing is]]))
 
+(defn nth-element?
+  [N]
+  #(zero? (mod (inc %) N)))
+
+;; lazy
 (defn drop-every-nth
   [xs N]
-  (letfn [(not-nth-element? [idx]
-            (pos? (mod (inc idx) N)))]
-    (keep-indexed #(if (not-nth-element? %1) %2)
+  (let [nth-element? (nth-element? N)]
+    (keep-indexed #(if (not (nth-element? %1)) %2)
                   xs)))
 
 (defn drop-every-nth2
-  ([xs N]
-   (drop-every-nth2 xs N 0 []))
-  ([xs N idx result]
-   (if (empty? xs)
-     result
-     (if (= 0 (mod (inc idx) N))
-       (recur (rest xs) N (inc idx) result)
-       (recur (rest xs) N (inc idx) (conj result (first xs)))))))
+  [xs N]
+  (let [nth-element? (nth-element? N)]
+    (loop [xs- xs
+           idx 0
+           result []]
+      (if (empty? xs-)
+        result
+        (if (nth-element? idx)
+          (recur (rest xs-) (inc idx) result)
+          (recur (rest xs-) (inc idx) (conj result (first xs-))))))))
 
-(deftest -drop-every-nth
+(deftest tests-drop-every-nth
   (testing "test1"
     (is (= (drop-every-nth [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8])))
   (testing "test2"
@@ -30,7 +36,7 @@
   (testing "test3"
     (is (= (drop-every-nth [1 2 3 4 5 6] 4) [1 2 3 5 6]))))
 
-(deftest test1-drop-every-nth2
+(deftest tests-drop-every-nth2
   (testing "test1"
     (is (= (drop-every-nth2 [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8])))
   (testing "test2"
