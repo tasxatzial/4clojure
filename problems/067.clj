931f5d70f91a2fb6bd32c4732dce77c6ac765620
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-01 04:59:27 +0200

    problem 67: refactoring & optimize current solution

--- problems/067.clj
+++ problems/067.clj
@@ -1,17 +1,21 @@
-;p67: Prime Numbers
-;Write a function which returns the first x number of prime numbers
-;
-(defn my-primes
-  "Returns the first x prime numbers."
-  ([x] (my-primes x [] 2))
-  ([x result N]
-   (if (= x 0)
-     result
-     (if (contains? (set (map #(mod N %) result)) 0)
-       (recur x result (inc N)) ; N is composite
-       (recur (dec x) (conj result N) (inc N)))))) ; N is prime
+;; p67: Prime Numbers
 
-;tests
-(= (my-primes 2) [2 3])
-(= (my-primes 5) [2 3 5 7 11])
-(= (last (my-primes 100)) 541)
+;; Write a function which returns the first x number of prime numbers
+
+(defn my-primes
+  ([x]
+   (my-primes x [] 2))
+  ([x result N]
+   (if (zero? x)
+     result
+     (if (some #(zero? (mod N %)) result)
+       (recur x result (inc N))
+       (recur (dec x) (conj result N) (inc N))))))
+
+(deftest tests
+  (testing "test1"
+    (is (= (my-primes 2) [2 3])))
+  (testing "test2"
+    (is (= (my-primes 5) [2 3 5 7 11])))
+  (testing "test3"
+    (is (= (last (my-primes 100)) 541))))
