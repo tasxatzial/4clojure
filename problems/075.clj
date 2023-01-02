5c8b547594b0ba806550430df0663302760910f5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-02 19:25:45 +0200

    problem 75: refactoring

--- problems/075.clj
+++ problems/075.clj
@@ -1,26 +1,34 @@
-;p75: Euler's Totient Function
-;Two numbers are coprime if their greatest common divisor equals 1. Euler's totient function f(x)
-;is defined as the number of positive integers less than x which are coprime to x. The special
-;case f(1) equals 1. Write a function which calculates Euler's totient function
-;
-(defn my-gcd
+;; p75: Euler's Totient Function
+
+;; Two numbers are coprime if their greatest common divisor equals 1. Euler's totient
+;; function f(x) is defined as the number of positive integers less than x which are
+;; coprime to x. The special case f(1) equals 1.
+;; Write a function which calculates Euler's totient function.
+
+(ns p75.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn get-gcd
   "Returns the GCD of N1, N2."
   [N1 N2]
   (if (= 0 N2)
     N1
-    (my-gcd N2 (mod N1 N2))))
+    (recur N2 (mod N1 N2))))
 
-(defn totient
-  "Calculates the number of positive integers less than N which are coprime to N.
-   The special case totient(1) equals 1."
+(defn get-euler-totient
   [N]
   (if (= 1 N)
     1
-    (let [gcds (map my-gcd (range 1 N) (repeat N))]
-      (count (filter #(= 1 %) gcds)))))
+    (let [get-N-gcd (partial get-gcd N)
+          gcds (map get-N-gcd (range 1 N))]
+      (count (filter #{1} gcds)))))
 
-;tests
-(= (totient 1) 1)
-(= (totient 10) (count '(1 3 7 9)) 4)
-(= (totient 40) 16)
-(= (totient 99) 60)
+(deftest tests
+  (testing "test1"
+    (is (= (get-euler-totient 1) 1)))
+  (testing "test2"
+    (is (= (get-euler-totient 10) (count '(1 3 7 9)) 4)))
+  (testing "test3"
+    (is (= (get-euler-totient 40) 16)))
+  (testing "test4"
+    (is (= (get-euler-totient 99) 60))))
