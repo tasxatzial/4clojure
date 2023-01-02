934d549b613714b82904c45741de508b6d67f68f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-02 04:19:59 +0200

    problem 70: refactoring, remove old solution & add new one

--- problems/070.clj
+++ problems/070.clj
@@ -1,17 +1,24 @@
-;p70: Word Sorting
-;Write a function that splits a sentence up into a sorted list of words. Capitalization should not affect sort
-;order and punctuation should be ignored
-;
-(defn word-split
-  "Splits a sentence up into a sorted list of words."
-  [s]
-  (let [no-punctuation (clojure.string/replace s #"[^A-Za-z ]" "")]
-    (sort-by clojure.string/lower-case (clojure.string/split no-punctuation #" "))))
+;; p70: Word Sorting
 
-;tests
-(= (word-split  "Have a nice day.")
-   ["a" "day" "Have" "nice"])
-(= (word-split  "Clojure is a fun language!")
-   ["a" "Clojure" "fun" "is" "language"])
-(= (word-split  "Fools fall for foolish follies.")
-   ["fall" "follies" "foolish" "Fools" "for"])
+;; Write a function that splits a sentence up into a sorted list of words.
+;; Capitalization should not affect sort order and punctuation should be ignored
+
+(defn word-split
+  [s]
+  (re-seq #"\w+" s))
+
+(defn word-sort
+  [s]
+  (->> s 
+       word-split 
+       (sort-by clojure.string/lower-case)))
+
+(deftest tests
+  (testing "test1"
+    (is (= (word-sort  "Have a nice day.") ["a" "day" "Have" "nice"])))
+  (testing "test2"
+    (is (= (word-sort  "Clojure is a fun language!")
+           ["a" "Clojure" "fun" "is" "language"])))
+  (testing "test3"
+    (is (= (word-sort  "Fools fall for foolish follies.")
+           ["fall" "follies" "foolish" "Fools" "for"]))))
