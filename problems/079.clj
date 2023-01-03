2a1de7b3468398d1537257df27e94dc042b61931
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-03 17:11:26 +0200

    problem 79: refactoring

--- problems/079.clj
+++ problems/079.clj
@@ -1,27 +1,41 @@
-;p79: Triangle Minimal Path
-;Write a function which calculates the sum of the minimal path through a triangle. The triangle is represented as a
-;collection of vectors. The path should start at the top of the triangle and move to an adjacent number on the next
-;row until the bottom of the triangle is reached
-;
-(defn min-path-sum
-  "Calculates the sum of the minimal path through a triangle."
-  ([col] (min-path-sum col 0 0))
-  ([col outer-idx inner-idx]
-   (if (= (dec (count col)) outer-idx)
-     ((nth col outer-idx) inner-idx)
-     (let [x (min-path-sum col (inc outer-idx) inner-idx)
-           y (min-path-sum col (inc outer-idx) (inc inner-idx))]
-       (+ (min x y) ((nth col outer-idx) inner-idx))))))
+;; p79: Triangle Minimal Path
 
-;tests
-(= 7 (min-path-sum '([1]
-                     [2 4]
-                     [5 1 4]
-                     [2 3 4 5]))) ; 1->2->1->3
+;; Write a function which calculates the sum of the minimal path through a triangle.
+;; The triangle is represented as a vector of vectors. The path should start at
+;; the top of the triangle and move to an adjacent number on the next row until the
+;; bottom of the triangle is reached.
 
-(= 20 (min-path-sum '([3]
-                      [2 4]
-                      [1 9 3]
-                      [9 9 2 4]
-                      [4 6 6 7 8]
-                      [5 7 3 5 1 4]))) ; 3->4->3->2->7->1
+(ns p79.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn last-row?
+  [triangle row]
+  (= (dec (count triangle)) row))
+
+(defn get-val
+  [triangle row col]
+  (get-in triangle [row col]))
+
+(defn sum-min-path
+  ([triangle]
+   (sum-min-path triangle 0 0))
+  ([triangle row col]
+   (if (last-row? triangle row)
+     (get-val triangle row col)
+     (let [x (sum-min-path triangle (inc row) col)
+           y (sum-min-path triangle (inc row) (inc col))]
+       (+ (min x y) (get-val triangle row col))))))
+
+(deftest tests
+  (testing "test1"
+    (is (= 7 (sum-min-path [   [1]
+                              [2 4]
+                             [5 1 4]
+                            [2 3 4 5]]))))
+  (testing "test2"
+    (is (= 20 (sum-min-path [     [3]
+                                 [2 4]
+                                [1 9 3]
+                               [9 9 2 4]
+                              [4 6 6 7 8]
+                             [5 7 3 5 1 4]])))))
