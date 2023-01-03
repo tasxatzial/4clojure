0c6bf88bfa96d265f53333b01470c0e810df12a6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-03 23:19:43 +0200

    problem 83: refactoring

--- problems/083.clj
+++ problems/083.clj
@@ -1,16 +1,27 @@
-;p83: A Half-Truth
-;Write a function which takes a variable number of booleans. Your function should
-;return true if some of the parameters are true, but not all of the parameters are true.
-;Otherwise your function should return false
-;
+;; p83: A Half-Truth
+
+;; Write a function which takes a variable number of booleans. Your function should
+;; return true if some of the parameters are true, but not all of the parameters are
+;; true. Otherwise your function should return false.
+
+(ns p83.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
 (defn half-truth
   [& args]
   (and (or (some true? args) false) (or (some false? args) false)))
 
-;tests
-(= false (half-truth false false))
-(= true (half-truth true false))
-(= false (half-truth true))
-(= true (half-truth false true false))
-(= false (half-truth true true true))
-(= true (half-truth true true true false))
+(deftest tests
+  (testing "test1"
+    (is (= false (half-truth false false))))
+  (testing "test2"
+    (is (= true (half-truth true false))))
+  (testing "test3"
+    (is (= false (half-truth true))))
+  (testing "test4"
+    (is (= true (half-truth false true false))))
+  (testing "test5"
+    (is (= false (half-truth true true true))))
+  (testing "test6"
+    (is (= true (half-truth true true true false)))))
+
