c3b9ee7c1e4519389350fb79230ed9383d061df9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-07 02:56:34 +0200

    problem 88: refactoring, remove old solution & add two new

--- problems/088.clj
+++ problems/088.clj
@@ -1,20 +1,38 @@
-;p88: Symmetric Difference
-;Write a function which returns the symmetric difference of two sets. The symmetric
-;difference is the set of items belonging to one but not both of the two sets
+;; p88: Symmetric Difference
+
+;; Write a function which returns the symmetric difference of two sets. The symmetric
+;; difference is the set of items belonging to one but not both of the two sets.
+
+(ns p88.core
+  (:require
+    [clojure.set :as set]
+    [clojure.test :refer [deftest testing is]]))
 
 (defn symmetric-difference
-  "Returns the symmetric difference of set1 and set2."
   [set1 set2]
-  (let [intersection (clojure.set/intersection set1 set2)]
-    (reduce (fn [result x]
-              (if (contains? intersection x)
-                result
-                (conj result x)))
-            #{}
-            (into set1 set2))))
+  (set/difference (set/union set1 set2) (set/intersection set1 set2)))
 
-;tests
-(= (symmetric-difference #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7})
-(= (symmetric-difference #{:a :b :c} #{}) #{:a :b :c})
-(= (symmetric-difference #{} #{4 5 6}) #{4 5 6})
-(= (symmetric-difference #{[1 2] [2 3]} #{[2 3] [3 4]}) #{[1 2] [3 4]})
+(defn symmetric-difference2
+  [set1 set2]
+  (let [intersection (set/intersection set1 set2)]
+    (into (set/difference set1 intersection) (set/difference set2 intersection))))
+
+(deftest tests-symmetric-difference
+  (testing "test1"
+    (is (= (symmetric-difference #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7})))
+  (testing "test2"
+    (is (= (symmetric-difference #{:a :b :c} #{}) #{:a :b :c})))
+  (testing "test3"
+    (is (= (symmetric-difference #{} #{4 5 6}) #{4 5 6})))
+  (testing "test4"
+    (is (= (symmetric-difference #{[1 2] [2 3]} #{[2 3] [3 4]}) #{[1 2] [3 4]}))))
+
+(deftest tests-symmetric-difference2
+  (testing "test1"
+    (is (= (symmetric-difference2 #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7})))
+  (testing "test2"
+    (is (= (symmetric-difference2 #{:a :b :c} #{}) #{:a :b :c})))
+  (testing "test3"
+    (is (= (symmetric-difference2 #{} #{4 5 6}) #{4 5 6})))
+  (testing "test4"
+    (is (= (symmetric-difference2 #{[1 2] [2 3]} #{[2 3] [3 4]}) #{[1 2] [3 4]}))))
