813cab53f8f308e07609123a363d6b29201b9521
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-07 02:15:50 +0200

    problem 85: refactoring

--- problems/085.clj
+++ problems/085.clj
@@ -1,26 +1,30 @@
-;p85: Power Set
-;Write a function which generates the power set of a given set. The power set of a
-;set x is the set of all subsets of x, including the empty set and x itself
-;
-(defn set-add
-  "Adds el to every set in col."
-  [col el]
-  (reduce (fn [result x]
-            (conj result (conj x el)))
-          #{}
-          col))
+;; p85: Power Set
+
+;; Write a function which generates the power set of a given set. The power set of a
+;; set x is the set of all subsets of x, including the empty set and x itself.
+
+(ns p85.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn add-to-colls
+  "Adds x to every collection in xs. Returns a set of the updated collections."
+  [xs x]
+  (set (map #(conj % x) xs)))
 
 (defn power-set
-  "Returns the power set of set."
-  [set]
-  (reduce (fn [result x]
-            (into result (conj (set-add result x) #{x})))
+  [xs]
+  (reduce (fn [res x]
+            (into res (conj (add-to-colls res x) #{x})))
           #{#{}}
-          set))
+          xs))
 
-;tests
-(= (power-set #{1 :a}) #{#{1 :a} #{:a} #{} #{1}})
-(= (power-set #{}) #{#{}})
-(= (power-set #{1 2 3})
-   #{#{} #{1} #{2} #{3} #{1 2} #{1 3} #{2 3} #{1 2 3}})
-(= (count (power-set (into #{} (range 10)))) 1024)
+(deftest tests
+  (testing "test1"
+    (is (= (power-set #{1 :a}) #{#{1 :a} #{:a} #{} #{1}})))
+  (testing "test2"
+    (is (= (power-set #{}) #{#{}})))
+  (testing "test3"
+    (is (= (power-set #{1 2 3})
+           #{#{} #{1} #{2} #{3} #{1 2} #{1 3} #{2 3} #{1 2 3}})))
+  (testing "test4"
+    (is (= (count (power-set (into #{} (range 10)))) 1024))))
