a173f46ecd0292e9da63b7fcb2b2fa111417e21d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-07 03:07:54 +0200

    problem 90: refactoring, remove old solution & add new one

--- problems/090.clj
+++ problems/090.clj
@@ -1,28 +1,25 @@
-;p90: Cartesian Product
-;Write a function which calculates the Cartesian product of two sets
-;
-(defn insert-into
-  "Returns a set in which every element x of col is the vector [x el]."
-  [col el]
-  (reduce (fn [result x]
-            (conj result [el x]))
-          #{}
-          col))
+;; p90: Cartesian Product
 
-(defn cartesian-prod
-  "Returns the cartesian product of set1, set2."
+;; Write a function which calculates the Cartesian product of two sets.
+
+(ns p90.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn cartesian-product
   [set1 set2]
-  (reduce (fn [result x]
-            (into result (insert-into set2 x)))
-          #{}
-          set1))
+  (set (for [x set1
+             y set2]
+         [x y])))
 
-;tests
-(= (cartesian-prod #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
-   #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
-     ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
-     ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})
-(= (cartesian-prod #{1 2 3} #{4 5})
-   #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
-(= 300 (count (cartesian-prod (into #{} (range 10))
-                  (into #{} (range 30)))))
+(deftest tests
+  (testing "test1"
+    (is (= (cartesian-product #{"ace" "king" "queen"} #{"♠" "♥" "♦" "♣"})
+           #{["ace"   "♠"] ["ace"   "♥"] ["ace"   "♦"] ["ace"   "♣"]
+             ["king"  "♠"] ["king"  "♥"] ["king"  "♦"] ["king"  "♣"]
+             ["queen" "♠"] ["queen" "♥"] ["queen" "♦"] ["queen" "♣"]})))
+  (testing "test2"
+    (is (= (cartesian-product #{1 2 3} #{4 5})
+           #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})))
+  (testing "test3"
+    (is (= 300 (count (cartesian-product (into #{} (range 10))
+                                         (into #{} (range 30))))))))
