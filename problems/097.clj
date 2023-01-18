4436907e7c4b4c1c383f60a0951028c5e63dde11
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-18 23:52:36 +0200

    problem 97: refactoring

--- problems/097.clj
+++ problems/097.clj
@@ -1,37 +1,39 @@
-;p97: Pascal's Triangle
-;Pascal's triangle is a triangle of numbers computed using the following rules:
-;A. The first row is 1.
-;B. Each successive row is computed by adding together adjacent numbers in the row above,
-;   and adding a 1 to the beginning and end of the row.
-;
-;Write a function which returns the nth row of Pascal's Triangle
-;
+;; p97: Pascal's Triangle
+
+;; Pascal's triangle is a triangle of numbers computed using the following rules:
+;; - The first row is 1.
+;; - Each successive row is computed by adding together adjacent numbers in the row
+;;   above, and adding a 1 to the beginning and end of the row.
+;; Write a function which returns the nth row of Pascal's Triangle
+
+(ns p97.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
 (defn next-row
-  "Given a row, it returns the next row of pascal's triangle."
+  "Returns the next row of pascal's triangle."
   [row]
-  (let [zero-append-row (conj row 0)
-        zero-prepend-row (into [0] row)]
-    (apply vector (map + zero-append-row zero-prepend-row))))
+  (let [zero-append (conj row 0)]
+    (mapv +' zero-append (rseq zero-append))))
 
-(defn pascal-triangle
+(defn row
   "Returns the Nth row of pascal's triangle."
-  ([N]
-   (case N
-     1 [1]
-     2 [1 1]
-     (pascal-triangle (dec N) (next-row [1 1]))))
-  ([N row]
-   (if (= N 2)
-     row
-     (recur (dec N) (next-row row)))))
+  [N]
+  (loop [row [1]
+         N N]
+    (if (= N 1)
+      row
+      (recur (next-row row) (dec N)))))
 
-;tests
-(= (pascal-triangle 1) [1])
-(= (map pascal-triangle (range 1 6))
-   [[1]
-    [1 1]
-    [1 2 1]
-    [1 3 3 1]
-    [1 4 6 4 1]])
-(= (pascal-triangle 11)
-   [1 10 45 120 210 252 210 120 45 10 1])
+(deftest tests
+  (testing "test1"
+    (is (= (row 1) [1])))
+  (testing "test2"
+    (is (= (map row (range 1 6))
+           [[1]
+            [1 1]
+            [1 2 1]
+            [1 3 3 1]
+            [1 4 6 4 1]])))
+  (testing "test3"
+    (is (= (row 11)
+           [1 10 45 120 210 252 210 120 45 10 1]))))
