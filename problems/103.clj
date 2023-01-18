76d27c2be4a43747a764e43439642467f013ed1d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-19 01:24:24 +0200

    problem 103: refactoring

--- problems/103.clj
+++ problems/103.clj
@@ -1,36 +1,47 @@
-;p103: Generating k-combinations
-;Given a sequence S consisting of n elements generate all k-combinations of S, i. e. generate
-;all possible sets consisting of k distinct elements taken from S. The number of k-combinations
-;for a sequence is equal to the binomial coefficient
-;
-(defn set-add
-  "Adds el to every set in col."
-  [col el]
-  (reduce (fn [result x]
-            (conj result (conj x el)))
-          #{}
-          col))
+;; p103: Generating k-combinations
+
+;; Given a sequence S consisting of n elements generate all k-combinations of S,
+;; i. e. generate all possible sets consisting of k distinct elements taken from S.
+;; The number of k-combinations for a sequence is equal to the binomial coefficient.
+
+(ns p103.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn add-to-colls
+  "Adds x to every collection in xs. Returns a set of the updated collections."
+  [xs x]
+  (->> xs
+       (map #(conj % x))
+       set))
 
 (defn power-set
-  "Returns the power set of set."
-  [set]
-  (let [power-set0 (reduce (fn [result x]
-                             (into result (conj (set-add result x) #{x})))
-                           #{}
-                           set)]
-    (conj power-set0 #{})))
+  "Returns the power set."
+  [xs]
+  (reduce (fn [res x]
+            (->> #{x}
+                 (conj (add-to-colls res x))
+                 (into res)))
+          #{#{}}
+          xs))
 
-(defn gen-k-combinations
-  "Generates all possible sets consisting of N distinct elements taken from col."
-  [N col]
-  (set (filter #(= N (count %)) (power-set col))))
+(defn generate-k-combinations
+  [N xs]
+  (->> (power-set xs)
+       (filter #(= N (count %)))
+       set))
 
-;tests
-(= (gen-k-combinations 1 #{4 5 6}) #{#{4} #{5} #{6}})
-(= (gen-k-combinations 10 #{4 5 6}) #{})
-(= (gen-k-combinations 2 #{0 1 2}) #{#{0 1} #{0 2} #{1 2}})
-(= (gen-k-combinations 3 #{0 1 2 3 4}) #{#{0 1 2} #{0 1 3} #{0 1 4} #{0 2 3} #{0 2 4}
-                                         #{0 3 4} #{1 2 3} #{1 2 4} #{1 3 4} #{2 3 4}})
-(= (gen-k-combinations 4 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a "abc" "efg"}})
-(= (gen-k-combinations 2 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a} #{[1 2 3] "abc"} #{[1 2 3] "efg"}
-                                                      #{:a "abc"} #{:a "efg"} #{"abc" "efg"}})
+(deftest tests
+  (testing "test1"
+    (is (= (generate-k-combinations 1 #{4 5 6}) #{#{4} #{5} #{6}})))
+  (testing "test2"
+    (is (= (generate-k-combinations 10 #{4 5 6}) #{})))
+  (testing "test3"
+    (is (= (generate-k-combinations 2 #{0 1 2}) #{#{0 1} #{0 2} #{1 2}})))
+  (testing "test4"
+    (is (= (generate-k-combinations 3 #{0 1 2 3 4}) #{#{0 1 2} #{0 1 3} #{0 1 4} #{0 2 3} #{0 2 4}
+                                                 #{0 3 4} #{1 2 3} #{1 2 4} #{1 3 4} #{2 3 4}})))
+  (testing "test5"
+    (is (= (generate-k-combinations 4 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a "abc" "efg"}})))
+  (testing "test6"
+    (is (= (generate-k-combinations 2 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a} #{[1 2 3] "abc"} #{[1 2 3] "efg"}
+                                                              #{:a "abc"} #{:a "efg"} #{"abc" "efg"}}))))
