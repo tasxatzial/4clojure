726d260bd04ad6e4b102d82325450f241148998e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-18 22:53:22 +0200

    problem 93: refactoring, remove old solution & add new one

--- problems/093.clj
+++ problems/093.clj
@@ -1,21 +1,31 @@
-;p93: Partially Flatten a Sequence
-;Write a function which flattens any nested combination of sequential things (lists, vectors, etc.), but
-;maintains the lowest level sequential items. The result should be a sequence of sequences with only one level of nesting
-;
-(defn my-partial-flatten
-   "Partially flattens a sequence. Maintains the lowest level sequential items."
-   [col]
-   (reduce (fn [result x]
-             (into result
-                   (if (and (sequential? x) (sequential? (first x)))
-                     (my-partial-flatten x)
-                     [x])))
-           [] col))
+;; p93: Partially Flatten a Sequence
 
-;tests
-(= (my-partial-flatten [["Do"] ["Nothing"]])
-   [["Do"] ["Nothing"]])
-(= (my-partial-flatten [[[[:a :b]]] [[:c :d]] [:e :f]])
-   [[:a :b] [:c :d] [:e :f]])
-(= (my-partial-flatten '((1 2) ((3 4) ((((5 6)))))))
-   '((1 2) (3 4) (5 6)))
+;; Write a function which flattens any nested combination of
+;; sequential things (lists, vectors, etc.), but maintains the
+;; lowest level sequential items. The result should be a
+;; sequence of sequences with only one level of nesting.
+
+(ns p93.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn make-flat
+  [xs]
+  (reduce (fn flat- [result x]
+            (if (sequential? x)
+              (if (every? (complement sequential?) x)
+                (conj result x)
+                (reduce flat- result x))
+              (conj result x)))
+          []
+          xs))
+
+(deftest tests
+  (testing "test1"
+    (is (= (make-flat [["Do"] ["Nothing"]])
+           [["Do"] ["Nothing"]])))
+  (testing "test2"
+    (is (= (make-flat [[[[:a :b]]] [[:c :d]] [:e :f]])
+           [[:a :b] [:c :d] [:e :f]])))
+  (testing "test3"
+    (is (= (make-flat '((1 2) ((3 4) ((((5 6)))))))
+           '((1 2) (3 4) (5 6))))))
