ed51d3c7f7b4853d83bd412435de9d47609a7fd3
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-19 23:10:55 +0200

    problem 122: refactoring

--- problems/122.clj
+++ problems/122.clj
@@ -1,23 +1,34 @@
-;p122: Read a binary number
-;Convert a binary number, provided in the form of a string, to its numerical value
+;; p122: Read a binary number
+
+;; Convert a binary number, provided in the form of a string, to its numerical value.
+
+(ns p122.core
+  (:require [clojure.test :refer [deftest testing is]]))
 
 (defn to-decimal
-  "Coverts a binary number represented as a string, to decimal."
-  ([digits] (to-decimal digits 0 (dec (count digits))))
-  ([digits result i]
-   (if (empty? digits)
-     result
-     (let [digit (first digits)]
-       (if (= \0 digit)
-         (recur (rest digits) result (dec i))
-         (let [new-result (+ result (Math/round (Math/pow 2 i)))]
-           (recur (rest digits) new-result (dec i))))))))
+  [str]
+  (loop [digits (seq str)
+         result 0
+         idx (dec (count digits))]
+    (if digits
+      (if (= \0 (first digits))
+        (recur (next digits) result (dec idx))
+        (let [pow2 (Math/round (Math/pow 2 idx))]
+          (recur (next digits) (+' result pow2) (dec idx))))
+      result)))
 
-;tests
-(= 0     (to-decimal "0"))
-(= 7     (to-decimal "111"))
-(= 8     (to-decimal "1000"))
-(= 9     (to-decimal "1001"))
-(= 255   (to-decimal "11111111"))
-(= 1365  (to-decimal "10101010101"))
-(= 65535 (to-decimal "1111111111111111"))
+(deftest tests
+  (testing "test1"
+    (is (= 0     (to-decimal "0"))))
+  (testing "test2"
+    (is (= 7     (to-decimal "111"))))
+  (testing "test3"
+    (is (= 8     (to-decimal "1000"))))
+  (testing "test4"
+    (is (= 9     (to-decimal "1001"))))
+  (testing "test5"
+    (is (= 255   (to-decimal "11111111"))))
+  (testing "test6"
+    (is (= 1365  (to-decimal "10101010101"))))
+  (testing "test7"
+    (is (= 65535 (to-decimal "1111111111111111")))))
