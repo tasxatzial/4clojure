c8f4f3596e36d10eee12956518cb6c123890915e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-19 21:14:51 +0200

    problem 120: refactoring

--- problems/120.clj
+++ problems/120.clj
@@ -1,27 +1,40 @@
-;p120: Sum of square of digits
-;Write a function which takes a collection of integers as an argument.
-;Return the count of how many elements are smaller than the sum of their squared
-;component digits. For example: 10 is larger than 1 squared plus 0 squared;
-;whereas 15 is smaller than 1 squared plus 5 squared
-;
+;; p120: Sum of square of digits
+
+;; Write a function which takes a collection of integers as an argument.
+;; Return the count of how many elements are smaller than the sum of their squared
+;; component digits. For example: 10 is larger than 1 squared plus 0 squared;
+;; whereas 15 is smaller than 1 squared plus 5 squared.
+
+(ns p120.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
 (defn to-digits
   "Returns a list of the digits of a number."
-  [N]
-  (map (comp read-string str) (str N)))
+  [n]
+  (->> n
+       str
+       (map #(Character/digit ^char % 10))))
 
 (defn squared-digit-sum
-  "Returns the sum of the squares of the digits of num."
-  [num]
-  (reduce (fn [result x]
-            (+ result (* x x)))
-          0 (to-digits num)))
+  "Returns the sum of the squares of the digits a number."
+  [n]
+  (->> n
+       to-digits
+       (map #(* % %))
+       (apply +)))
 
 (defn p120
   [col]
-  (count (filter #(< % (squared-digit-sum %)) col)))
+  (->> col
+       (filter #(< % (squared-digit-sum %)))
+       count))
 
-;tests
-(= 8 (p120 (range 10)))
-(= 19 (p120 (range 30)))
-(= 50 (p120 (range 100)))
-(= 50 (p120 (range 1000)))
+(deftest tests
+  (testing "test1"
+    (is (= 8 (p120 (range 10)))))
+  (testing "test2"
+    (is (= 19 (p120 (range 30)))))
+  (testing "test3"
+    (is (= 50 (p120 (range 100)))))
+  (testing "test4"
+    (is (= 50 (p120 (range 1000))))))
