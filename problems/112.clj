978144b5b8fafe77e7e3b617d3e6d8024ae77b00
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-19 17:46:23 +0200

    problem 112: refactoring

--- problems/112.clj
+++ problems/112.clj
@@ -1,9 +1,13 @@
-;p112: Sequs Horribilis
-;Create a function which takes an integer and a nested collection of integers as
-;arguments. Analyze the elements of the input collection and return a sequence which
-;maintains the nested structure, and which includes all elements starting from the
-;head whose sum is less than or equal to the input integer
-;
+;; p112: Sequs Horribilis
+
+;; Create a function which takes an integer and a nested collection of integers as
+;; arguments. Analyze the elements of the input collection and return a sequence which
+;; maintains the nested structure, and which includes all elements starting from the
+;; head whose sum is less than or equal to the input integer.
+
+(ns p112.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
 (defn sequs-horribilis
   [N col]
   ((fn _sequs-horribilis
@@ -12,9 +16,9 @@
        result
        (let [current-element (first col)]
          (if (sequential? current-element)
-           (let [partial-seq (_sequs-horribilis current-element sum '())
-                 new-sum (+ sum (reduce + (flatten partial-seq)))
-                 new-result (concat result [partial-seq])]
+           (let [sublist (_sequs-horribilis current-element sum '())
+                 new-sum (+ sum (apply + (flatten sublist)))
+                 new-result (concat result [sublist])]
              (recur (next col) new-sum new-result))
            (if (< N (+ sum current-element))
              result
@@ -23,18 +27,25 @@
                (recur (next col) new-sum new-result)))))))
    col 0 '()))
 
-;tests
-(=  (sequs-horribilis 10 [1 2 [3 [4 5] 6] 7])
-    '(1 2 (3 (4))))
-(=  (sequs-horribilis 30 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11])
-    '(1 2 (3 (4 (5 (6 (7)))))))
-(=  (sequs-horribilis 9 (range))
-    '(0 1 2 3))
-(=  (sequs-horribilis 1 [[[[[1]]]]])
-    '(((((1))))))
-(=  (sequs-horribilis 0 [1 2 [3 [4 5] 6] 7])
-    '())
-(=  (sequs-horribilis 0 [0 0 [0 [0]]])
-    '(0 0 (0 (0))))
-(=  (sequs-horribilis 1 [-10 [1 [2 3 [4 5 [6 7 [8]]]]]])
-    '(-10 (1 (2 3 (4)))))
+(deftest tests
+  (testing "test1"
+    (is (=  (sequs-horribilis 10 [1 2 [3 [4 5] 6] 7])
+            '(1 2 (3 (4))))))
+  (testing "test2"
+    (is (=  (sequs-horribilis 30 [1 2 [3 [4 [5 [6 [7 8]] 9]] 10] 11])
+            '(1 2 (3 (4 (5 (6 (7)))))))))
+  (testing "test3"
+    (is (=  (sequs-horribilis 9 (range))
+            '(0 1 2 3))))
+  (testing "test4"
+    (is (=  (sequs-horribilis 1 [[[[[1]]]]])
+            '(((((1))))))))
+  (testing "test5"
+    (is (=  (sequs-horribilis 0 [1 2 [3 [4 5] 6] 7])
+            '())))
+  (testing "test5"
+    (is (=  (sequs-horribilis 0 [0 0 [0 [0]]])
+            '(0 0 (0 (0))))))
+  (testing "test7"
+    (is (=  (sequs-horribilis 1 [-10 [1 [2 3 [4 5 [6 7 [8]]]]]])
+            '(-10 (1 (2 3 (4))))))))
