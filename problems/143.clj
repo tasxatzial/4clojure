08fc215a0900f1973c3098b6c229ecf05dd5ac7d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-20 05:21:47 +0200

    problem 143: refactoring

--- problems/143.clj
+++ problems/143.clj
@@ -1,13 +1,21 @@
-;p143: dot product
-;Create a function that computes the dot product of two sequences. You may assume that the
-;vectors will have the same length
-;
- (defn compute-dot-product
-   [col1 col2]
-   (reduce + (map * col1 col2)))
+;; p143: dot product
 
-;tests
-(= 0 (compute-dot-product [0 1 0] [1 0 0]))
-(= 3 (compute-dot-product [1 1 1] [1 1 1]))
-(= 32 (compute-dot-product [1 2 3] [4 5 6]))
-(= 256 (compute-dot-product [2 5 6] [100 10 1]))
+;; Create a function that computes the dot product of two sequences. You may assume
+;; that the vectors will have the same length.
+
+(ns p143.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn dot-product
+  [coll1 coll2]
+  (reduce + (map * coll1 coll2)))
+
+(deftest tests
+  (testing "test1"
+    (is (= 0 (dot-product [0 1 0] [1 0 0]))))
+  (testing "test2"
+    (is (= 3 (dot-product [1 1 1] [1 1 1]))))
+  (testing "test3"
+    (is (= 32 (dot-product [1 2 3] [4 5 6]))))
+  (testing "test4"
+    (is (= 256 (dot-product [2 5 6] [100 10 1])))))
