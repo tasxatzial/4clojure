7e3d2a10c0ee09c7943239ff5929c48d565e9b6a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-20 17:26:56 +0200

    problem 144: refactoring

--- problems/144.clj
+++ problems/144.clj
@@ -1,21 +1,26 @@
-;p144: Oscillate
-;Write an oscillating iterate: a function that takes an initial value and a variable number of
-;functions. It should return a lazy sequence of the functions applied to the value in order,
-;restarting from the first function after it hits the end
-;
-(defn oscillate
-  "Returns a lazy sequence of the functions args applied to val in order,
-   restarting from the first function after it hits the end."
-  [val & args]
-  (let [func-count (count args)]
-    ((fn _oscillate
-       [val i]
-       (let [index (mod i func-count)]
-         (cons val (lazy-seq (_oscillate ((nth args index) val) (inc index))))))
-     val
-     0)))
+;; p144: Oscillate
 
-;tests
-(= (take 3 (oscillate 3.14 int double)) [3.14 3 3.0])
-(= (take 5 (oscillate 3 #(- % 3) #(+ 5 %))) [3 0 5 2 7])
-(= (take 12 (oscillate 0 inc dec inc dec inc)) [0 1 0 1 0 1 2 1 2 1 2 3])
+;; Write an oscillating iterate: a function that takes an initial value and a variable
+;; number of functions. It should return a lazy sequence of the functions applied to the
+;; value in order, restarting from the first function after it hits the end.
+
+(ns p144.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn oscillate
+  [val & fns]
+  (let [fns-count (count fns)]
+    (letfn [(_oscillate [i last-val]
+              (lazy-seq
+                (let [f (nth fns (mod i fns-count))
+                      fval (f last-val)]
+                  (cons fval (_oscillate (inc i) fval)))))]
+      (cons val (_oscillate 0 val)))))
+
+(deftest tests
+  (testing "test1"
+    (is (= (take 3 (oscillate 3.14 int double)) [3.14 3 3.0])))
+  (testing "test2"
+    (is (= (take 5 (oscillate 3 #(- % 3) #(+ 5 %))) [3 0 5 2 7])))
+  (testing "test3"
+    (is (= (take 12 (oscillate 0 inc dec inc dec inc)) [0 1 0 1 0 1 2 1 2 1 2 3]))))
