a095c20dd368a1038b478636ce9ca5640490f1a1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-20 05:18:11 +0200

    problem 141: refactoring

--- problems/141.clj
+++ problems/141.clj
@@ -1,41 +1,45 @@
-;p141: Tricky card games
-;In trick-taking card games such as bridge, spades, or hearts, cards are played in groups known
-;as "tricks" - each player plays a single card, in order; the first player is said to "lead" to
-;the trick. After all players have played, one card is said to have "won" the trick. How the
-;winner is determined will vary by game, but generally the winner is the highest card played in
-;the suit that was led. Sometimes (again varying by game), a particular suit will be designated
-;"trump", meaning that its cards are more powerful than any others: if there is a trump suit, and
-;any trumps are played, then the highest trump wins regardless of what was led.
-;Your goal is to devise a function that can determine which of a number of cards has won a trick.
-;You should accept a trump suit, and return a function winner. Winner will be called on a sequence
-;of cards, and should return the one which wins the trick. Cards will be represented in the format
-;returned by Problem 128, Recognize Playing Cards: a hash-map of :suit and a numeric :rank.
-;Cards with a larger rank are stronger
-;
+;; p141: Tricky card games
+
+;; In trick-taking card games such as bridge, spades, or hearts, cards are played in
+;; groups known as "tricks" - each player plays a single card, in order; the first player
+;; is said to "lead" to the trick. After all players have played, one card is said to have
+;; "won" the trick. How the winner is determined will vary by game, but generally the
+;; winner is the highest card played in the suit that was led.
+;; Sometimes (again varying by game), a particular suit will be designated "trump", meaning
+;; that its cards are more powerful than any others: if there is a trump suit, and any
+;; trumps are played, then the highest trump wins regardless of what was led.
+;; Your goal is to devise a function that can determine which of a number of cards has won
+;; a trick. You should accept a trump suit, and return a function winner. Winner will be
+;; called on a sequence of cards, and should return the one which wins the trick. Cards will
+;; be represented in the format returned by Problem 128, Recognize Playing Cards: a hash-map
+;; of :suit and a numeric :rank. Cards with a larger rank are stronger.
+
+(ns p141.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
 (defn p141
   [trump]
-  (fn [col]
-    (let [new-trump (if (nil? trump)
-                   (:suit (first col))
-                   trump)]
+  (fn [coll]
+    (let [trump (or trump (:suit (first coll)))]
       (reduce (fn [result x]
-                (if (and (= new-trump (:suit x))
+                (if (and (= trump (:suit x))
                          (> (:rank x) (:rank result)))
                   x
                   result))
-              {:rank 0}
-              col))))
+              (first coll)
+              (rest coll)))))
 
-;tests
-(let [notrump (p141 nil)]
-  (and (= {:suit :club :rank 9}  (notrump [{:suit :club :rank 4}
-                                           {:suit :club :rank 9}]))
-       (= {:suit :spade :rank 2} (notrump [{:suit :spade :rank 2}
-                                           {:suit :club :rank 10}]))))
-
-(= {:suit :club :rank 10} ((p141 :club) [{:suit :spade :rank 2}
-                                       {:suit :club :rank 10}]))
-
-(= {:suit :heart :rank 8}
-   ((p141 :heart) [{:suit :heart :rank 6} {:suit :heart :rank 8}
-                 {:suit :diamond :rank 10} {:suit :heart :rank 4}]))
+(deftest tests
+  (let [notrump (p141 nil)]
+    (testing "test1"
+      (is (= {:suit :club :rank 9}  (notrump [{:suit :club :rank 4}
+                                              {:suit :club :rank 9}])))
+      (is (= {:suit :spade :rank 2} (notrump [{:suit :spade :rank 2}
+                                              {:suit :club :rank 10}])))))
+  (testing "test2"
+    (is (= {:suit :club :rank 10} ((p141 :club) [{:suit :spade :rank 2}
+                                                 {:suit :club :rank 10}]))))
+  (testing "test3"
+    (is (= {:suit :heart :rank 8}
+           ((p141 :heart) [{:suit :heart :rank 6} {:suit :heart :rank 8}
+                           {:suit :diamond :rank 10} {:suit :heart :rank 4}])))))
