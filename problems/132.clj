9cb86896f61e9de7b091be123d07b00ec3de46c9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-20 02:36:53 +0200

    problem 132: refactoring, remove old solution & add new one

--- problems/132.clj
+++ problems/132.clj
@@ -1,33 +1,39 @@
-;p132: Insert between two items
-;Write a function that takes a two-argument predicate, a value, and a collection; and returns
-;a new collection where the value is inserted between every two items that satisfy the predicate
-;
+;; p132: Insert between two items
+
+;; Write a function that takes a two-argument predicate, a value, and a collection;
+;; and returns a new collection where the value is inserted between every two items
+;; that satisfy the predicate.
+
+(ns p132.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
 (defn insert-between
-  "Returns a new collection where the value is inserted between every two
-   items that satisfy the predicate."
-  [pred val col]
-  (if (empty? col)
-    col
-    ((fn _insert-between [idx]
-       (let [next-item (nth col (inc idx) nil)
-             cur-item (nth col idx)]
-         (if (nil? next-item)
-           [cur-item]
-           (if (pred cur-item next-item)
-             (lazy-seq (cons cur-item (cons val (_insert-between (inc idx)))))
-             (lazy-seq (cons cur-item (_insert-between (inc idx))))))))
-     0)))
+  [pred val coll]
+  (if (empty? coll)
+    coll
+    (letfn [(_insert-between [coll last-added]
+              (lazy-seq
+                (when (seq coll)
+                  (let [x (first coll)]
+                    (if (pred last-added x)
+                      (cons val (cons x (_insert-between (rest coll) x)))
+                      (cons x (_insert-between (rest coll) x)))))))]
+      (cons (first coll) (_insert-between (rest coll) (first coll))))))
 
-;tests
-(= '(1 :less 6 :less 7 4 3) (insert-between < :less [1 6 7 4 3]))
-(= '(2) (insert-between > :more [2]))
-(= [0 1 :x 2 :x 3 :x 4]  (insert-between #(and (pos? %) (< % %2)) :x (range 5)))
-(empty? (insert-between > :more ()))
-
-(= [0 1 :same 1 2 3 :same 5 8 13 :same 21]
-   (take 12 (->> [0 1]
-                 (iterate (fn [[a b]] [b (+ a b)]))
-                 (map first) ; fibonacci numbers
-                 (insert-between (fn [a b] ; both even or both odd
-                       (= (mod a 2) (mod b 2)))
-                     :same))))
+(deftest tests
+  (testing "test1"
+    (is (= '(1 :less 6 :less 7 4 3) (insert-between < :less [1 6 7 4 3]))))
+  (testing "test2"
+    (is (= '(2) (insert-between > :more [2]))))
+  (testing "test3"
+    (is (= [0 1 :x 2 :x 3 :x 4]  (insert-between #(and (pos? %) (< % %2)) :x (range 5)))))
+  (testing "test4"
+    (is (empty? (insert-between > :more ()))))
+  (testing "test5"
+    (is (= [0 1 :same 1 2 3 :same 5 8 13 :same 21]
+           (take 12 (->> [0 1]
+                         (iterate (fn [[a b]] [b (+ a b)]))
+                         (map first) ; fibonacci numbers
+                         (insert-between (fn [a b] ; both even or both odd
+                                           (= (mod a 2) (mod b 2)))
+                                         :same)))))))
