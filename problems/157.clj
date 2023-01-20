2058da2c108e00bf53bea859a849fe06bf3679b6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-20 21:09:02 +0200

    problem 157: refactoring, add new solution

--- problems/157.clj
+++ problems/157.clj
@@ -1,11 +1,31 @@
-;p157: Indexing Sequences
-;Transform a sequence into a sequence of pairs containing the original elements along with their index
-;
-(defn index-sequence
-  [col]
-  (partition 2 (interleave col (range))))
+;; p157: Indexing Sequences
 
-;tests
-(= (index-sequence [:a :b :c]) [[:a 0] [:b 1] [:c 2]])
-(= (index-sequence [0 1 3]) '((0 0) (1 1) (3 2)))
-(= (index-sequence [[:foo] {:bar :baz}]) [[[:foo] 0] [{:bar :baz} 1]])
+;; Transform a sequence into a sequence of pairs containing the original elements
+;; along with their index.
+
+(ns p157.core
+  (:require [clojure.test :refer [deftest testing is]]))
+
+(defn indexed-sequence1
+  [coll]
+  (partition 2 (interleave coll (range))))
+
+(defn indexed-sequence2
+  [coll]
+  (keep-indexed #(vector %2 %1) coll))
+
+(deftest tests-create-map1
+  (testing "test1"
+    (is (= (indexed-sequence1 [:a :b :c]) [[:a 0] [:b 1] [:c 2]])))
+  (testing "test2"
+    (is (= (indexed-sequence1 [0 1 3]) '((0 0) (1 1) (3 2)))))
+  (testing "test3"
+    (is (= (indexed-sequence1 [[:foo] {:bar :baz}]) [[[:foo] 0] [{:bar :baz} 1]]))))
+
+(deftest tests-create-map2
+  (testing "test1"
+    (is (= (indexed-sequence2 [:a :b :c]) [[:a 0] [:b 1] [:c 2]])))
+  (testing "test2"
+    (is (= (indexed-sequence2 [0 1 3]) '((0 0) (1 1) (3 2)))))
+  (testing "test3"
+    (is (= (indexed-sequence2 [[:foo] {:bar :baz}]) [[[:foo] 0] [{:bar :baz} 1]]))))
