fd80327ec035f83843b80bab7371e7e51401215c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-01-27 04:39:01 +0200

    problem 67: refactoring, optimize for speed

--- problems/067.clj
+++ problems/067.clj
@@ -5,20 +5,28 @@
 (ns p67.core
   (:require [clojure.test :refer [deftest testing is]]))
 
-(defn my-primes
-  ([x]
-   (my-primes x [] 2))
-  ([x result N]
-   (if (zero? x)
-     result
-     (if (some #(zero? (mod N %)) result)
-       (recur x result (inc N))
-       (recur (dec x) (conj result N) (inc N))))))
+(defn composite?
+  "Returns true if N has any of the primes as a factor, else false."
+  [N primes]
+  (->> primes
+       (take-while #(< % (inc (Math/sqrt N))))
+       (some #(zero? (mod N %)))))
+
+(defn primes
+  [x]
+  (loop [x x
+         result []
+         N 2]
+    (if (zero? x)
+      result
+      (if (composite? N result)
+        (recur x result (inc N))
+        (recur (dec x) (conj result N) (inc N))))))
 
 (deftest tests
   (testing "test1"
-    (is (= (my-primes 2) [2 3])))
+    (is (= (primes 2) [2 3])))
   (testing "test2"
-    (is (= (my-primes 5) [2 3 5 7 11])))
+    (is (= (primes 5) [2 3 5 7 11])))
   (testing "test3"
-    (is (= (last (my-primes 100)) 541))))
+    (is (= (last (primes 100)) 541))))
