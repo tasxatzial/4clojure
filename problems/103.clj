d7bed96eb47424cf323bf58a6f2f7397735806a8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-02-10 04:40:30 +0200

    problem 103: remove old solution and add new one

--- problems/103.clj
+++ problems/103.clj
@@ -7,41 +7,33 @@
 (ns p103.core
   (:require [clojure.test :refer [deftest testing is]]))
 
-(defn add-to
-  "Adds x to every collection. Returns a set."
-  [colls x]
-  (->> colls
-       (map #(conj % x))
-       set))
-
-(defn power-set
-  "Returns the power set."
-  [coll]
-  (reduce (fn [res x]
-            (->> #{x}
-                 (conj (add-to res x))
-                 (into res)))
-          #{#{}}
-          coll))
-
-(defn generate-k-combinations
-  [N coll]
-  (->> (power-set coll)
-       (filter #(= N (count %)))
-       set))
+(defn k-combinations
+  [k coll]
+  (if (<= k 0)
+    #{}
+    (if (= k 1)
+      (set (map hash-set coll))
+      (loop [result []
+             coll coll]
+        (if (seq coll)
+          (let [[el & rest-coll] coll
+                rest-combinations (k-combinations (dec k) rest-coll)
+                el-combinations (map #(conj % el) rest-combinations)]
+            (recur (into result el-combinations) rest-coll))
+          (set result))))))
 
 (deftest tests
   (testing "test1"
-    (is (= (generate-k-combinations 1 #{4 5 6}) #{#{4} #{5} #{6}})))
+    (is (= (k-combinations 1 #{4 5 6}) #{#{4} #{5} #{6}})))
   (testing "test2"
-    (is (= (generate-k-combinations 10 #{4 5 6}) #{})))
+    (is (= (k-combinations 10 #{4 5 6}) #{})))
   (testing "test3"
-    (is (= (generate-k-combinations 2 #{0 1 2}) #{#{0 1} #{0 2} #{1 2}})))
+    (is (= (k-combinations 2 #{0 1 2}) #{#{0 1} #{0 2} #{1 2}})))
   (testing "test4"
-    (is (= (generate-k-combinations 3 #{0 1 2 3 4}) #{#{0 1 2} #{0 1 3} #{0 1 4} #{0 2 3} #{0 2 4}
-                                                 #{0 3 4} #{1 2 3} #{1 2 4} #{1 3 4} #{2 3 4}})))
+    (is (= (k-combinations 3 #{0 1 2 3 4}) #{#{0 1 2} #{0 1 3} #{0 1 4} #{0 2 3} #{0 2 4}
+                                                      #{0 3 4} #{1 2 3} #{1 2 4} #{1 3 4} #{2 3 4}})))
   (testing "test5"
-    (is (= (generate-k-combinations 4 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a "abc" "efg"}})))
+    (is (= (k-combinations 4 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a "abc" "efg"}})))
   (testing "test6"
-    (is (= (generate-k-combinations 2 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a} #{[1 2 3] "abc"} #{[1 2 3] "efg"}
-                                                              #{:a "abc"} #{:a "efg"} #{"abc" "efg"}}))))
+    (is (= (k-combinations 2 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a} #{[1 2 3] "abc"} #{[1 2 3] "efg"}
+                                                                   #{:a "abc"} #{:a "efg"} #{"abc" "efg"}}))))
