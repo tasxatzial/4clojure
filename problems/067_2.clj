dc1b744ae8ac4100241060c6012f82302ce63195
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-04-03 05:00:17 +0300

    problem 67: refactoring (lazy)

--- problems/067_2.clj
+++ problems/067_2.clj
@@ -2,36 +2,47 @@
 
 ;; Write a function which returns the first x number of prime numbers.
 
+;; lazy implementation
+
 (ns p67.core
   (:require [clojure.test :refer [deftest testing is]]))
 
 (defn prime?
-  "Returns true if N is prime. This will happen if N is not a multiple
-  of any number in the given list of primes."
+  "Returns true if N is prime. This will happen if N is a not multiple
+  of any number in the given vector of primes."
   [N primes]
-  (reduce (fn [result prime]
-            (if (> prime (inc (Math/sqrt N)))
-              (reduced true)
-              (if (= 0 (mod N prime))
-                (reduced false)
-                result)))
-          true
-          primes))
+  (let [sqrt-N (Math/sqrt N)]
+    (reduce (fn [result prime]
+              (if (> prime sqrt-N)
+                (reduced true)
+                (if (= 0 (mod N prime))
+                  (reduced false)
+                  result)))
+            true
+            primes)))
 
-(defn primes-lazy
-  "Returns a lazy seq of primes."
-  []
-  (letfn [(_step [result N]
-            (lazy-seq
-              (if (prime? N result)
-                (cons N (_step (conj result N) (inc N)))
-                (_step result (inc N)))))]
-    (_step [] 2)))
+(defn add-next-prime
+  "Adds the next prime to the given vector of all previous primes."
+  [primes]
+  (let [n (inc (peek primes))]
+    (loop [n n]
+      (if (prime? n primes)
+        (conj primes n)
+        (recur (inc n))))))
+
+(defn gen-primes-seq
+  "Returns a lazy sequence of primes."
+  ([]
+   (gen-primes-seq [2]))
+  ([primes]
+   (lazy-seq
+     (cons (peek primes) (gen-primes-seq (add-next-prime primes))))))
+
+(def primes-seq (gen-primes-seq))
 
 (defn primes
-  "Returns the first x primes."
-  [x]
-  (take x (primes-lazy)))
+  [n]
+  (take n primes-seq))
 
 (deftest tests
   (testing "test1"
